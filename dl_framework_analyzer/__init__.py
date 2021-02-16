1c508fda9954d96b3fb64cb88df22ec7cac66900
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-02-16 09:46:44 +0100

    [#23774] Added appending project's root to PATH in __init__.py
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- dl_framework_analyzer/__init__.py
+++ dl_framework_analyzer/__init__.py
@@ -7,3 +7,8 @@ This module consists of benchmarking tools and scenarios for checking:
 * Deep Learning models' optimizers,
 * Deep Learning models' compilers.
 """
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(__file__ + '../'))

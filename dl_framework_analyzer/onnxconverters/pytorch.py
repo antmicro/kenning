23f1a049c034b4000a01f483356e587cce702868
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-03-02 19:47:05 +0100

    [#24665] Added ONNX support tester for PyTorch
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

---
+++ dl_framework_analyzer/onnxconverters/pytorch.py
@@ -0,0 +1,57 @@
+import torchvision.models as models
+import torch
+
+from dl_framework_analyzer.core.onnxconversion import ONNXConversion
+from dl_framework_analyzer.core.onnxconversion import SupportStatus
+
+class PyTorchONNXConversion(ONNXConversion):
+    def __init__(self):
+        super().__init__('pytorch', torch.__version__)
+
+    def prepare(self):
+        self.add_entry(
+            'DenseNet201',
+            lambda: models.densenet201(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )                                 
+        self.add_entry(                   
+            'MobileNetV2',                
+            lambda: models.mobilenet_v2   (True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'ResNet50',
+            lambda: models.resnet50(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'VGG16',
+            lambda: models.vgg16(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'DeepLabV3 ResNet50',
+            lambda: models.segmentation.deeplabv3_resnet50(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'Faster R-CNN ResNet50 FPN',
+            lambda: models.detection.fasterrcnn_resnet50_fpn(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'RetinaNet ResNet50 FPN',
+            lambda: models.detection.retinanet_resnet50_fpn(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+        self.add_entry(
+            'Mask R-CNN',
+            lambda: models.detection.maskrcnn_resnet50_fpn(True),
+            input_tensor=torch.randn((1, 3, 224, 224))
+        )
+
+    def onnx_export(self, modelentry, exportpath):
+        model = modelentry.modelgenerator()
+        input_tensor = modelentry.parameters['input_tensor']
+        torch.onnx.export(model, input_tensor, exportpath, opset_version=11)
+        return SupportStatus.SUPPORTED

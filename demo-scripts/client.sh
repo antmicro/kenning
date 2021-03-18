8f622e4e803752e849e5a20fa51f55bd8515b76b
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-03-18 20:51:28 +0100

    [#25151] Updated sample scripts for server and client
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- demo-scripts/client.sh
+++ demo-scripts/client.sh
@@ -8,15 +8,17 @@ python3 -m dl_framework_analyzer.scenarios.inference_client \
     dl_framework_analyzer.datasets.pet_dataset.PetDataset \
     ./build/before-compile.onnx \
     ./build/xavier-output \
+    jetson-agx-xavier-tvm \
     --model-path build/pytorch-net-2.pth \
     --target "nvidia/jetson-agx-xavier" \
     --target-host "llvm -mtriple=aarch64-linux-gnu" \
     --compiled-model-path ./build/compiled-model.tar \
-    --opt-level 1 \
+    --opt-level 3 \
     --host 192.168.188.100 \
     --port 12345 \
+    --packet-size 32768 \
     --save-model-path /home/nvidia/compiled-model.tar \
     --target-device-context cuda \
     --dataset-root ./build/pet-dataset/ \
     --inference-batch-size 1 \
-    --verbosity DEBUG
+    --verbosity INFO
--- demo-scripts/server.sh
+++ demo-scripts/server.sh
@@ -5,7 +5,8 @@ python3 -m dl_framework_analyzer.scenarios.inference_server \
     dl_framework_analyzer.runtimes.tvm.TVMRuntime \
     --host 192.168.188.100 \
     --port 12345 \
+    --packet-size 32768 \
     --save-model-path /home/nvidia/compiled-model.tar \
     --target-device-context cuda \
     --input-dtype float32 \
-    --verbosity DEBUG
+    --verbosity INFO

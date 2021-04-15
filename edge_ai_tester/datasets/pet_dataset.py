09c70ede77ac4dbf47cad9262ad4517d892dbef7
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-04-15 11:39:57 +0200

    datasets.pet_dataset: switched to using ImageNet mean/std values
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- edge_ai_tester/datasets/pet_dataset.py
+++ edge_ai_tester/datasets/pet_dataset.py
@@ -183,4 +183,7 @@ class PetDataset(Dataset):
         return mean, std
 
     def get_input_mean_std(self):
-        return np.array([0.48136492, 0.44937421, 0.39576963]), np.array([0.22781384, 0.22496867, 0.22693157])  # noqa: E501
+        # The first mean/std are computed based on Pet Dataset
+        # However, it is recommended to use Imagenet-based mean and std values
+        # return np.array([0.48136492, 0.44937421, 0.39576963]), np.array([0.22781384, 0.22496867, 0.22693157])  # noqa: E501
+        return np.array([0.485, 0.456, 0.406], dtype='float32'), np.array([0.229, 0.224, 0.225], dtype='float32')  # noqa: E501

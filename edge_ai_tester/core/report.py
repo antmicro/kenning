f17fe6e9d5d2269fc3320e92211cdf72ae37c22e
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-04-20 12:06:56 +0200

    [#25785] Added report generation methods
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- edge_ai_tester/core/report.py
+++ edge_ai_tester/core/report.py
@@ -5,14 +5,36 @@ Functions to generate RST reports from templates and Measurements objects.
 from jinja2 import Template
 from pathlib import Path
 from typing import Dict, List
+import numpy as np
+
+
+def accuracy(confusion_matrix):
+    return np.trace(confusion_matrix) / np.sum(confusion_matrix)
+
+
+def mean_precision(confusion_matrix):
+    return np.mean(
+        np.array(confusion_matrix).diagonal() / np.sum(confusionmatrix, axis=1)
+    )
+
+
+def mean_sensitivity(confusion_matrix):
+    return np.mean(
+        np.array(confusion_matrix).diagonal() / np.sum(confusionmatrix, axis=0)
+    )
+
+
+def g_mean(confusion_matrix):
+    return np.float_power(np.prod(
+        np.array(confusion_matrix).diagonal() / np.sum(confusionmatrix, axis=0)
+    ))
 
 
 def create_report_from_measurements(
         template: Path,
-        measurementsdata: Dict[str, List],
-        result: Path):
+        measurementsdata: Dict[str, List]):
     """
-    Creates a report file from template and measurements data.
+    Creates a report from template and measurements data.
 
     Parameters
     ----------
@@ -20,14 +42,20 @@ def create_report_from_measurements(
         Path to the Jinja template
     measurementsdata : Dict[str, List]
         dictionary describing measurements taken during benchmark
-    result : Path
-        Path to the output
     """
     with open(template, 'r') as resourcetemplatefile:
         resourcetemplate = resourcetemplatefile.read()
         tm = Template(resourcetemplate)
+        tm.globals['mean'] = np.mean
+        tm.globals['std'] = np.std
+        tm.globals['median'] = np.median
+        tm.globals['accuracy'] = accuracy
+        tm.globals['mean_precision'] = mean_precision
+        tm.globals['mean_sensitivity'] = mean_sensitivity
+        tm.globals['g_mean'] = g_mean
+
         content = tm.render(
             data=measurementsdata
         )
-        with open(result, 'w') as out:
-            out.write(content)
+
+        return content

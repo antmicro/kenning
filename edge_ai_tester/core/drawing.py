37b0b0ee45261b906eee7a659071b8e1a7d4793a
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-04-20 12:06:37 +0200

    [#25785] Improved drawing methods
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- edge_ai_tester/core/drawing.py
+++ edge_ai_tester/core/drawing.py
@@ -4,10 +4,12 @@ Wrappers for drawing plots for reports.
 
 from matplotlib import pyplot as plt
 from matplotlib import patheffects
-from typing import List
+from typing import List, Tuple
 import numpy as np
 import itertools
 from pathlib import Path
+import matplotlib.colors as colors
+from matplotlib import gridspec
 
 
 def create_line_plot(
@@ -19,7 +21,10 @@ def create_line_plot(
         yunit: str,
         xdata: List,
         ydata: List,
-        trimxvalues: bool = True):
+        trimxvalues: bool = True,
+        skipfirst: bool = False,
+        figsize: Tuple = (15, 8.5),
+        bins: int = 20):
     """
     Draws single line plot.
 
@@ -46,28 +51,52 @@ def create_line_plot(
     trimxvalues : bool
         True if all values for the X dimension should be subtracted by
         the minimal value on this dimension
+    skipfirst: bool
+        True if the first entry should be removed from plotting.
+    figsize: Tuple
+        The size of the figure
+    bins: int
+        Number of bins for 
     """
-    plt.figure(tight_layout=True, figsize=(20, 20))
-    plt.title(title)
+    start = 1 if skipfirst else 0
+    xdata = np.array(xdata[start:], copy=True)
+    ydata = np.array(ydata[start:], copy=True)
     if trimxvalues:
         minx = min(xdata)
         xdata = [x - minx for x in xdata]
-    plt.plot(xdata, ydata, 'b-', alpha=0.6)
-    plt.plot(xdata, ydata, 'bo')
-    plt.xlabel(f'{xtitle} [{xunit}]')
-    plt.ylabel(f'{ytitle} [{yunit}]')
-    plt.grid()
+    fig, (axplot, axhist) = plt.subplots(
+        ncols=2,
+        tight_layout=True,
+        figsize=figsize,
+        sharey=True,
+        gridspec_kw={'width_ratios': (8, 3)}
+    )
+    fig.suptitle(title, fontsize='x-large')
+    axplot.scatter(xdata, ydata, c='purple', alpha=0.5)
+    axplot.set_xlabel(f'{xtitle} [{xunit}]', fontsize='large')
+    axplot.set_ylabel(f'{ytitle} [{yunit}]', fontsize='large')
+    axplot.grid()
 
-    plt.savefig(outpath)
+    axhist.hist(ydata, bins=bins, orientation='horizontal', color='purple')
+    axhist.set_xscale('log')
+    axhist.set_xlabel('Value histogram', fontsize='large')
+    axhist.grid(which='both')
+    plt.setp(axhist.get_yticklabels(), visible=False)
+
+    if outpath is None:
+        plt.show()
+    else:
+        plt.savefig(outpath)
 
 
 def draw_confusion_matrix(
         confusion_matrix,
-        outpath: Path,
+        outpath,
         title: str,
-        class_names: List[str],
-        normalized: bool = True,
-        cmap=None):
+        class_names,
+        cmap=None,
+        figsize=None,
+        dpi=None):
     """
     Creates a confusion matrix plot.
 
@@ -82,56 +111,165 @@ def draw_confusion_matrix(
         Title of the plot
     class_names : List[str]
         List of the class names
-    normalized : bool
-        If true, the values for each ground truth class will be normalized
     cmap : Any
         Color map for the plot
+    figsize : Tuple
+        The size of the plot
+    dpi : int
+        The dpi of the plot
     """
     if cmap is None:
-        cmap = plt.get_cmap('Blues')
+        cmap = plt.get_cmap('BuPu')
 
+    confusion_matrix = np.array(confusion_matrix, dtype=np.float32, copy=True)
+
+    # normalize confusion matrix
+    confusion_matrix /= confusion_matrix.sum(axis=0)
+
+    # compute overall accuracy
     accuracy = np.trace(confusion_matrix) / np.sum(confusion_matrix)
 
-    plt.figure(tight_layout=True, figsize=(20, 20))
-    plt.title(f'{title} (ACC={accuracy})')
+    # compute sensitivity
+    correctactual = confusion_matrix.diagonal() / confusion_matrix.sum(axis=1)
+    correctactual = correctactual.reshape(1, len(class_names))
 
+    # compute precision
+    correctpredicted = confusion_matrix.diagonal() / confusion_matrix.sum(axis=0)
+    correctpredicted = correctpredicted.reshape(len(class_names), 1)
+
+    if figsize is None:
+        figsize = confusion_matrix.shape
+
+    if dpi is None:
+        dpi = 216
+
+    # create axes
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    gs = gridspec.GridSpec(len(class_names) + 1, len(class_names) + 1)
+    axConfMatrix = fig.add_subplot(gs[0:len(class_names), 0:len(class_names)])
+    axPredicted = fig.add_subplot(gs[len(class_names), 0:len(class_names)], sharex=axConfMatrix)
+    axActual = fig.add_subplot(gs[0:len(class_names), len(class_names)], sharey=axConfMatrix)
+    axTotal = fig.add_subplot(gs[len(class_names), len(class_names)], sharex=axActual, sharey=axPredicted)
+
+    # define ticks for classes
     ticks = np.arange(len(class_names))
-    plt.xticks(ticks, class_names, rotation=90)
-    plt.yticks(ticks, class_names)
-    plt.xlabel('Actual class')
-    plt.ylabel('Predicted class')
 
-    if normalized:
-        confusion_matrix /= confusion_matrix.sum(axis=0)
-
-    plt.imshow(
+    # configure and draw confusion matrix
+    axConfMatrix.set_xticks(ticks)
+    axConfMatrix.set_xticklabels(class_names, fontsize='large', rotation=90)
+    axConfMatrix.set_yticks(ticks)
+    axConfMatrix.set_yticklabels(class_names, fontsize='large')
+    axConfMatrix.set_xlabel('Actual class', fontsize='x-large')
+    axConfMatrix.set_ylabel('Predicted class', fontsize='x-large')
+    img = axConfMatrix.imshow(
         confusion_matrix,
+        norm=colors.LogNorm(),
         interpolation='nearest',
-        cmap=cmap)
+        cmap=cmap,
+        aspect='auto',
+    )
+    axConfMatrix.xaxis.set_ticks_position('top')
+    axConfMatrix.xaxis.set_label_position('top')
 
+    # add percentages for confusion matrix
     for i, j in itertools.product(
             range(len(class_names)),
             range(len(class_names))):
-        if normalized:
-            txt = plt.text(
-                i, j,
-                f'{confusion_matrix[i,j]:0.3f}',
-                horizontalalignment='center',
-                color='black',
-                fontsize=9)
-            txt.set_path_effects([
-                patheffects.withStroke(linewidth=3, foreground='w')
-            ])
-        else:
-            txt = plt.text(
-                i, j,
-                f'{confusion_matrix[i,j]}',
-                horizontalalignment='center',
-                color='black',
-                fontsize=9)
-            txt.set_path_effects([
-                patheffects.withStroke(linewidth=3, foreground='w')
-            ])
+        txt = axConfMatrix.text(
+            i, j,
+            '100' if confusion_matrix[i,j] == 1.0 else f'{100.0 * confusion_matrix[i,j]:3.1f}',
+            ha='center',
+            va='center',
+            color='black',
+            fontsize='medium')
+        txt.set_path_effects([
+            patheffects.withStroke(linewidth=5, foreground='w')
+        ])
 
-    plt.tight_layout()
-    plt.savefig(outpath)
+    # configure and draw sensitivity percentages
+    axPredicted.set_xticks(ticks)
+    axPredicted.set_yticks([0])
+    axPredicted.set_xlabel('Sensitivity', fontsize='large')    
+    axPredicted.imshow(
+        correctactual,
+        interpolation='nearest',
+        cmap='RdYlGn',
+        aspect='auto',
+        vmin=0.0,
+        vmax=1.0
+    )
+    for i in range(len(class_names)):
+        txt = axPredicted.text(
+            i, 0,
+            '100' if correctactual[0, i] == 1.0 else f'{100.0 * correctactual[0, i]:3.1f}',
+            ha='center',
+            va='center',
+            color='black',
+            fontsize='medium')
+        txt.set_path_effects([
+            patheffects.withStroke(linewidth=5, foreground='w')
+        ])
+
+    # configure and draw precision percentages
+    axActual.set_xticks([0])
+    axActual.set_yticks(ticks)
+    axActual.set_ylabel('Precision', fontsize='large')
+    axActual.yaxis.set_label_position('right')
+    axActual.imshow(
+        correctpredicted,
+        interpolation='nearest',
+        cmap='RdYlGn',
+        aspect='auto',
+        vmin=0.0,
+        vmax=1.0
+    )
+    for i in range(len(class_names)):
+        txt = axActual.text(
+            0, i,
+            '100' if correctpredicted[i, 0] == 1.0 else f'{100.0 * correctpredicted[i, 0]:3.1f}',
+            ha='center',
+            va='center',
+            color='black',
+            fontsize='medium')
+        txt.set_path_effects([
+            patheffects.withStroke(linewidth=5, foreground='w')
+        ])
+
+    # configure and draw total accuracy
+    axTotal.set_xticks([0])
+    axTotal.set_yticks([0])
+    axTotal.set_xlabel('Accuracy', fontsize='large')
+    axTotal.imshow(
+        np.array([[accuracy]]),
+        interpolation='nearest',
+        cmap='RdYlGn',
+        aspect='auto',
+        vmin=0.0,
+        vmax=1.0
+    )
+    txt = axTotal.text(
+        0, 0,
+        f'{100 * accuracy:3.1f}',
+        ha='center',
+        va='center',
+        color='black',
+        fontsize='medium'
+    )
+    txt.set_path_effects([
+        patheffects.withStroke(linewidth=5, foreground='w')
+    ])
+
+    # disable axes for other matrices than confusion matrix
+    for a in (axPredicted, axActual, axTotal):
+        plt.setp(a.get_yticklabels(), visible=False)
+        plt.setp(a.get_xticklabels(), visible=False)
+
+    # draw colorbar for confusion matrix
+    cbar = fig.colorbar(img, ax=[axPredicted, axConfMatrix, axActual, axTotal], shrink=0.5, pad=0.1)
+    for t in cbar.ax.get_yticklabels():
+        t.set_fontsize('medium')
+    suptitlehandle = fig.suptitle(f'{title} (ACC={accuracy:.5f})', fontsize='xx-large')
+    if outpath is None:
+        plt.show()
+    else:
+        plt.savefig(outpath, dpi=dpi, bbox_inches='tight', bbox_extra_artists=[suptitlehandle], pad_inches=0.1)

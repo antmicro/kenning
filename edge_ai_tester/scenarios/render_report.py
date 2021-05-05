afb0d01b501e24bf3f286c0fba098f336cba0027
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-05-05 21:22:38 +0200

    [#25865] Added providing docs root directory to generated reports
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- edge_ai_tester/scenarios/render_report.py
+++ edge_ai_tester/scenarios/render_report.py
@@ -9,7 +9,7 @@ It requires providing the report type and JSON file to extract data from.
 import sys
 import argparse
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 import json
 if sys.version_info.minor < 9:
     from importlib_resources import path
@@ -21,6 +21,7 @@ from edge_ai_tester.core.drawing import time_series_plot
 from edge_ai_tester.core.drawing import draw_confusion_matrix
 from edge_ai_tester.utils import logger
 from edge_ai_tester.core.report import create_report_from_measurements
+import numpy as np
 
 
 log = logger.get_logger()
@@ -30,15 +31,19 @@ def performance_report(
         reportname: str,
         measurementsdata: Dict[str, List],
         imgdir: Path,
-        reportpath: Path):
+        reportpath: Path,
+        rootdir: Optional[Path] = None):
     """
     Creates performance section of the report.
     """
     log.info('Running performance_report')
 
+    if rootdir is None:
+        rootdir = reportpath.parent
+
     if 'target_inference_step' in measurementsdata:
         log.info('Using target measurements for inference time')
-        usepath = imgdir / f'{reportname}_inference_time.png'
+        usepath = imgdir / f'{reportpath.stem}_inference_time.png'
         time_series_plot(
             str(usepath),
             f'Inference time for {reportname}',
@@ -48,13 +53,13 @@ def performance_report(
             measurementsdata['target_inference_step'],
             skipfirst=True)
         measurementsdata['inferencetimepath'] = str(
-            usepath.relative_to(reportpath.parent)
+            usepath.relative_to(rootdir)
         )
         measurementsdata['inferencetime'] = \
             measurementsdata['target_inference_step']
     elif 'protocol_inference_step' in measurementsdata:
         log.info('Using protocol measurements for inference time')
-        usepath = imgdir / f'{reportname}_inference_time.png'
+        usepath = imgdir / f'{reportpath.stem}_inference_time.png'
         time_series_plot(
             str(usepath),
             f'Inference time for {reportname}',
@@ -64,7 +69,7 @@ def performance_report(
             measurementsdata['protocol_inference_step'],
             skipfirst=True)
         measurementsdata['inferencetimepath'] = str(
-            usepath.relative_to(reportpath.parent)
+            usepath.relative_to(rootdir)
         )
         measurementsdata['inferencetime'] = \
             measurementsdata['protocol_inference_step']
@@ -73,7 +78,7 @@ def performance_report(
 
     if 'session_utilization_mem_percent' in measurementsdata:
         log.info('Using target measurements memory usage percentage')
-        usepath = imgdir / f'{reportname}_cpu_memory_usage.png'
+        usepath = imgdir / f'{reportpath.stem}_cpu_memory_usage.png'
         time_series_plot(
             str(usepath),
             f'Memory usage for {reportname}',
@@ -82,14 +87,14 @@ def performance_report(
             measurementsdata['session_utilization_timestamp'],
             measurementsdata['session_utilization_mem_percent'])
         measurementsdata['memusagepath'] = str(
-            usepath.relative_to(reportpath.parent)
+            usepath.relative_to(rootdir)
         )
     else:
         log.warning('No memory usage measurements in the report')
 
     if 'session_utilization_gpu_mem_utilization' in measurementsdata:
         log.info('Using target measurements GPU memory usage percentage')
-        usepath = imgdir / f'{reportname}_gpu_memory_usage.png'
+        usepath = imgdir / f'{reportpath.stem}_gpu_memory_usage.png'
         time_series_plot(
             str(usepath),
             f'GPU memory usage for {reportname}',
@@ -98,14 +103,14 @@ def performance_report(
             measurementsdata['session_utilization_gpu_timestamp'],
             measurementsdata['session_utilization_gpu_mem_utilization'])
         measurementsdata['gpumemusagepath'] = str(
-            usepath.relative_to(reportpath.parent)
+            usepath.relative_to(rootdir)
         )
     else:
         log.warning('No GPU memory usage measurements in the report')
 
     if 'session_utilization_gpu_utilization' in measurementsdata:
         log.info('Using target measurements GPU utilization')
-        usepath = imgdir / f'{reportname}_gpu_usage.png'
+        usepath = imgdir / f'{reportpath.stem}_gpu_usage.png'
         time_series_plot(
             str(usepath),
             f'GPU Utilization for {reportname}',
@@ -114,7 +119,7 @@ def performance_report(
             measurementsdata['session_utilization_gpu_timestamp'],
             measurementsdata['session_utilization_gpu_utilization'])
         measurementsdata['gpuusagepath'] = str(
-            usepath.relative_to(reportpath.parent)
+            usepath.relative_to(rootdir)
         )
     else:
         log.warning('No GPU memory usage measurements in the report')
@@ -130,17 +135,21 @@ def classification_report(
         reportname: str,
         measurementsdata: Dict[str, List],
         imgdir: Path,
-        reportpath: Path):
+        reportpath: Path,
+        rootdir: Optional[Path] = None):
     """
     Creates classification quality section of the report.
     """
     log.info('Running classification report')
 
+    if rootdir is None:
+        rootdir = reportpath.parent
+
     if 'eval_confusion_matrix' not in measurementsdata:
         log.error('Confusion matrix not present for classification report')
         return ''
     log.info('Using confusion matrix')
-    confusionpath = imgdir / f'{reportname}_confusion_matrix.png'
+    confusionpath = imgdir / f'{reportpath.stem}_confusion_matrix.png'
     draw_confusion_matrix(
         measurementsdata['eval_confusion_matrix'],
         str(confusionpath),
@@ -148,7 +157,7 @@ def classification_report(
         measurementsdata['class_names']
     )
     measurementsdata['confusionpath'] = str(
-        confusionpath.relative_to(reportpath.parent)
+        confusionpath.relative_to(rootdir)
     )
     with path(reports, 'classification.rst') as reporttemplate:
         return create_report_from_measurements(
@@ -162,7 +171,8 @@ def generate_report(
         data: Dict,
         outputpath: Path,
         imgdir: Path,
-        report_types: List[str]):
+        report_types: List[str],
+        rootdir: Optional[Path]):
     reptypes = {
         'performance': performance_report,
         'classification': classification_report
@@ -171,7 +181,7 @@ def generate_report(
     content = ''
     data['reportname'] = [reportname]
     for typ in report_types:
-        content += reptypes[typ](reportname, data, imgdir, outputpath)
+        content += reptypes[typ](reportname, data, imgdir, outputpath, rootdir)
 
     with open(outputpath, 'w') as out:
         out.write(content)
@@ -193,6 +203,12 @@ def main(argv):
         'output',
         help='Path to the output RST file',
         type=Path
+    )
+    parser.add_argument(
+        '--root-dir',
+        help='Path to root directory for documentation (paths in RST files are in respect to this directory)',  # noqa: E501
+        type=Path,
+        default=None
     )
     parser.add_argument(
         '--report-types',
@@ -228,7 +244,8 @@ def main(argv):
         measurementsdata,
         args.output,
         args.img_dir,
-        args.report_types
+        args.report_types,
+        args.root_dir
     )
 
 

93cca4f74c80293ce125ca23be32dcb771a06fae
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-06-02 19:51:56 +0200

    [#25709] Added AP and mAP metrics to Open Images Dataset
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- edge_ai_tester/datasets/open_images_dataset.py
+++ edge_ai_tester/datasets/open_images_dataset.py
@@ -178,6 +178,87 @@ def download_all_images(
             progress_bar.update(1)
     progress_bar.close()
 
+
+def compute_ap11(
+        recall: List[float],
+        precision: List[float]) -> float:
+    """
+    Computes 11-point Average Precision for a single class.
+
+    Parameters
+    ----------
+    recall : List[float]
+        List of recall values
+    precision : List[float]
+        List of precision values
+
+    Returns
+    -------
+    float: 11-point interpolated average precision value
+    """
+    if len(recall) == 0:
+        return 0
+    return np.sum(np.interp(np.arange(0, 1.1, 0.1), recall, precision)) / 11
+
+
+def get_recall_precision(
+        measurementsdata: Dict,
+        scorethresh: float = 0.5) -> List[Tuple[List[float], List[float]]]:
+    """
+    Computes recall and precision values at a given objectness threshold.
+
+    Parameters
+    ----------
+    measurementsdata : Dict
+        Data from Measurements object with eval_gcount/{cls} and eval_det/{cls}
+        fields containing number of ground truths and per-class detections
+    scorethresh : float
+        Minimal objectness score threshold for detections
+
+    Returns
+    -------
+    List[Tuple[List[float], List[float]]] : List with per-class lists of recall
+    and precision values
+    """
+    lines = []
+    for cls in measurementsdata['class_names']:
+        gt_count = measurementsdata[f'eval_gtcount/{cls}'] if f'eval_gtcount/{cls}' in measurementsdata else []
+        dets = measurementsdata[f'eval_det/{cls}'] if f'eval_det/{cls}' in measurementsdata else []
+        dets = [d for d in dets if d[0] >= scorethresh]
+        dets.sort(reverse=True, key=lambda x: x[0])
+        truepositives = np.array([entry[1] for entry in dets])
+        cumsum = np.cumsum(truepositives)
+        precision = cumsum / np.arange(1, len(cumsum) + 1)
+        recall = cumsum / gt_count
+        lines.append([recall, precision])
+    return lines
+
+
+def compute_map_per_threshold(
+        measurementsdata: Dict,
+        scorethresholds: List[float]) -> List[float]:
+    """
+    Computes mAP values depending on the objectness threshold.
+
+    Parameters
+    ----------
+    measurementsdata : Dict
+        Data from Measurements object with eval_gcount/{cls} and eval_det/{cls}
+        fields containing number of ground truths and per-class detections
+    scorethresholds : List[float]
+        List of threshold values to verify the mAP for
+    """
+    maps = []
+    for thresh in scorethresholds:
+        lines = get_recall_precision(measurementsdata, thresh)
+        aps = []
+        for line in lines:
+            aps.append(compute_ap11(line[0], line[1]))
+        maps.append(np.mean(aps))
+
+    return np.array(maps, dtype=np.float32)
+
+
 class OpenImagesDatasetV6(Dataset):
     """
     The Open Images Dataset V6
@@ -392,7 +473,21 @@ class OpenImagesDatasetV6(Dataset):
             result.append(npimg)
         return result
 
-    def compute_iou(self, b1: DectObject, b2: DectObject):
+    def compute_iou(self, b1: DectObject, b2: DectObject) -> float:
+        """
+        Computes the IoU between two bounding boxes.
+
+        Parameters
+        ----------
+        b1 : DectObject
+            First bounding box
+        b2 : DectObject
+            Second bounding box
+
+        Returns
+        -------
+        float : IoU value
+        """
         xmn = max(b1.xmin, b2.xmin)
         ymn = max(b1.ymin, b2.ymin)
         xmx = min(b1.xmax, b2.xmax)

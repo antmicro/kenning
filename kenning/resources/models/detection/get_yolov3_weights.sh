f9915757e5fafdb8bac0dbc73a96e41f0f41f6ad
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-06-10 13:49:28 +0200

    get_yolov3_weights.sh: fixed script so it downloads weights to containing directory
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- kenning/resources/models/detection/get_yolov3_weights.sh
+++ kenning/resources/models/detection/get_yolov3_weights.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-wget https://pjreddie.com/media/files/yolov3.weights
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+wget https://pjreddie.com/media/files/yolov3.weights -O $SCRIPT_DIR/yolov3.weights

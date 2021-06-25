deba89ae26c2e7e3b8109aea704415895e863bae
Grzegorz Latosinski <glatosinski@antmicro.com>
2021-06-25 19:00:42 +0200

    Updated setup.py requirements
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -9,7 +9,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     long_description=long_description,
     include_package_data=True,
-    description="Deep Learning Frameworks' Analyzer",
+    description="Kenning - a framework for implementing and testing deployment pipelines for deep learning applications on edge devices",  # noqa: E501
     author='Antmicro Ltd.',
     author_email='contact@antmicro.com',
     classifiers=[
@@ -18,20 +18,42 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'numpy>=1.20.0',
-        'psutil>=5.8.0',
-        'tensorflow>=2.4.1',
-        'tensorflow_addons>=0.12.1',
-        'onnx_tf>=1.7.0',
-        'tf2onnx>=1.8.3',
-        'Pillow>=8.1.0',
-        'scikit_learn>=0.24.1',
         'Jinja2>=2.11.2',
+        'Pillow>=8.1.0',
         'matplotlib>=3.3.4',
-        'pynvml>=8.0.4',
-        'tqdm>=4.56.2',
+        'numpy>=1.20.0',
         'onnx>=1.7.0',
-        'torchvision>=0.8.2',
-        'torch>=1.7.1',
+        'psutil>=5.8.0',
+        'scikit_learn>=0.24.1',
+        'sphinx_antmicro_theme @ git+https://github.com/antmicro/sphinx_antmicro_theme.git#egg=sphinx_antmicro_theme',  # noqa: E501
+        'tqdm>=4.56.2',
     ],
+    extras_require={
+        'tensorflow': [
+            'onnx_tf>=1.7.0',
+            'tensorflow>=2.4.1',
+            'tensorflow_addons>=0.12.1',
+            'tf2onnx>=1.8.3'
+            ],
+        'torch': [
+            'torch>=1.7.1'
+            'torchvision>=0.8.2',
+        ],
+        'mxnet': [
+            'gluoncv>=0.10.2',
+            'mxnet>=1.8.0',
+        ],
+        'nvidia_perf': [
+            'pynvml>=8.0.4',
+        ],
+        'object_detection': [
+            'boto3>=1.17.5',
+            'botocore>=1.20.5',
+            'opencv_python>=4.5.2',
+            'pandas>=1.2.1',
+        ],
+        ":python_version<'3.9'": [
+            'importlib_resources>=5.1.4'
+        ],
+    },
 )

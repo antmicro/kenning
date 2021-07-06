0cd573adf9dbb44aeb50cc9ffb63825028b38e6e
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-06 17:33:33 +0200

    [#27195] add "__main__" condition and argparse
    add the "__main__" condition and argument parser to demonstation scenario
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/scenarios/demos/open-images-v6-instance-segmentation-demo.py
+++ kenning/scenarios/demos/open-images-v6-instance-segmentation-demo.py
@@ -4,34 +4,38 @@ This program was used to test and debug the process of
 downloading and preparation of the data from
 OpenImagesV6 dataset for instance segmentation problem
 
+Run:
+
+$ python open-images-v6-instance-segmentation-demo.py \
+    --task='instance_segmentation' \
+    --dataset-root="./demo-data" \
+    --download-dataset \
+    --download-num-bboxes-per-class=3 \
+    --image-memory-layout="NHWC" \
+
 """
 
 from kenning.datasets.open_images_dataset import OpenImagesDatasetV6
 import numpy as np
 import cv2
 
-ds = OpenImagesDatasetV6(
-                        "./data",
-                        1,
-                        True,
-                        "instance_segmentation",
-                        'coco',
-                        3,
-                        'validation',
-                        "NHWC")
+if __name__ == '__main__':
+    parser, group = OpenImagesDatasetV6.form_argparse()
+    args = parser.parse_args()
+    ds = OpenImagesDatasetV6.from_argparse(args)
 
-result_input = ds.prepare_input_samples(ds.dataX)
-result_output = ds.prepare_output_samples(ds.dataY)
+    result_input = ds.prepare_input_samples(ds.dataX)
+    result_output = ds.prepare_output_samples(ds.dataY)
 
-iteration = 0
-for i, batch_j in zip(result_input, result_output):
-    for j in batch_j:
-        int_i = np.multiply(i, 255).astype('uint8')
-        out_array = cv2.bitwise_and(int_i, int_i, mask=j.mask)
-        out_array = cv2.cvtColor(out_array, cv2.COLOR_BGR2RGB)
+    iteration = 0
+    for i, batch_j in zip(result_input, result_output):
+        for j in batch_j:
+            int_i = np.multiply(i, 255).astype('uint8')
+            out_array = cv2.bitwise_and(int_i, int_i, mask=j.mask)
+            out_array = cv2.cvtColor(out_array, cv2.COLOR_BGR2RGB)
 
-        out_path = ds.root / 'demo-img'
-        out_path.mkdir(parents=True, exist_ok=True)
+            out_path = ds.root / 'demo-img'
+            out_path.mkdir(parents=True, exist_ok=True)
 
-        cv2.imwrite(str(out_path / str(iteration))+".jpg", out_array)
-        iteration += 1
+            cv2.imwrite(str(out_path / str(iteration))+".jpg", out_array)
+            iteration += 1

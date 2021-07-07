a3631c05705a90b55f16592c80ec191761cdf6e7
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-07 12:47:30 +0200

    [#27197] pytorch_coco: Cleaned up formatting issues
    format code and delete obsolete comments
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/modelwrappers/detectors/instance_segmentation/pytorch_coco.py
+++ kenning/modelwrappers/detectors/instance_segmentation/pytorch_coco.py
@@ -8,8 +8,7 @@ import numpy as np
 from pathlib import Path
 
 import torch
-from torchvision import models, transforms
-from torch.utils.data import Dataset
+from torchvision import models
 
 from kenning.core.dataset import Dataset
 from kenning.modelwrappers.frameworks.pytorch import PyTorchWrapper
@@ -21,17 +20,18 @@ class PyTorchCOCOMaskRCNN(PyTorchWrapper):
         print(self.numclasses)
         super().__init__(modelpath, dataset, from_file)
 
-    def prepare_input_data(self,data : np.array):
+    def prepare_input_data(self, data: np.array):
         print(np.shape(data))
         return torch.Tensor(data)
 
     def prepare_model(self):
         self.model = models.detection.maskrcnn_resnet50_fpn(
-            pretrained=True, 
-            progress=True, 
-            num_classes=91, # TODO: check why it crashes upon changing it to something else
+            pretrained=True,
+            progress=True,
+            num_classes=91,
             pretrained_backbone=True
         )
-    def run_model(self, torch_data : torch.Tensor):
+
+    def run_model(self, torch_data: torch.Tensor):
         self.model.eval()
         return self.model([torch_data])

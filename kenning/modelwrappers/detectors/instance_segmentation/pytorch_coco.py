cc986f8e60f87e6be1994c06856366c1192de7ad
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-07 16:54:49 +0200

    [#27197] pytorch_coco: Added bboxes to outputs
    add bbox data to model output parser
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/modelwrappers/detectors/instance_segmentation/pytorch_coco.py
+++ kenning/modelwrappers/detectors/instance_segmentation/pytorch_coco.py
@@ -17,12 +17,12 @@ from kenning.modelwrappers.frameworks.pytorch import PyTorchWrapper
 
 class PyTorchCOCOMaskRCNN(PyTorchWrapper):
     def __init__(self, modelpath: Path, dataset: Dataset, from_file=True):
+        self.threshold = 0.7
         self.numclasses = dataset.numclasses
         print(self.numclasses)
         super().__init__(modelpath, dataset, from_file)
 
     def prepare_input_data(self, data: np.array):
-        print(np.shape(data))
         return torch.Tensor(data)
 
     def prepare_model(self):
@@ -32,22 +32,53 @@ class PyTorchCOCOMaskRCNN(PyTorchWrapper):
             num_classes=91,
             pretrained_backbone=True
         )
+        self.custom_classnames = [
+            '__background__', 'person', 'bicycle',
+            'car', 'motorcycle', 'airplane', 'bus',
+            'train', 'truck', 'boat', 'traffic light',
+            'fire hydrant', 'N/A', 'stop sign',
+            'parking meter', 'bench', 'bird', 'cat',
+            'dog', 'horse', 'sheep', 'cow',
+            'elephant', 'bear', 'zebra', 'giraffe', 'N/A',
+            'backpack', 'umbrella', 'N/A', 'N/A',
+            'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+            'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard',
+            'surfboard', 'tennis racket',
+            'bottle', 'N/A', 'wine glass', 'cup', 'fork',
+            'knife', 'spoon', 'bowl',
+            'banana', 'apple', 'sandwich', 'orange', 'broccoli',
+            'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant',
+            'bed', 'N/A', 'dining table',
+            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse',
+            'remote', 'keyboard', 'cell phone',
+            'microwave', 'oven', 'toaster', 'sink', 'refrigerator',
+            'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+            'hair drier', 'toothbrush'
+        ]
 
     def run_model(self, torch_data: torch.Tensor):
         self.model.eval()
         return self.model([torch_data])
 
-    def parse_output(self,out: dict) -> list[SegmObject]:
+    def parse_output(self, out: dict) -> list[SegmObject]:
         ret = []
         for i in range(len(out['labels'])):
-            ret.append(SegmObject(
-                clsname=self.dataset.classnames[int(out['labels'][i])],
-                maskpath=None,
-                xmin=None,
-                ymin=None,
-                xmax=None,
-                ymax=None,
-                mask=out['masks'][i].detach().numpy().transpose(1,2,0),
-                score=1.0
-            ))
+            if float(out['scores'][i]) > self.threshold:
+                ret.append(SegmObject(
+                    clsname=self.custom_classnames[int(out['labels'][i])],
+                    maskpath=None,
+                    xmin=float(out['boxes'][i][0]),
+                    ymin=float(out['boxes'][i][1]),
+                    xmax=float(out['boxes'][i][2]),
+                    ymax=float(out['boxes'][i][3]),
+                    mask=np.multiply(
+                        self.postprocess_outputs(
+                            out['masks'][i]
+                        ).transpose(1, 2, 0),
+                        255
+                    ).astype('uint8'),
+                    score=1.0
+                ))
         return ret

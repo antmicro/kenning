869751db826d53b532101e1a93d7ef1a6ee8da88
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-09 11:18:20 +0200

    [#27197] pytorch_coco: minor fixes in class names
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/modelwrappers/instance_segmentation/pytorch_coco.py
+++ kenning/modelwrappers/instance_segmentation/pytorch_coco.py
@@ -13,6 +13,8 @@ from kenning.core.dataset import Dataset
 from kenning.datasets.open_images_dataset import SegmObject
 from kenning.modelwrappers.frameworks.pytorch import PyTorchWrapper
 
+from kenning.resources.coco_instance_segmentation.pytorch_coco_classnames import pytorch_coco_classnames  # noqa: E501
+
 
 class PyTorchCOCOMaskRCNN(PyTorchWrapper):
     def __init__(self, modelpath: Path, dataset: Dataset, from_file=False):
@@ -38,31 +40,7 @@ class PyTorchCOCOMaskRCNN(PyTorchWrapper):
             )
         self.model.to(self.device)
         self.model.eval()
-        self.custom_classnames = [
-            '__background__', 'person', 'bicycle',
-            'car', 'motorcycle', 'airplane', 'bus',
-            'train', 'truck', 'boat', 'traffic light',
-            'fire hydrant', 'N/A', 'stop sign',
-            'parking meter', 'bench', 'bird', 'cat',
-            'dog', 'horse', 'sheep', 'cow',
-            'elephant', 'bear', 'zebra', 'giraffe', 'N/A',
-            'backpack', 'umbrella', 'N/A', 'N/A',
-            'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-            'snowboard', 'sports ball',
-            'kite', 'baseball bat', 'baseball glove', 'skateboard',
-            'surfboard', 'tennis racket',
-            'bottle', 'N/A', 'wine glass', 'cup', 'fork',
-            'knife', 'spoon', 'bowl',
-            'banana', 'apple', 'sandwich', 'orange', 'broccoli',
-            'carrot', 'hot dog', 'pizza',
-            'donut', 'cake', 'chair', 'couch', 'potted plant',
-            'bed', 'N/A', 'dining table',
-            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse',
-            'remote', 'keyboard', 'cell phone',
-            'microwave', 'oven', 'toaster', 'sink', 'refrigerator',
-            'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-            'hair drier', 'toothbrush'
-        ]
+        self.custom_classnames = pytorch_coco_classnames
 
     def postprocess_outputs(self, out_all: list[dict]) -> list[SegmObject]:
         ret = []

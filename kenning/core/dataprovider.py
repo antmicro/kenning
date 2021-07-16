e16fc9429c9e85ff9e343d9d35b2373bdbf37316
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-16 13:54:58 +0200

    [#27517] Added core Dataprovider class
    add core/Dataprovider class with basic function templates that will
    occur in implementations
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

---
+++ kenning/core/dataprovider.py
@@ -0,0 +1,90 @@
+"""
+Provides an API for gathering and preparing data from external sources
+"""
+
+from typing import Any
+import argparse
+
+
+class Dataprovider(object):
+    def __init__(self):
+        """
+        Initializes dataprovider object.
+
+        Parameters
+        ----------
+
+        """
+        self.device = None
+        self.prepare()
+
+    @classmethod
+    def form_argparse(cls):
+        """
+        Creates argparse parser for the Dataprovider object.
+
+        This method is used to create a list of arguments for the object so
+        it is possible to configure the object from the level of command
+        line.
+
+        Returns
+        -------
+        (ArgumentParser, ArgumentGroup) :
+            tuple with the argument parser object that can act as parent for
+            program's argument parser, and the corresponding arguments' group
+            pointer
+        """
+        parser = argparse.ArgumentParser(add_help=False)
+        group = parser.add_argument_group(title='Dataprovider arguments')
+
+        return parser, group
+
+    @classmethod
+    def from_argparse(cls, args):
+        """
+        Constructor wrapper that takes the parameters from argparse args.
+
+        This method takes the arguments created in form_argparse and uses them
+        to create the object.
+
+        Parameters
+        ----------
+        args : Dict
+            arguments from ArgumentParser object
+
+        Returns
+        -------
+        Dataprovider : object of class Dataprovider
+        """
+        return cls()
+
+    def prepare(self):
+        """
+        Prepares the source for data gathering depending on the
+        source type.
+
+        This will for example initialize the camera and
+        set the self.device to it
+        """
+        raise NotImplementedError
+
+    def get_input(self):
+        """
+        Gets the sample from device
+        """
+        raise NotImplementedError
+
+    def preprocess_input(self, data: Any) -> Any:
+        """
+        Processes the input for inference
+
+        Parameters
+        ----------
+        data : Any
+            the data to be preprocessed
+
+        Returns
+        -------
+        Any : preprocessed data
+        """
+        return self.data

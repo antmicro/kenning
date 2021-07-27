c64af52fe38f565e32e3e150ea32c32b23b2b93a
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-27 10:31:29 +0200

    [#27559] name_printer: Made NamePrinter accept more inputs
    make the nameprinter handle FloatTensors from pytorch_pet_dataset since
    it is the only model currently Wrapped that has implemented the
    converting from bytes method.
    
    Added an argument for the type of what is being printed
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/outputcollectors/name_printer.py
+++ kenning/outputcollectors/name_printer.py
@@ -4,13 +4,30 @@ handling of multiple OutputCollectors in inference_runner scenario
 """
 from kenning.core.outputcollector import OutputCollector
 from kenning.datasets.open_images_dataset import DectObject
-from typing import Any
+from typing import Any, Union
+from torch import FloatTensor
 
 
 class NamePrinter(OutputCollector):
-    def __init__(self):
+    def __init__(self, print_type: str = "detctor"):
         self.frame_counter = 0
+        self.print_type = print_type
         super().__init__()
+
+    @classmethod
+    def form_argparse(cls):
+        parser, group = super().form_argparse()
+        group.add_argument(
+            '--print-type',
+            help='What is the type of model that will input data to the NamePrinter',  # noqa: E501
+            choices=['detector', 'classificator'],
+            default='detector'
+        )
+        return parser, group
+
+    @classmethod
+    def from_argparse(cls, args):
+        return cls(args.print_type)
 
     def detach_from_output(self):
         pass
@@ -18,10 +35,15 @@ class NamePrinter(OutputCollector):
     def should_close(self):
         return False
 
-    def process_output(self, i: Any, o: DectObject):
-        o = o[0]
+    def process_output(self, i: Any, o: Union[DectObject, FloatTensor]):
         print("Frame", self.frame_counter, end=": ")
-        self.frame_counter += 1
-        for x in o:
-            print(x.clsname, end=" ")
-        print()
+        if self.print_type == 'detector':
+            o = o[0]
+            self.frame_counter += 1
+            for x in o:
+                print(x.clsname, end=" ")
+            print()
+        elif self.print_type == 'classificator':
+            for i in o:
+                print(float(i), end=', ')
+            print()

c4f94e7f8048e9fc7b460a22b9c3f50d94991622
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-07-29 17:11:49 +0200

    [#27746] name_printer: Modified name_printer for classifier data
    modify the name_printer outputcollector to work with classification
    models and optionally load class names (that are not necessary for
    correct function, sequential option names are generated instead)
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- kenning/outputcollectors/name_printer.py
+++ kenning/outputcollectors/name_printer.py
@@ -5,13 +5,19 @@ handling of multiple OutputCollectors in inference_runner scenario
 from kenning.core.outputcollector import OutputCollector
 from kenning.datasets.open_images_dataset import DectObject
 from typing import Any, Union
-from torch import FloatTensor
+import numpy as np
+from pathlib import Path
 
 
 class NamePrinter(OutputCollector):
-    def __init__(self, print_type: str = "detector"):
+    def __init__(self, print_type: str = "detector", file_path: Path = None):
         self.frame_counter = 0
         self.print_type = print_type
+        self.classnames = []
+        if file_path:
+            with open(file_path, 'r') as f:
+                for line in f:
+                    self.classnames.append(line.strip())
         super().__init__()
 
     @classmethod
@@ -23,11 +29,16 @@ class NamePrinter(OutputCollector):
             choices=['detector', 'classificator'],
             default='detector'
         )
+        group.add_argument(
+            '--classification-class-names',
+            help='File with class names used to identify the output from classification models',  # noqa: E501
+            type=Path
+        )
         return parser, group
 
     @classmethod
     def from_argparse(cls, args):
-        return cls(args.print_type)
+        return cls(args.print_type, args.classification_class_names)
 
     def detach_from_output(self):
         pass
@@ -35,15 +46,31 @@ class NamePrinter(OutputCollector):
     def should_close(self):
         return False
 
-    def process_output(self, i: Any, o: Union[DectObject, FloatTensor]):
+    def process_output(self, i: Any, o: Union[DectObject, np.array]):
         print("Frame", self.frame_counter, end=": ")
+        o = o[0]
         if self.print_type == 'detector':
-            o = o[0]
-            self.frame_counter += 1
             for x in o:
                 print(x.clsname, end=" ")
             print()
         elif self.print_type == 'classificator':
-            for i in o:
-                print(float(i), end=', ')
+            tuples = []
+            if self.classnames:
+                for i, j in zip(o, self.classnames):
+                    tuples.append((i, j))
+            else:
+                it = 0
+                for i in o:
+                    tuples.append((i, "object {}".format(it)))
+                    it += 1
+            tuples.sort(key=lambda x: x[0], reverse=True)
+            for i in range(min(5, len(tuples))):
+                print(
+                    "{}: {:.2f}".format(
+                        tuples[i][1],
+                        float(tuples[i][0])
+                    ),
+                    end=', '
+                )
             print()
+        self.frame_counter += 1

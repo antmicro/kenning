999cc7480140aa25aaa5ed081990ba556afd4bd4
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-04-26 12:03:15 +0200

    [#33502] runtimes: Added support for quantized model in tvm runtime
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- kenning/runtimes/tvm.py
+++ kenning/runtimes/tvm.py
@@ -14,6 +14,7 @@ from tvm.runtime.vm import VirtualMachine, Executable
 
 from kenning.core.runtime import Runtime
 from kenning.core.runtimeprotocol import RuntimeProtocol
+from kenning.core.runtimeprotocol import MessageType
 
 
 class TVMRuntime(Runtime):
@@ -61,6 +62,8 @@ class TVMRuntime(Runtime):
         self.use_json_out = use_json_out
         self.io_details_path = io_details_path
         super().__init__(protocol)
+        self.callbacks[MessageType.QUANTIZATION] = \
+            self._prepare_quantization_details
 
     @classmethod
     def form_argparse(cls):
@@ -101,7 +104,8 @@ class TVMRuntime(Runtime):
         )
         group.add_argument(
             '--io-details-path',
-            help="Path where the quantization details are saved in json.",
+            help="Path where the quantization details are saved in json. \
+                By default save_model_path.quantparams is checked",
             type=Path,
             required=False
         )
@@ -119,6 +123,97 @@ class TVMRuntime(Runtime):
             args.use_json_at_output,
             args.io_details_path
         )
+
+    def upload_essentials(self, compiledmodelpath):
+        super().upload_essentials(compiledmodelpath)
+        self.upload_quantization_details(compiledmodelpath)
+
+    def prepare_local(self):
+        super().prepare_local()
+        self.prepare_quantization_details(None)
+
+    def _get_quantization_details_path(
+            self,
+            modelpath: Path) -> Path:
+        """
+        Gets path to a preferred quantization details file.
+
+        If ``self.io_details_path`` is not specified, then the preferred path
+        is ``self.modelpath`` with '.quantparams' suffix.
+
+        Parameters
+        ----------
+        modelpath : Path
+            Path to the compiled model
+
+        Returns
+        -------
+        Path : Returns preferred path to a quantization details file
+        """
+        if self.io_details_path:
+            path = self.io_details_path
+        else:
+            name = modelpath.stem.split('.')[0]
+            parent = modelpath.parent
+            path = parent.joinpath(name).with_suffix('.quantparams')
+
+        return path
+
+    def _prepare_quantization_details(
+            self,
+            input_data: Optional[bytes]) -> bool:
+        """
+        Wrapper for preparing quantization details.
+
+        Parameters
+        ----------
+        input_data : Optional[bytes]
+            Quantization details data or None, if the data should be loaded
+            from another source.
+
+        Returns
+        -------
+        bool : True if there is no data to send or if succeded
+        """
+        ret = self.prepare_quantization_details(input_data)
+        if ret:
+            self.protocol.request_success()
+        else:
+            self.protocol.request_failure()
+        return ret
+
+    def prepare_quantization_details(self, input_data):
+        if input_data:
+            self.input_details, self.output_details = json.loads(input_data)
+        else:
+            path = self._get_quantization_details_path(self.modelpath)
+
+            if not path.exists():
+                if self.io_details_path:
+                    self.log.info(
+                        f'Could not find specified quantization details \
+                        file {path}'
+                    )
+                    return False
+
+                # The path was not specified by the user
+                # and there is no file in the preferred path
+                return True
+
+            with open(path, 'rb') as f:
+                self.input_details, self.output_details = json.load(f)
+
+        self.input_details = self.input_details[0]
+        self.output_details = self.output_details[0]
+        self.model_inputdtype = self.input_details['dtype']
+        self.log.info('Quantization details loaded')
+        return True
+
+    def upload_quantization_details(self, compiledmodelpath):
+        path = self._get_quantization_details_path(compiledmodelpath)
+
+        if path.exists():
+            self.protocol.upload_quantization_details(path)
 
     def prepare_input(self, input_data):
         self.log.debug(f'Preparing inputs of size {len(input_data)}')
@@ -150,24 +245,6 @@ class TVMRuntime(Runtime):
 
     def prepare_model(self, input_data):
         self.log.info('Loading model')
-
-        if not input_data:
-            if self.io_details_path:
-                path = self.io_details_path
-            else:
-                name = self.modelpath.stem.split('.')[0]
-                parent = self.modelpath.parent
-                path = parent.joinpath(name).with_suffix('.quantparams')
-            try:
-                with open(path, 'rb') as f:
-                    # Assumption that model is tensor quantized
-                    # ie. input and output have only one item
-                    self.input_details, self.output_details = json.load(f)
-                self.input_details = self.input_details[0]
-                self.output_details = self.output_details[0]
-                self.model_inputdtype = self.input_details['dtype']
-            except FileNotFoundError:
-                pass
 
         if self.use_tvm_vm:
             self.module = tvm.runtime.load_module(str(self.modelpath)+'.so')

2bbca4c4a7826eb35f17c7848d394944a566f5e8
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-05-13 15:45:18 +0200

    [#33881] utils: Added helper functions for adding and parsing arguments
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- kenning/utils/args_manager.py
+++ kenning/utils/args_manager.py
@@ -3,6 +3,9 @@ Module for preparing and serializing class arguments.
 """
 
 import json
+import jsonschema
+import argparse
+from typing import Dict
 from pathlib import Path
 
 
@@ -43,3 +46,163 @@ def serialize(obj: object) -> str:
             return json.JSONEncoder.default(self, obj)
 
     return json.dumps(serialized_dict, cls=ArgumentEncoder)
+
+
+"""
+arguments_structure is a mapping (argument_name -> keywords)
+
+Supported keywords:
+argparse_name: Name that is prompted as an argparse argument.
+description: Description of the argument.
+type: Same as 'type' in argparse. The argument is converted to this value.
+default: Default value of the argument.
+required: Defines whether argument is required.
+enum: List of possible values of the argument.
+
+Example of an argument:
+'compiled_model_path': {
+    'argparse_name': '--compiled-model-path',
+    'description': 'The path to the compiled model output',
+    'type': Path,
+    'required': True,
+    'enum': ['/home/Documents', '/tmp']
+}
+"""
+
+
+def get_parsed_json_dict(schema, json_dict):
+    """
+    Validates the given dictionary with the schema.
+    Then it adds default values for missing
+    arguments and converts the arguments to the
+    appropriate types, as jsonschema can not do that.
+
+    Parameters
+    ----------
+    schema : Dict
+        Schema to validate with
+    json_dict : Dict
+        Dictionary to validate
+    """
+    jsonschema.validate(
+        instance=json_dict,
+        schema=schema
+    )
+
+    converted_json_dict = {}
+    for name, keywords in schema['properties'].items():
+        if name not in json_dict and 'default' in keywords:
+            converted_json_dict[name] = keywords['default']
+        elif name in json_dict:
+            converted_json_dict[name] = json_dict[name]
+
+    converted_json_dict = {
+        name: (
+            schema['properties'][name]['convert-type'](str(value))
+            if 'convert-type' in schema['properties'][name]
+            else value
+        )
+        for name, value in converted_json_dict.items()
+    }
+
+    return converted_json_dict
+
+
+def add_argparse_argument(
+        group: argparse._ArgumentGroup,
+        struct: Dict[str, Dict],
+        *names: str):
+    """
+    Adds arguments to the argparse group based on the given
+    struct and names. If no names are given it uses all
+    properties of the struct.
+
+    Note that the function modifies the given group.
+
+    Parameters
+    ----------
+    group : argparse._ArgumentGroup
+        Argparse group that is filled with new properties.
+    struct : Dict[str, Dict]
+        Struct with properties described.
+    *names : str
+        Names of the properties that are to be added to the group.
+        If empty every property in struct is used.
+    """
+    if not names:
+        names = struct.keys()
+
+    for name in names:
+        prop = struct[name]
+
+        argparse_name = prop['argparse_name']
+        arguments = {}
+
+        if 'type' in prop:
+            arguments['type'] = prop['type']
+        if 'description' in prop:
+            arguments['help'] = prop['description']
+        if 'default' in prop:
+            arguments['default'] = prop['default']
+        if 'required' in prop and prop['required']:
+            arguments['required'] = prop['required']
+        if 'enum' in prop:
+            arguments['choices'] = prop['enum']
+
+        group.add_argument(argparse_name, **arguments)
+
+
+def add_parameterschema_argument(
+        schema: Dict,
+        struct: Dict[str, Dict],
+        *names: str):
+    """
+    Adds arguments to the schema based on the given
+    struct and names. If no names are given it uses all
+    properties of the struct.
+
+    Note that the function modifies the given schema.
+
+    Parameters
+    ----------
+    schema : Dict
+        Schema that is filled with new properties.
+    struct : Dict[str, Dict]
+        Struct with properties described.
+    *names : str
+        Names of the properties that are to be added to the group.
+        If empty every property in struct is used.
+    """
+    if 'properties' not in schema:
+        schema['properties'] = {}
+
+    if not names:
+        names = struct.keys()
+
+    for name in names:
+        prop = struct[name]
+
+        schema['properties'][name] = {}
+        arguments = schema['properties'][name]
+
+        type_to_jsontype = {
+            Path: 'string',
+            str: 'string',
+            float: 'number',
+            int: 'number',
+            bool: 'boolean'
+        }
+
+        if 'type' in prop:
+            arguments['convert-type'] = prop['type']
+            arguments['type'] = type_to_jsontype[prop['type']]
+        if 'description' in prop:
+            arguments['description'] = prop['description']
+        if 'default' in prop:
+            arguments['default'] = prop['default']
+        if 'required' in prop and prop['required']:
+            if 'required' not in schema:
+                schema['required'] = []
+            schema['required'].append(name)
+        if 'enum' in prop:
+            arguments['enum'] = prop['enum']

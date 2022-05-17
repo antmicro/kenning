e58cb30f3daef13266bb7b1a0dfca15974392f94
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-05-17 15:47:20 +0200

    [#34054] datasets: pet_dataset: Add sketch of test two

--- kenning/datasets/tests/test_pet_dataset.py
+++ kenning/datasets/tests/test_pet_dataset.py
@@ -3,8 +3,19 @@ import pytest
 
 
 class TestPetDataset:
-    def test_two(self):
-        # Provide empty path
+    def test_one(self, tmp_path):
+        # Provide empty dir
+        empty_path = str(tmp_path.absolute())
         with pytest.raises(FileNotFoundError) as execinfo:
-            PetDataset("/tmp/existing/")
-        assert "No such file or directory: '/tmp/existing/annotations/list.txt'" in str(execinfo.value)
+            PetDataset(tmp_path)
+        error_message = f"No such file or directory: '{empty_path}/annotations/list.txt'"  # noqa: E501
+        assert error_message in str(execinfo.value)
+
+    def test_two(self, tmp_path):
+        # Provide dir with empty 'annotations/list.txt'
+        q = tmp_path / 'annotations'
+        q.mkdir()
+        q = q / 'list.txt'
+        q.touch()
+        dataset = PetDataset(tmp_path)
+        assert ([], []) == dataset.get_data()

d1daed833fe45f4ff49520e00a6be15176d0494e
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-05-18 10:31:04 +0200

    [#34054] datasets: pet_dataset: Add contstructor tests for PetDataset

--- kenning/datasets/tests/test_pet_dataset.py
+++ kenning/datasets/tests/test_pet_dataset.py
@@ -1,21 +1,46 @@
 from kenning.datasets.pet_dataset import PetDataset
 import pytest
+from string import printable
+from random import choices, randint
 
 
 class TestPetDataset:
     def test_one(self, tmp_path):
-        # Provide empty dir
-        empty_path = str(tmp_path.absolute())
+        # Test constructor
+        empty_path = f"'{str(tmp_path.absolute())}/annotations/list.txt'"
         with pytest.raises(FileNotFoundError) as execinfo:
             PetDataset(tmp_path)
-        error_message = f"No such file or directory: '{empty_path}/annotations/list.txt'"  # noqa: E501
+        error_message = f"No such file or directory: {empty_path}"
         assert error_message in str(execinfo.value)
 
-    def test_two(self, tmp_path):
+        with pytest.raises(AssertionError):
+            PetDataset(tmp_path, standardize=False, classify_by=random_string)
+
+        with pytest.raises(AssertionError):
+            PetDataset(tmp_path, standardize=False, image_memory_layout=random_string)  # noqa: E501
+
+    def test_two(self, empty_annotations):
         # Provide dir with empty 'annotations/list.txt'
-        q = tmp_path / 'annotations'
-        q.mkdir()
-        q = q / 'list.txt'
-        q.touch()
-        dataset = PetDataset(tmp_path)
+        dataset = PetDataset(empty_annotations)
         assert ([], []) == dataset.get_data()
+
+    def test_three(self):
+        # Provide dir with random data in 'annotations/list.txt'
+        pytest.fail(str(NotImplemented))
+
+    def test_four(self, empty_annotations):
+        # Provide dir with simple valid data
+        PetDataset(empty_annotations)
+        pytest.fail(str(NotImplemented))
+
+
+@pytest.fixture(scope="function")
+def empty_annotations(tmp_path):
+    (tmp_path / 'annotations').mkdir()
+    (tmp_path / 'annotations' / 'list.txt').touch()
+    return tmp_path
+
+
+@pytest.fixture(scope="function")
+def random_string():
+    return "".join(choices(printable, k=randint(0, 1000)))

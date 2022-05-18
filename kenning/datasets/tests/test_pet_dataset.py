c532db9a54ca54eaa84cf6553c067009a25d4cbe
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-05-18 15:27:49 +0200

    [#34054] datasets: pet_dataset: Add tests for not valid data

--- kenning/datasets/tests/test_pet_dataset.py
+++ kenning/datasets/tests/test_pet_dataset.py
@@ -2,10 +2,11 @@ from kenning.datasets.pet_dataset import PetDataset
 import pytest
 from string import printable
 from random import choices, randint
+from PIL.Image import UnidentifiedImageError
 
 
 class TestPetDataset:
-    def test_one(self, tmp_path):
+    def test_one(self, tmp_path, random_string):
         # Test constructor
         empty_path = f"'{str(tmp_path.absolute())}/annotations/list.txt'"
         with pytest.raises(FileNotFoundError) as execinfo:
@@ -24,14 +25,33 @@ class TestPetDataset:
         dataset = PetDataset(empty_annotations)
         assert ([], []) == dataset.get_data()
 
-    def test_three(self):
+        with pytest.raises(StopIteration):
+            dataset.__next__()
+
+    def test_three(self, random_annotations):
         # Provide dir with random data in 'annotations/list.txt'
+        PetDataset(random_annotations)
         pytest.fail(str(NotImplemented))
 
-    def test_four(self, empty_annotations):
-        # Provide dir with simple valid data
-        PetDataset(empty_annotations)
-        pytest.fail(str(NotImplemented))
+    def test_four(self, fake_annotations):
+        # Provide dir with valid data in list.txt
+        # but nothing valid elsewhere
+        dataset = PetDataset(fake_annotations)
+        error_message = "No such file or directory: '"
+        error_message += str(fake_annotations.absolute())
+        with pytest.raises(FileNotFoundError) as execinfo:
+            dataset.get_data()
+        assert error_message in str(execinfo.value)
+
+    def test_five(self, fake_images):
+        # Provide dir with valid data in annotations/list.txt
+        # and empty image files in images/
+        with pytest.raises(UnidentifiedImageError) as execinfo:
+            dataset = PetDataset(fake_images)
+            dataset.get_data()
+        error_message = "cannot identify image file '"
+        error_message += str(fake_images.absolute())
+        assert error_message in str(execinfo.value)
 
 
 @pytest.fixture(scope="function")
@@ -39,6 +59,25 @@ def empty_annotations(tmp_path):
     (tmp_path / 'annotations').mkdir()
     (tmp_path / 'annotations' / 'list.txt').touch()
     return tmp_path
+
+
+@pytest.fixture(scope="function")
+def fake_annotations(empty_annotations):
+    with open(empty_annotations / 'annotations' / 'list.txt', 'w') as f:
+        print("Abyssinian_100 1 1 1", file=f)
+    return empty_annotations
+
+
+@pytest.fixture(scope="function")
+def fake_images(fake_annotations):
+    (fake_annotations / 'images').mkdir()
+    (fake_annotations / 'images' / 'Abyssinian_100.jpg').touch()
+    return fake_annotations
+
+
+@pytest.fixture(scope="function")
+def random_annotations(empty_annotations):
+    return empty_annotations
 
 
 @pytest.fixture(scope="function")

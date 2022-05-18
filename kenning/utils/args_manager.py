8c447841a5ec870dc3844b76aa4ad3983bb5c60e
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-05-18 13:55:21 +0200

    [#33881] utils: Unified names of the arguments for JSON and argparse
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- kenning/utils/args_manager.py
+++ kenning/utils/args_manager.py
@@ -9,50 +9,14 @@ from typing import Dict
 from pathlib import Path
 
 
-def serialize(obj: object) -> str:
-    """
-    Serializes the given object into a JSON format.
-
-    It serializes all variables mentioned in the dictionary
-    returned by a `form_parameterschema` function.
-
-    Note that object has to implement `form_parameterschema`
-    to be serializable.
-
-    Parameters
-    ----------
-    obj : object
-        Object to serialize
-
-    Returns
-    -------
-    str: Serialized object in a valid JSON format.
-    """
-    if hasattr(obj, 'form_parameterschema'):
-        to_serialize = obj.form_parameterschema()['properties'].keys()
-    else:
-        return '{}'
-
-    serialized_dict = {}
-
-    for name, value in vars(obj).items():
-        if name in to_serialize:
-            serialized_dict[name] = value
-
-    class ArgumentEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, Path):
-                return str(obj)
-            return json.JSONEncoder.default(self, obj)
-
-    return json.dumps(serialized_dict, cls=ArgumentEncoder)
-
-
 """
 arguments_structure is a mapping (argument_name -> keywords)
 
 Supported keywords:
-argparse_name: Name that is prompted as an argparse argument.
+argparse_name: Name that is prompted as an argparse argument. It is also
+    used for JSON arguments with hyphens stripped from the prefix and
+    changed to underscores.
+    E.g. --some-name becomes some_name
 description: Description of the argument.
 type: Same as 'type' in argparse. The argument is converted to this value.
     Possible values for type: [int, float, str, bool, Path].
@@ -64,7 +28,6 @@ enum: List of possible values of the argument.
 is_list: Determines whether argument is a list of arguments.
     Possible values for is_list: [True, False].
     By default it is False.
-
     List of bool arguments is not supported
 
 Example of an argument:
@@ -78,12 +41,61 @@ Example of an argument:
 """
 
 
+def convert_argparse_name(s):
+    return s.lstrip('-').replace('-', '_')
+
+
+def serialize(obj: object) -> str:
+    """
+    Serializes the given object into a JSON format.
+
+    It serializes all variables mentioned in the dictionary
+    returned by a `form_parameterschema` function.
+
+    Note that object has to implement `form_parameterschema`
+    which uses `add_parameterschema_argument` function
+    to be serializable.
+
+    Parameters
+    ----------
+    obj : object
+        Object to serialize
+
+    Returns
+    -------
+    str: Serialized object in a valid JSON format.
+    """
+    if hasattr(obj, 'form_parameterschema'):
+        properties = obj.form_parameterschema()['properties']
+        to_serialize = {
+            keywords['real_name']: name
+            for name, keywords in properties.items()
+        }
+    else:
+        return '{}'
+
+    serialized_dict = {}
+
+    for name, value in vars(obj).items():
+        if name in to_serialize:
+            serialized_dict[to_serialize[name]] = value
+
+    class ArgumentEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Path):
+                return str(obj)
+            return json.JSONEncoder.default(self, obj)
+
+    return json.dumps(serialized_dict, cls=ArgumentEncoder)
+
+
 def get_parsed_json_dict(schema, json_dict):
     """
     Validates the given dictionary with the schema.
     Then it adds default values for missing
-    arguments and converts the arguments to the
-    appropriate types, as jsonschema can not do that.
+    arguments and converts the arguments to the appropriate types
+    as jsonschema can not do that. Finally it provides new names
+    that match the constructor arguments,
 
     Parameters
     ----------
@@ -98,23 +110,33 @@ def get_parsed_json_dict(schema, json_dict):
     )
 
     converted_json_dict = {}
+
+    # Including default values
     for name, keywords in schema['properties'].items():
         if name not in json_dict and 'default' in keywords:
             converted_json_dict[name] = keywords['default']
         elif name in json_dict:
             converted_json_dict[name] = json_dict[name]
 
+    # Converting arguments to the final types
     for name, value in converted_json_dict.items():
-        if 'convert-type' in schema['properties'][name]:
-            converter = schema['properties'][name]['convert-type']
+        keywords = schema['properties'][name]
 
-            if 'type' in schema['properties'][name] and \
-               schema['properties'][name]['type'] == 'array':
+        if 'convert-type' in keywords:
+            converter = keywords['convert-type']
+
+            if 'type' in keywords and keywords['type'] == 'array':
                 converted_json_dict[name] = [converter(v) for v in value]
             else:
                 converted_json_dict[name] = converter(value)
         else:
             converted_json_dict[name] = value
+
+    # Changing names so they match the constructor arguments
+    converted_json_dict = {
+        schema['properties'][name]['real_name']: value
+        for name, value in converted_json_dict.items()
+    }
 
     return converted_json_dict
 
@@ -147,30 +169,30 @@ def add_argparse_argument(
         prop = struct[name]
 
         argparse_name = prop['argparse_name']
-        arguments = {}
+        keywords = {}
 
         if 'type' in prop:
             if prop['type'] is bool:
                 assert 'default' in prop and prop['default'] in [True, False]
 
-                arguments['action'] = (
+                keywords['action'] = (
                     'store_false' if prop['default']
                     else 'store_true'
                 )
             else:
-                arguments['type'] = prop['type']
+                keywords['type'] = prop['type']
         if 'description' in prop:
-            arguments['help'] = prop['description']
+            keywords['help'] = prop['description']
         if 'default' in prop:
-            arguments['default'] = prop['default']
+            keywords['default'] = prop['default']
         if 'required' in prop and prop['required']:
-            arguments['required'] = prop['required']
+            keywords['required'] = prop['required']
         if 'enum' in prop:
-            arguments['choices'] = prop['enum']
+            keywords['choices'] = prop['enum']
         if 'is_list' in prop and prop['is_list']:
-            arguments['nargs'] = '+'
+            keywords['nargs'] = '+'
 
-        group.add_argument(argparse_name, **arguments)
+        group.add_argument(argparse_name, **keywords)
 
 
 def add_parameterschema_argument(
@@ -203,8 +225,13 @@ def add_parameterschema_argument(
     for name in names:
         prop = struct[name]
 
-        schema['properties'][name] = {}
-        arguments = schema['properties'][name]
+        argparse_name = prop['argparse_name']
+        argschema_name = convert_argparse_name(argparse_name)
+
+        schema['properties'][argschema_name] = {}
+        keywords = schema['properties'][argschema_name]
+
+        keywords['real_name'] = name
 
         type_to_jsontype = {
             Path: 'string',
@@ -214,30 +241,30 @@ def add_parameterschema_argument(
             bool: 'boolean'
         }
 
-        # Case for a list of arguments
+        # Case for a list of keywords
         if 'is_list' in prop and prop['is_list']:
-            arguments['type'] = 'array'
+            keywords['type'] = 'array'
 
             if 'type' in prop:
                 assert prop['type'] is not bool
 
-                arguments['convert-type'] = prop['type']
-                arguments['items'] = {
+                keywords['convert-type'] = prop['type']
+                keywords['items'] = {
                     'type': type_to_jsontype[prop['type']]
                 }
         # Case for a single argument
         else:
             if 'type' in prop:
-                arguments['convert-type'] = prop['type']
-                arguments['type'] = type_to_jsontype[prop['type']]
+                keywords['convert-type'] = prop['type']
+                keywords['type'] = type_to_jsontype[prop['type']]
 
         if 'description' in prop:
-            arguments['description'] = prop['description']
+            keywords['description'] = prop['description']
         if 'default' in prop:
-            arguments['default'] = prop['default']
+            keywords['default'] = prop['default']
         if 'required' in prop and prop['required']:
             if 'required' not in schema:
                 schema['required'] = []
-            schema['required'].append(name)
+            schema['required'].append(argschema_name)
         if 'enum' in prop:
-            arguments['enum'] = prop['enum']
+            keywords['enum'] = prop['enum']

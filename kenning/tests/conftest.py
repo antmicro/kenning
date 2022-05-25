f450b2e950db5da89e3893fe6b9abf562536c945
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-05-25 15:57:32 +0200

    [#34054] conftest: WIP: Adds not working imitation of open_image_dataset files

--- kenning/tests/conftest.py
+++ kenning/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from random import randint
+from random import randint, random
 from PIL import Image
 
 
@@ -12,12 +12,23 @@ def empty_dir(tmp_path):
 
 @pytest.fixture
 def fake_images(empty_dir):
-    def random_one_hot():
+    def three_random_one_hot():
         return f'{randint(0, 1)} {randint(0, 1)} {randint(0, 1)}'
+
+    def four_random_range():
+        # return f'{random()},{random()},{random()},{random()}'
+        return '0.3,0.9,0.2,0.7'
     amount = 100
 
     with open(empty_dir / 'annotations' / 'list.txt', 'w') as f:
-        [print(f'image_{i} {random_one_hot()}', file=f) for i in range(amount)]
+        [print(f'image_{i} {three_random_one_hot()}', file=f)
+         for i in range(amount)]
+    with open(empty_dir / 'classnames.csv', 'w') as f:
+        print('/m/o0fd,person', file=f)
+    with open(empty_dir / 'annotations.csv', 'w') as f:
+        print('ImageID,Source,LabelName,Confidence,XMin,XMax,YMin,YMax,IsOccluded,IsTruncated,IsGroupOf,IsDepiction,IsInside', file=f)  # noqa: E501
+        [print(f'image_{i},xclick,/m/o0fd,1,{four_random_range()},0,0,0,0,0', file=f)
+         for i in range(amount)]
     (empty_dir / 'images').mkdir()
     for i in range(amount):
         file = (empty_dir / 'images' / f'image_{i}.jpg')

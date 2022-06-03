0bc65c3b0120c79a1ea5c733973a5bc84c52ef6d
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-03 18:38:32 +0200

    [#34054] tests: Add dataset input delivery methods
    Adds test for testing dataset's input delivery method

--- kenning/tests/compatibility/test_modelwrappers_dataset.py
+++ kenning/tests/compatibility/test_modelwrappers_dataset.py
@@ -5,18 +5,17 @@ if sys.version_info.minor < 9:
 else:
     from importlib.resources import path
 from kenning.utils.class_loader import load_class
-from kenning.modelwrappers.classification.pytorch_pet_dataset import PyTorchPetDatasetMobileNetV2  # noqa: E501
 
 
 class TestModelWrapperAndDatasetCompatibility:
     dataset_dict = {
-        'random':
-            'random_dataset.RandomizedClassificationDataset',
         'pet_dataset':
             'pet_dataset.PetDataset',
         'open_images':
             'open_images_dataset.OpenImagesDatasetV6',
     }
+    # 'random':
+    #        'random_dataset.RandomizedClassificationDataset',
 
     def test_one(self, fake_images):
         """
@@ -103,22 +102,86 @@ class TestModelWrapperAndDatasetCompatibility:
         for modelwrapper_value in modelwrapper_dict.values():
             test_model_wrapper(*modelwrapper_value)
 
-    def test_without_model(self, fake_images):
+    def test_model_and_dataset(self, fake_images):
         """
-        Perform tests on ModelWrapper without and model
+        Tests ModelWrapper and dataset compatibility.
         """
 
+        # TODO: implement it for every single modelwrapper out there
+        # We are looking for errors in regular conditions
+        from kenning.modelwrappers.classification.pytorch_pet_dataset import PyTorchPetDatasetMobileNetV2  # noqa: E501
+        from kenning.core.measurements import Measurements
         images_path, images_amount = fake_images
         module_name = self.dataset_dict['pet_dataset']
         dataset = load_class('kenning.datasets.'+module_name)(images_path)
+
+        # Initialize modelwrapper
         modelwrapper = PyTorchPetDatasetMobileNetV2(
-            "",
+            images_path / "model.pth5",
             dataset,
             from_file=False
         )
 
-        measurments = modelwrapper.test_inference()
-        assert measurments.data['total'] == images_amount
+        # Run test_inference on model
+        measurements = modelwrapper.test_inference()
+        assert isinstance(measurements, Measurements)
+        assert measurements.data['total'] == images_amount
 
-        print(dataset.get_input_mean_std())
-        assert 0
+        # Run model training
+        modelwrapper.train_model(1, 1, 1, images_path)
+
+    def test_deliver_input(self, fake_images):
+        """
+        Tests dataset functions used by modelwrappers for data delivering
+
+        List of methods are being tested
+        -----------------------
+        dataset.prepare_input_samples
+        dataset.prepare_output_samples
+        dataset.get_input_mean_std
+        dataset.train_test_split_representations
+
+        Used fixtures
+        -------------
+        fake_images - to generate images and feed them to datasets
+        """
+
+        def run_tests(module, images_path):
+            dataset = load_class('kenning.datasets.' + module)(images_path)
+
+            # Test train_test_split_representations
+            # this test should check the return type
+            # if train or validation data is empty
+            # and check that corresponding sets of data are equal in length
+            Xt, Xv, Yt, Yv = dataset.train_test_split_representations(
+                test_fraction=0.25)
+            assert isinstance(Xt, list) and isinstance(Xv, list)
+            assert len(Xt) > 0 and len(Xv) > 0
+            assert len(Xt) == len(Yt) and len(Xv) == len(Yv)
+
+            # Test prepare_input_samples
+            # this test should check the return type
+            # if length of output equals to length of provided data
+            samples = dataset.prepare_input_samples(Xv)
+            assert isinstance(samples, list)
+            assert len(samples) == len(Xv)
+
+            # Test prepare_output_samples
+            # this test should check the return type
+            # if length of output equals to length of provided data
+
+            samples = dataset.prepare_output_samples(Yv)
+            assert isinstance(samples, list)
+            assert len(samples) == len(Yv)
+
+            # TODO: This method isn't implemented for openimages dataset
+            # what leads to NotImplementedError, to my opnion
+            # method should be cutted off from base class
+            # or implementation should be added to openimages dataset
+
+            # Test get_input_mean_std
+            # this test has just check if output of method is a numerical value
+            # mean, std = dataset.get_input_mean_std()
+
+        for module in self.dataset_dict.values():
+            run_tests(module, fake_images[0])

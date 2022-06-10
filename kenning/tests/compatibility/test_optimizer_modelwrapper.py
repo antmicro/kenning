ef0cff35244a0b9351e95f85d78e26506d95efe9
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-10 18:15:24 +0200

    [#34054] tests: Finish optimizer base testing

--- kenning/tests/compatibility/test_optimizer_modelwrapper.py
+++ kenning/tests/compatibility/test_optimizer_modelwrapper.py
@@ -5,20 +5,45 @@ from pathlib import Path
 
 class TestOptimizerModelWrapper:
     optimizer_dict = {
-        'tflite': [
-            'tflite.TFLiteCompiler',
-            'pet_dataset.PetDataset',
-            '/resources/models/classification/tensorflow_pet_dataset_mobilenetv2.h5',    # noqa: E501
-            'tflite',
-            'tensorflow_pet_dataset',
-        ],
-        'tvm': [
-            'tvm.TVMCompiler',
-            'pet_dataset.PetDataset',
-            '/resources/models/classification/pytorch_pet_dataset_mobilenetv2.pth',      # noqa: E501
-            'so',
-            'pytorch_pet_dataset',
-        ],
+        'tflite_keras': (
+            (
+                'tflite.TFLiteCompiler',
+                'pet_dataset.PetDataset',
+                '/resources/models/classification/tensorflow_pet_dataset_mobilenetv2.h5',    # noqa: E501
+                'tflite',
+                'tensorflow_pet_dataset',
+            ),
+            {
+                'target': 'default',
+                'modelframework': 'keras',
+            },
+        ),
+        'tvm_keras': (
+            (
+                'tvm.TVMCompiler',
+                'pet_dataset.PetDataset',
+                '/resources/models/classification/tensorflow_pet_dataset_mobilenetv2.h5',      # noqa: E501
+                'so',
+                'tensorflow_pet_dataset',
+            ),
+            {
+                'target': 'llvm',
+                'modelframework': 'keras'
+            },
+        ),
+        'tvm_pytorch': (
+            (
+                'tvm.TVMCompiler',
+                'pet_dataset.PetDataset',
+                '/resources/models/classification/pytorch_pet_dataset_mobilenetv2_full_model.pth',      # noqa: E501
+                'so',
+                'pytorch_pet_dataset',
+            ),
+            {
+                'target': 'llvm',
+                'modelframework': 'torch'
+            },
+        ),
     }
     modelwrapper_dict = {
         'tensorflow_pet_dataset': 'classification.tensorflow_pet_dataset.TensorFlowPetDatasetMobileNetV2',  # noqa: E501
@@ -40,11 +65,13 @@ class TestOptimizerModelWrapper:
         fake_images - to generate images and feed them to dataset
         """
 
-        def run_tests(optimizer_path, dataset_path, model_path,
-                      file_suffix, wrapper_name, images_path):
+        def run_tests(images_path, optimizer_path, dataset_path, model_path,
+                      file_suffix, wrapper_name, **kwargs):
             """
             Arguments
             ---------
+            images_path: Path
+                Path to temporary folder with images for dataset class
             optimizer_path: str
                 An import path to optimizer
             dataset_path: str
@@ -55,9 +82,6 @@ class TestOptimizerModelWrapper:
                 A suffix with which output file should be saved
             wrapper_name: str
                 A key to self.modelwrapper_dict for modelwrapper to use
-            images_path: pytest.fixture
-                Fixture which generates a temporary folder
-                with images for dataset class
             """
             # General classes
             datasetcls = load_class("kenning.datasets." + dataset_path)
@@ -68,19 +92,19 @@ class TestOptimizerModelWrapper:
             file_name = optimizer_path + "_compiled." + file_suffix
 
             # Path where to save model
-            path_to_save_model = Path("/tmp/savedmodels/"+file_name)
+            path_to_save_model = images_path / file_name
 
             # Path to input model
             path_to_input_model = Path(self.kenning_path+model_path)
 
             # Initialize classes
             dataset = datasetcls(images_path)
-            wrapper = wrappercls(path_to_input_model, dataset, from_file=True)
-            optimizer = optimizercls(dataset, path_to_save_model)
+            # wrapper=wrappercls(path_to_input_model, dataset, from_file=True)
+            wrapper = wrappercls(images_path, dataset, from_file=False)
+            optimizer = optimizercls(dataset, path_to_save_model, **kwargs)
 
             inputshapes, dtype = wrapper.get_input_spec()
-            print(wrapper.get_input_spec())
             optimizer.compile(path_to_input_model, inputshapes, dtype=dtype)
 
-        for optimizer in self.optimizer_dict.values():
-            run_tests(*optimizer, fake_images[0])
+        for optimizer_args, optimizer_kwargs in self.optimizer_dict.values():
+            run_tests(fake_images[0], *optimizer_args, **optimizer_kwargs)

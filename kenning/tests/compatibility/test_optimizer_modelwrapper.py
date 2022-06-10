459d71d72a0655fe4afb6ea5929050b03a0e4af0
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-10 13:21:27 +0200

    [#34054] tests: Add docstring

--- kenning/tests/compatibility/test_optimizer_modelwrapper.py
+++ kenning/tests/compatibility/test_optimizer_modelwrapper.py
@@ -42,19 +42,45 @@ class TestOptimizerModelWrapper:
 
         def run_tests(optimizer_path, dataset_path, model_path,
                       file_suffix, wrapper_name, images_path):
-            file_name = optimizer_path + ".compiled." + file_suffix
-            path_to_save_model = (images_path / file_name).absolute()
-            path_to_model = Path(self.kenning_path+model_path)
+            """
+            Arguments
+            ---------
+            optimizer_path: str
+                An import path to optimizer
+            dataset_path: str
+                An import path to dataset
+            model_path: str
+                A path to input model
+            file_suffix: str
+                A suffix with which output file should be saved
+            wrapper_name: str
+                A key to self.modelwrapper_dict for modelwrapper to use
+            images_path: pytest.fixture
+                Fixture which generates a temporary folder
+                with images for dataset class
+            """
+            # General classes
             datasetcls = load_class("kenning.datasets." + dataset_path)
             wrappercls = load_class("kenning.modelwrappers." + self.modelwrapper_dict[wrapper_name])    # noqa: E501
             optimizercls = load_class("kenning.compilers." + optimizer_path)
+
+            # The filename for compiled model
+            file_name = optimizer_path + "_compiled." + file_suffix
+
+            # Path where to save model
+            path_to_save_model = Path("/tmp/savedmodels/"+file_name)
+
+            # Path to input model
+            path_to_input_model = Path(self.kenning_path+model_path)
+
+            # Initialize classes
             dataset = datasetcls(images_path)
-            wrapper = wrappercls(images_path, dataset, from_file=False)
+            wrapper = wrappercls(path_to_input_model, dataset, from_file=True)
             optimizer = optimizercls(dataset, path_to_save_model)
 
             inputshapes, dtype = wrapper.get_input_spec()
-            print(inputshapes, dtype)
-            optimizer.compile(path_to_model, inputshapes, dtype=dtype)
+            print(wrapper.get_input_spec())
+            optimizer.compile(path_to_input_model, inputshapes, dtype=dtype)
 
         for optimizer in self.optimizer_dict.values():
             run_tests(*optimizer, fake_images[0])

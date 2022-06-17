e83a8a160684eeeb5c84c4400346eba7d3c362d3
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-17 18:20:15 +0200

    [#34054] tests: compatibility: Add Onnx to optimizer test
    Adds test with converting model to onnx format and passing it to optimizer.
    
    Updates the model -> optimizer test.

--- kenning/tests/compatibility/test_optimizer_modelwrapper.py
+++ kenning/tests/compatibility/test_optimizer_modelwrapper.py
@@ -1,5 +1,10 @@
+import pytest
+import tempfile
+import os
+
+
 class TestOptimizerModelWrapper:
-    def test_compile_existance_models(self, optimizerSamples,
+    def test_compile_existance_models(self, optimizerSamples, fake_images,
                                       modelSamples, modelwrapperSamples):
         """
         Tests compilation process for models presented in kenning.
@@ -29,13 +34,24 @@ class TestOptimizerModelWrapper:
             """
 
             wrapper = modelwrapperSamples.get(wrapper_name)
+            block_output_format = optimizer.consult_model_type(wrapper)
+            assert isinstance(block_output_format, list)
+            assert optimizer.modelframework in block_output_format
+
+            filepath = tempfile.NamedTemporaryFile().name[5:]
+            filepath = fake_images.path / filepath
             inputshapes, dtype = wrapper.get_input_spec()
+
+            optimizer.set_compiled_model_path(filepath)
             optimizer.compile(model_path, inputshapes, dtype=dtype)
+            assert os.path.exists(filepath)
+            os.remove(filepath)
 
         for optimizer, modelframework in optimizerSamples:
             run_tests(optimizer, *modelSamples.get(modelframework))
 
-    def test_onnx_model_optimization(self, modelwrapperSamples):
+    def test_onnx_model_optimization(self, modelwrapperSamples,
+                                     optimizerSamples, fake_images):
         """
         Tests saving models to onnx format with modelwrappers
         and converting them using optimizers.
@@ -48,10 +64,30 @@ class TestOptimizerModelWrapper:
         Used fixtures
         -------------
         modelwrapperSamples - to get modelwrappers instances.
+        optimizerSamples - to get optimizers instances.
         """
 
-        def save_to_onnx(warpper):
-            print(wrapper)
-
         for wrapper in modelwrapperSamples:
-            save_to_onnx(wrapper)
+            inputshapes, dtype = wrapper.get_input_spec()
+
+            filename = tempfile.NamedTemporaryFile().name[5:]
+            filepath = fake_images.path / filename
+            wrapper.save_to_onnx(filepath)
+            assert os.path.exists(filepath)
+
+            for optimizer, modelframework in optimizerSamples:
+                with pytest.raises(ValueError):
+                    optimizer.consult_model_type(optimizer)
+                model_type = optimizer.consult_model_type(wrapper)
+                assert 'onnx' in model_type
+
+                optimizer.set_input_type('onnx')
+                compiled_model_path = (filename + '_' + modelframework)
+                compiled_model_path = fake_images.path / compiled_model_path
+                optimizer.set_compiled_model_path(compiled_model_path)
+                optimizer.compile(filepath, inputshapes, dtype=dtype)
+
+                assert os.path.exists(compiled_model_path)
+                os.remove(compiled_model_path)
+
+            os.remove(filepath)

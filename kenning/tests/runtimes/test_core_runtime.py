ad1fca770c08ae1c033935e1e07c58149cd9ae5f
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-30 16:55:09 +0200

    [#35230] tests: runtime: Add basic implementation for tests
    Adds implementation for Runtime tests

--- kenning/tests/runtimes/test_core_runtime.py
+++ kenning/tests/runtimes/test_core_runtime.py
@@ -1,73 +1,112 @@
 from kenning.core.runtime import Runtime
+from kenning.core.runtimeprotocol import RuntimeProtocol
+from typing import Type
 import pytest
 
 
+@pytest.mark.parametrize("runtimecls", [(Runtime)])
 class TestCoreRuntime:
-    @pytest.mark.xfail()
-    def test_constructor(self):
-        assert 0
 
-    @pytest.mark.xfail()
-    def test_inference_session_start(self):
-        assert 0
+    def test_inference_session_start(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol(), collect_performance_data=False)
+        runtime.inference_session_start()
+        assert runtime.statsmeasurements is None
 
-    @pytest.mark.xfail()
-    def test_inference_session_end(self):
-        assert 0
+        runtime = runtimecls(RuntimeProtocol(), collect_performance_data=True)
+        runtime.inference_session_start()
+        runtime.inference_session_end()
+        assert runtime.statsmeasurements is None
+        # TODO: Test with data is being collected
 
-    @pytest.mark.xfail()
-    def test_close_server(self):
-        assert 0
+    def test_inference_session_end(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        runtime.inference_session_end()
+        assert runtime.statsmeasurements is None
+        # TODO: Test with data is being collected
 
-    def test_prepare_server(self):
-        runtime = Runtime(None)
-        with pytest.raises(AttributeError):
+    def test_close_server(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        assert runtime.shouldwork is True
+
+        runtime.close_server()
+        assert runtime.shouldwork is False
+
+    def test_prepare_server(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
             runtime.prepare_server()
 
-    @pytest.mark.xfail()
-    def test_prepare_client(self):
-        assert 0
+    def test_prepare_client(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_client()
 
-    @pytest.mark.xfail()
-    def test_prepare_input(self):
-        assert 0
+    def test_prepare_input(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_input('kenning'.encode())
 
-    @pytest.mark.xfail()
-    def test_prepare_model(self):
-        assert 0
+    def test_prepare_model(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_model('kenning'.encode())
 
-    @pytest.mark.xfail()
-    def test_process_input(self):
-        assert 0
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_model(None)
 
-    @pytest.mark.xfail()
-    def test_run(self):
-        assert 0
+    def test_process_input(self, mocker, caplog, runtimecls: Type[Runtime]):
+        mocker.patch(
+            'kenning.core.runtimeprotocol.RuntimeProtocol.request_success',
+            lambda x: None
+                     )
+        mocker.patch(
+            'kenning.core.runtime.Runtime._run',
+            lambda x: None
+                     )
 
-    @pytest.mark.xfail()
-    def test_upload_output(self):
-        assert 0
+        import logging
+        log_messages = ('Processing input', 'Input processed')
+        runtime = runtimecls(RuntimeProtocol())
+        caplog.set_level(logging.DEBUG)
+        runtime.process_input('kenning'.encode())
+        for i in range(len(log_messages)):
+            assert caplog.records[i].msg == log_messages[i]
 
-    @pytest.mark.xfail()
-    def test_upload_stats(self):
-        assert 0
+    def test_run(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.run()
 
-    @pytest.mark.xfail()
-    def test_upload_essentials(self):
-        assert 0
+    def test_upload_output(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.upload_output('kenning'.encode())
 
-    @pytest.mark.xfail()
-    def test_prepare_local(self):
-        assert 0
+    def test_upload_stats(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        assert b'{}' == runtime.upload_stats('kenning'.encode())
+        # TODO: Test on real data with real output
 
-    @pytest.mark.xfail()
-    def test_run_locally(self):
-        assert 0
+    def test_upload_essentials(self, mocker, runtimecls: Type[Runtime]):
+        from pathlib import Path
+        import uuid
 
-    @pytest.mark.xfail()
-    def test_run_client(self):
-        assert 0
+        runtime = runtimecls(RuntimeProtocol())
+        path = Path(uuid.uuid4().hex)
 
-    @pytest.mark.xfail()
-    def test_run_server(self):
-        assert 0
+        def moc_func(self, modelpath: Path):
+            assert modelpath == path
+            return
+
+        mocker.patch(
+            'kenning.core.runtimeprotocol.RuntimeProtocol.upload_model',
+            moc_func)
+        runtime.upload_essentials(path)
+
+    def test_prepare_local(self, runtimecls: Type[Runtime]):
+        runtime = runtimecls(RuntimeProtocol())
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_model('kenning'.encode())
+
+        with pytest.raises(NotImplementedError):
+            runtime.prepare_model(None)

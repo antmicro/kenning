0710ec861759ec5526ac86f6212e6a5a4e4bcecd
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-06-30 17:13:02 +0200

    [#35230] tests: runtime: Add docstring

--- kenning/tests/runtimes/test_core_runtime.py
+++ kenning/tests/runtimes/test_core_runtime.py
@@ -1,5 +1,7 @@
 from kenning.core.runtime import Runtime
 from kenning.core.runtimeprotocol import RuntimeProtocol
+from pytest_mock import MockerFixture
+from pytest import LogCaptureFixture
 from typing import Type
 import pytest
 
@@ -8,6 +10,15 @@ import pytest
 class TestCoreRuntime:
 
     def test_inference_session_start(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.inference_session_start()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
+        # TODO: Test with data is being collected
         runtime = runtimecls(RuntimeProtocol(), collect_performance_data=False)
         runtime.inference_session_start()
         assert runtime.statsmeasurements is None
@@ -16,15 +27,30 @@ class TestCoreRuntime:
         runtime.inference_session_start()
         runtime.inference_session_end()
         assert runtime.statsmeasurements is None
-        # TODO: Test with data is being collected
 
     def test_inference_session_end(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.inference_session_end()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
+        # TODO: Test with data is being collected
         runtime = runtimecls(RuntimeProtocol())
         runtime.inference_session_end()
         assert runtime.statsmeasurements is None
-        # TODO: Test with data is being collected
 
     def test_close_server(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.test_close_server()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         assert runtime.shouldwork is True
 
@@ -32,21 +58,53 @@ class TestCoreRuntime:
         assert runtime.shouldwork is False
 
     def test_prepare_server(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.test_prepare_server()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.prepare_server()
 
     def test_prepare_client(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.test_prepare_client()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.prepare_client()
 
     def test_prepare_input(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.prepare_input()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.prepare_input('kenning'.encode())
 
     def test_prepare_model(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.prepare_input()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.prepare_model('kenning'.encode())
@@ -54,7 +112,22 @@ class TestCoreRuntime:
         with pytest.raises(NotImplementedError):
             runtime.prepare_model(None)
 
-    def test_process_input(self, mocker, caplog, runtimecls: Type[Runtime]):
+    def test_process_input(self,
+                           mocker: MockerFixture,
+                           caplog: LogCaptureFixture,
+                           runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.process_input()` method.
+
+        Parameters
+        ----------
+        mocker: MockerFixture
+            Fixture to provide changes to source code
+        caplog: LogCaptureFixture
+            Fixture to read logs
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         mocker.patch(
             'kenning.core.runtimeprotocol.RuntimeProtocol.request_success',
             lambda x: None
@@ -73,21 +146,57 @@ class TestCoreRuntime:
             assert caplog.records[i].msg == log_messages[i]
 
     def test_run(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.run()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.run()
 
     def test_upload_output(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.upload_output()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.upload_output('kenning'.encode())
 
     def test_upload_stats(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.upload_stats()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
+        # TODO: Test on real data with real output
         runtime = runtimecls(RuntimeProtocol())
         assert b'{}' == runtime.upload_stats('kenning'.encode())
-        # TODO: Test on real data with real output
 
-    def test_upload_essentials(self, mocker, runtimecls: Type[Runtime]):
+    def test_upload_essentials(self,
+                               mocker: MockerFixture,
+                               runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.upload_essentials()` method.
+
+        Parameters
+        ----------
+        mocker: MockerFixture
+            Fixture to provide changes to source code
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         from pathlib import Path
         import uuid
 
@@ -104,6 +213,14 @@ class TestCoreRuntime:
         runtime.upload_essentials(path)
 
     def test_prepare_local(self, runtimecls: Type[Runtime]):
+        """
+        Tests the `Runtime.prepare_local()` method.
+
+        Parameters
+        ----------
+        runtimecls: Type[Runtime]
+            Class to initialize Runtime object
+        """
         runtime = runtimecls(RuntimeProtocol())
         with pytest.raises(NotImplementedError):
             runtime.prepare_model('kenning'.encode())

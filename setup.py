078e835228afa9083319c8c3acab0ac0d6fae742
Grzegorz Latosinski <glatosinski@antmicro.com>
2022-08-25 11:24:49 +0200

    [#36646] setup.py: Added requirements for docs and tvm
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- setup.py
+++ setup.py
@@ -25,35 +25,39 @@ setuptools.setup(
         'onnx>=1.7.0',
         'psutil>=5.8.0',
         'scikit_learn>=0.24.1',
-        'sphinx_antmicro_theme @ git+https://github.com/antmicro/sphinx_antmicro_theme.git#egg=sphinx_antmicro_theme',  # noqa: E501
-        'docutils==0.16',
         'tqdm>=4.56.2',
         'jsonschema'
     ],
     extras_require={
+        'docs': [
+            'sphinx',
+            'sphinx_antmicro_theme @ git+https://github.com/antmicro/sphinx_antmicro_theme.git#egg=sphinx_antmicro_theme',  # noqa: E501
+            'sphinxcontrib-napoleon',
+            'docutils==0.16'
+        ],
         'tensorflow': [
             'onnx_tf>=1.7.0',
             'tensorflow>=2.4.1',
             'tensorflow_addons>=0.12.1',
             'tf2onnx>=1.8.3',
             'tensorflow_probability'
-            ],
+        ],
         'torch': [
-            'torch>=1.7.1'
-            'torchvision>=0.8.2',
+            'torch>=1.7.1',
+            'torchvision>=0.8.2'
         ],
         'mxnet': [
             'gluoncv>=0.10.2',
-            'mxnet>=1.8.0',
+            'mxnet>=1.8.0'
         ],
         'nvidia_perf': [
-            'pynvml>=8.0.4',
+            'pynvml>=8.0.4'
         ],
         'object_detection': [
             'boto3>=1.17.5',
             'botocore>=1.20.5',
             'opencv_python>=4.5.2',
-            'pandas>=1.2.1',
+            'pandas>=1.2.1'
         ],
         ":python_version<'3.9'": [
             'importlib_resources>=5.1.4'
@@ -62,7 +66,10 @@ setuptools.setup(
             'iree-compiler>=20220415.108',
             'iree-runtime>=20220415.108',
             'iree-tools-tf>=20220415.108',
-            'iree-tools-tflite>=20220415.108',
+            'iree-tools-tflite>=20220415.108'
+        ],
+        'tvm': [
+            'apache-tvm'
         ]
     },
 )

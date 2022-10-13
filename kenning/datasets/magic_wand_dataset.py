3770875612dd9444b1a858e7cc10e6c221cb19ff
Jan Bylicki <jbylicki@antmicro.com>
2022-10-13 17:41:00 +0200

    [#37801] kenning: datasets: magic_wand_dataset: Added file parsing
    Added input file parsing to prepare() with a regex that matches only the
    internal data and strips off the unwanted characters that are present in
    there (It seems that the dataset is a dump of screen sessions with a
    device and some control characters and bash prompts are present inside)
    
    Signed-off-by: Jan Bylicki <jbylicki@antmicro.com>

--- kenning/datasets/magic_wand_dataset.py
+++ kenning/datasets/magic_wand_dataset.py
@@ -6,6 +6,8 @@ import tarfile
 import tempfile
 import glob
 import os
+import re
+from copy import deepcopy
 
 
 class MagicWandDataset(Dataset):
@@ -20,6 +22,22 @@ class MagicWandDataset(Dataset):
             self.download_dataset_fun()
         super().__init__(root, batch_size, download_dataset)
 
+    def rev_class_id(self, classname: str) -> int:
+        """
+        Returns an integer representing a class based on a class name
+
+        It generates a reversed dictionary from the self.classnames and
+        it gets the ID that is assigned to that name
+        Parameters
+        ----------
+        classname : str
+            The name of the class for which the ID will be returned
+        Returns
+        -------
+        Int : the class id
+        """
+        return {v: k for k, v in self.classnames.items()}[classname]
+
     def prepare(self):
         self.classnames = {
             0: "wing",
@@ -31,12 +49,25 @@ class MagicWandDataset(Dataset):
         self.dataY = []
         for i in self.classnames.values():
             path = self.root / i
-            for file in glob.glob(str(path) / "*.txt"):
-                filedata = []
+            for file in glob.glob(str(path / "*.txt")):
+                data_frame = []
                 with open(file) as f:
                     for line in f:
-                        pass
-        pass
+                        # match if line begins with '-', a number or a space
+                        # and later shows only visible characters up to the end
+                        if re.match(r"^[-0-9 ][\x20-\x7E]{1,}$", line):
+                            if re.search("-,-,-", line):
+                                if data_frame is not []:
+                                    self.dataX.append(deepcopy(data_frame))
+                                    self.dataY.append(self.rev_class_id(i))
+                            else:
+                                data = line.rstrip()
+                                data_frame.append(
+                                    [float(i) for i in data.split(',')]
+                                )
+        # print(self.dataX)
+        # print(self.dataY)
+        assert len(self.dataX) == len(self.dataY)
 
     def download_dataset_fun(self):
         dataset_url = "http://download.tensorflow.org/models/tflite/magic_wand/data.tar.gz"  # noqa: E501
@@ -45,7 +76,7 @@ class MagicWandDataset(Dataset):
             download_url(dataset_url, tarpath)
             data = tarfile.open(tarpath)
             data.extractall(self.dataset_root)
-        # cleanup MacOS dotfiles with metadata
+        # cleanup MacOS dotfiles with its internal metadata
         for macos_dotfile in glob.glob(str(self.dataset_root)+"/**/._*") + \
                 glob.glob(str(self.dataset_root)+"/._*"):
             os.remove(macos_dotfile)

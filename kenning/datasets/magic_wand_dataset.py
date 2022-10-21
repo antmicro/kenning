af72f505765c3542cf8e3e64a130bb447cb4c287
Jan Bylicki <jbylicki@antmicro.com>
2022-10-21 13:30:36 +0200

    [#37801] datasets: magic_wand_dataset: Made padding window_shift-aware
    Made the padding function aware of the window shift that will be
    happening in the model and made the padding fit the data according to
    that
    
    Signed-off-by: Jan Bylicki <jbylicki@antmicro.com>

--- kenning/datasets/magic_wand_dataset.py
+++ kenning/datasets/magic_wand_dataset.py
@@ -8,16 +8,21 @@ import glob
 import os
 import re
 from copy import deepcopy
+import numpy as np
 
 
 class MagicWandDataset(Dataset):
     def __init__(
             self,
             root: Path,
+            window_size: int = 128,
+            window_shift: int = 128,
             batch_size: int = 1,
             download_dataset: bool = False):
         self.dataset_root = root
         self.batch_size = batch_size
+        self.window_size = window_size
+        self.window_shift = window_shift
         if download_dataset:
             self.download_dataset_fun()
         super().__init__(root, batch_size, download_dataset)
@@ -45,6 +50,7 @@ class MagicWandDataset(Dataset):
             2: "slope",
             3: "negative"
         }
+        self.numclasses = 4
         self.dataX = []
         self.dataY = []
         for i in self.classnames.values():
@@ -57,8 +63,8 @@ class MagicWandDataset(Dataset):
                         # and later shows only visible characters up to the end
                         if re.match(r"^[-0-9 ][\x20-\x7E]{1,}$", line):
                             if re.search("-,-,-", line):
-                                if data_frame is not []:
-                                    self.dataX.append(deepcopy(data_frame))
+                                if data_frame != []:
+                                    self.dataX.append(deepcopy(self.generate_padding(data_frame)))  # noqa: E501
                                     self.dataY.append(self.rev_class_id(i))
                             else:
                                 data = line.rstrip()
@@ -80,6 +86,48 @@ class MagicWandDataset(Dataset):
         for macos_dotfile in glob.glob(str(self.dataset_root)+"/**/._*") + \
                 glob.glob(str(self.dataset_root)+"/._*"):
             os.remove(macos_dotfile)
+
+    def _generate_padding(self, noise_level, amount, neighbor: list) -> list:
+        return [list(i) for i in np.round((np.random.rand(
+            amount,
+            3
+        ) - 0.5) * noise_level, 1)+neighbor]
+
+    def generate_padding(
+            self,
+            data_frame: list,
+            noise_level: int = 20) -> list:
+        """
+        Generates neighbor-based padding around a given data frame
+
+        Parameters
+        ----------
+        data_frame : List
+            A frame of data to be padded
+        noise_level : Int
+            Level of noise (window in which the
+            neighbor data will vary in each axis)
+
+        Returns
+        -------
+        List : The padded data frame
+
+        """
+        pre_padding = self._generate_padding(
+                noise_level,
+                self.window_size - 1,
+                data_frame[0]
+        )
+        unpadded_len = len(pre_padding) + len(data_frame)
+        post_len = (self.window_shift - (unpadded_len %
+                    self.window_shift)) % self.window_shift
+
+        post_padding = self._generate_padding(
+                noise_level,
+                post_len,
+                data_frame[-1]
+        )
+        return pre_padding+data_frame+post_padding
 
     def get_class_names(self):
         return [self.classnames[i] for i in self.classnames.keys()]

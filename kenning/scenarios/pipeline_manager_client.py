fbeb18cbfd8432917e05f90e7e0794e1d8eb05b3
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-11-16 17:27:06 +0100

    [#38950] scenario: Add case for RUN and VALIDATE message types
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- kenning/scenarios/pipeline_manager_client.py
+++ kenning/scenarios/pipeline_manager_client.py
@@ -3,10 +3,12 @@
 import json
 import argparse
 import sys
+from pathlib import Path
 
 from pipeline_manager_backend_communication.communication_backend import CommunicationBackend   # noqa: E501
 from pipeline_manager_backend_communication.misc_structures import MessageType, Status  # noqa: E501
-from kenning.utils.pipeline_manager.misc import get_specification
+from kenning.utils.pipeline_manager.misc import get_specification, parse_dataflow  # noqa: E501
+from kenning.scenarios.json_inference_tester import run_scenario
 
 
 def main(argv):
@@ -23,6 +25,12 @@ def main(argv):
         help='The port of the Pipeline Manager Server',
         default=9000
     )
+    parser.add_argument(
+        '--file-path',
+        type=Path,
+        help='Path where inference output will be stored',
+        required=True
+    )
     args, _ = parser.parse_known_args(argv[1:])
 
     client = CommunicationBackend(args.host, args.port)
@@ -38,6 +46,37 @@ def main(argv):
                     MessageType.OK,
                     json.dumps(get_specification()).encode(encoding='UTF-8')
                 )
+
+            if (message_type == MessageType.VALIDATE or
+                    message_type == MessageType.RUN):
+
+                dataflow = json.loads(data)
+                successful, msg = parse_dataflow(dataflow)
+
+                if not successful:
+                    client.send_message(MessageType.ERROR, msg.encode())
+                    continue
+                try:
+                    run_scenario(
+                        msg,
+                        args.file_path,
+                        validate_only=(message_type == MessageType.VALIDATE)
+                    )
+                except Exception as ex:
+                    client.send_message(MessageType.ERROR, str(ex).encode())
+                    continue
+
+                feedback_msg = ''
+                if message_type == MessageType.VALIDATE:
+                    feedback_msg = 'Successfuly validated'
+                elif message_type == MessageType.RUN:
+                    feedback_msg = f'Successfuly run. Output saved in {args.file_path}'  # noqa: E501
+
+                client.send_message(
+                    MessageType.OK,
+                    feedback_msg.encode()
+                )
+                continue
 
 
 if __name__ == '__main__':

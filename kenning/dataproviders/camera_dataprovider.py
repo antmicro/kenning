bab920fd5b427e3db3ccdc65a8df4ce9b1af8fd4
Maciej Torhan <mtorhan@antmicro.com>
2023-01-13 11:43:52 +0100

    [#40470] dataproviders: camera_dataprovider: Refactor CameraDataProvider class for Kenning Flow
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/dataproviders/camera_dataprovider.py
+++ kenning/dataproviders/camera_dataprovider.py
@@ -7,31 +7,83 @@ A DataProvider-derived class used to interface with a
 camera, video file or a dummy video device
 (consult ffmpeg and v4l2loopback for configuration for dummy video devices)
 """
-
-from kenning.core.dataprovider import DataProvider
+from typing import Dict, Tuple, List
 import cv2
 import numpy as np
 from pathlib import Path
 
+from kenning.core.dataprovider import DataProvider
+from kenning.utils.args_manager import get_parsed_json_dict
+
 
 class CameraDataProvider(DataProvider):
+
+    arguments_structure = {
+        'video_file_path': {
+            'argparse_name': '--video_file_path',
+            'description': 'Path to the camera device',
+            'type': Path,
+            'required': True
+        },
+        'image_memory_layout': {
+            'argparse_name': '--image_memory_layout',
+            'description': 'Layout of capture frames (NHWC or NCHW)',
+            'type': str,
+            'required': False
+        },
+        'image_width': {
+            'argparse_name': '--image_width',
+            'description': 'Width of captured frame',
+            'type': int,
+            'required': False
+        },
+        'image_height': {
+            'argparse_name': '--image_height',
+            'description': 'Height of captured frame',
+            'type': int,
+            'required': False
+        }
+    }
+
     def __init__(
             self,
-            file_path: Path,
-            memory_layout: str = "NCHW",
+            video_file_path: Path,
+            image_memory_layout: str = "NCHW",
             image_width: int = 416,
-            image_height: int = 416):
+            image_height: int = 416,
+            inputs_sources: Dict[str, Tuple[int, str]] = {},
+            outputs: Dict[str, str] = {}):
+        """
+        Creates the camera data provider.
 
-        self.device_id = str(file_path)
+        Parameters
+        ----------
+        video_file_path: Path
+            Path to the video file
+        image_memory_layout: str
+            Layout of the frame memory: NCHW or NHWC
+        image_width: int
+            Width of the frame
+        image_height: int
+            Height of the frame
+        inputs_sources: Dict[str, Tuple[int, str]]
+            Input from where data is being retrieved
+        outputs: Dict[str, str]
+            Outputs of this Runner
+        """
 
-        self.file_path = file_path
-        self.memory_layout = memory_layout
+        self.device_id = str(video_file_path)
+
+        self.video_file_path = video_file_path
+        self.image_memory_layout = image_memory_layout
         self.image_width = image_width
         self.image_height = image_height
 
         self.device = None
 
-        super().__init__()
+        super().__init__(
+            inputs_sources=inputs_sources,
+            outputs=outputs)
 
     @classmethod
     def form_argparse(cls):
@@ -71,6 +123,20 @@ class CameraDataProvider(DataProvider):
             args.image_height
         )
 
+    @classmethod
+    def from_json(
+            cls,
+            json_dict: Dict,
+            inputs_sources: Dict[str, Tuple[int, str]] = None,
+            outputs: Dict[str, str] = None):
+        parameterschema = cls.form_parameterschema()
+        parsed_json_dict = get_parsed_json_dict(parameterschema, json_dict)
+
+        return cls(
+            **parsed_json_dict,
+            inputs_sources=inputs_sources,
+            outputs=outputs)
+
     def prepare(self):
         self.device = cv2.VideoCapture(self.device_id)
 
@@ -79,7 +145,7 @@ class CameraDataProvider(DataProvider):
             data,
             (self.image_width, self.image_height)
         )
-        if self.memory_layout == "NCHW":
+        if self.image_memory_layout == "NCHW":
             img = np.transpose(data, (2, 0, 1))
             return np.array(img, dtype=np.float32) / 255.0
         else:
@@ -91,11 +157,36 @@ class CameraDataProvider(DataProvider):
             return frame
         else:
             raise VideoCaptureDeviceException(self.device_id)
-            return None
 
     def detach_from_source(self):
         if self.device:
             self.device.release()
+
+    def get_io_specification(self) -> Dict[str, List[Dict]]:
+        if self.image_memory_layout == 'NCHW':
+            frame_shape = (1, 3, self.image_height, self.image_width)
+        else:
+            frame_shape = (1, self.image_height, self.image_width, 3)
+        return {
+            'input': [],
+            'output': [{
+                'name': 'frame',
+                'shape': frame_shape,
+                'dtype': 'float32'
+            }]
+        }
+
+    def run(
+            self,
+            inputs: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+        frame_global_name = None
+        for local_name, global_name in self.outputs.items():
+            if local_name == 'frame':
+                frame_global_name = global_name
+
+        frame = self.fetch_input()
+        frame = self.preprocess_input(frame)
+        return {frame_global_name: np.expand_dims(frame, 0)}
 
 
 class VideoCaptureDeviceException(Exception):

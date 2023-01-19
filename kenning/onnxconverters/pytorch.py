0fa20d3770715c633d953464390fbaec1ef2071d
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-01-19 18:39:10 +0100

    [#40900] onnxconverters: pytorch: onnx_import implemented
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- kenning/onnxconverters/pytorch.py
+++ kenning/onnxconverters/pytorch.py
@@ -8,6 +8,9 @@ ONNXConversion for PyTorch models.
 
 import torchvision.models as models
 import torch
+import onnx
+from pathlib import Path
+from typing import Union
 
 from kenning.core.onnxconversion import ONNXConversion
 from kenning.core.onnxconversion import SupportStatus
@@ -67,4 +70,49 @@ class PyTorchONNXConversion(ONNXConversion):
         return SupportStatus.SUPPORTED
 
     def onnx_import(self, modelentry, importpath):
-        return SupportStatus.UNSUPPORTED
+        model_onnx = onnx.load(str(importpath))
+        if model_onnx.ir_version <= 3:
+            return SupportStatus.UNSUPPORTED
+
+        if modelentry is not None:
+            input_tensor = modelentry.parameters['input_tensor']
+        else:
+            input_tensor = self.try_extracting_input_shape_from_onnx(
+                model_onnx)
+            input_tensor = [torch.rand(shape) for shape in input_tensor] \
+                if input_tensor else None
+
+        if input_tensor is None:
+            return SupportStatus.UNSUPPORTED
+
+        try:
+            model_torch = self.onnx_to_torch(model_onnx)
+        except RuntimeError or NotImplementedError:
+            del model_onnx
+            return SupportStatus.UNSUPPORTED
+
+        if len(input_tensor) == 1:
+            model_torch(input_tensor[0])
+        else:  # input_tensor: List[Tensor]
+            model_torch(*input_tensor)
+
+        del model_onnx  # noqa: F821
+        del model_torch
+        return SupportStatus.SUPPORTED
+
+    @staticmethod
+    def onnx_to_torch(onnx_model: Union[Path, onnx.ModelProto]):
+        """
+        Function for converting model from ONNX framework to PyTorch
+
+        Parameters
+        ----------
+        onnx_model: Path | ModelProto
+            Path to ONNX model or loaded ONNX model
+
+        Returns
+        -------
+        Model converted to PyTorch framework
+        """
+        import onnx2torch
+        return onnx2torch.convert(onnx_model)

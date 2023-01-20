489d07ca56ce6a223dd0e72c5d92200c9e606a8e
Maciej Torhan <mtorhan@antmicro.com>
2023-01-20 11:34:18 +0100

    [#40466] modelwrappers: classification: tensorflow_pet_dataset: Add metadata to io specs, add inputs specs to constructor
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/modelwrappers/classification/tensorflow_pet_dataset.py
+++ kenning/modelwrappers/classification/tensorflow_pet_dataset.py
@@ -10,28 +10,80 @@ Pretrained on ImageNet dataset, trained on Pet Dataset
 
 from pathlib import Path
 
+import tensorflow as tf
+import numpy as np
+
 from kenning.core.dataset import Dataset
-from kenning.modelwrappers.frameworks.tensorflow import TensorFlowWrapper  # noqa: E501
+from kenning.modelwrappers.frameworks.tensorflow import TensorFlowWrapper
+from kenning.interfaces.io_interface import IOInterface
 
 
 class TensorFlowPetDatasetMobileNetV2(TensorFlowWrapper):
-    def __init__(self, modelpath: Path, dataset: Dataset, from_file=True):
-        import tensorflow as tf
+    arguments_structure = {}
+
+    def __init__(
+            self,
+            modelpath: Path,
+            dataset: Dataset,
+            from_file=True
+    ):
         gpus = tf.config.list_physical_devices('GPU')
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        self.numclasses = dataset.numclasses
-        self.mean, self.std = dataset.get_input_mean_std()
+
+        if dataset is not None:
+            self.numclasses = dataset.numclasses
+            self.mean, self.std = dataset.get_input_mean_std()
+            self.class_names = dataset.get_class_names()
+        else:
+            io_spec = self.load_io_specification(modelpath)
+            input_1 = IOInterface.find_spec(io_spec, 'input', 'input_1')
+            out_layer = IOInterface.find_spec(io_spec, 'output', 'out_layer')
+
+            self.mean = input_1['mean']
+            self.std = input_1['std']
+            self.class_names = out_layer['class_names']
+            self.numclasses = len(self.class_names)
+
         super().__init__(
             modelpath,
             dataset,
             from_file
         )
 
+    @classmethod
+    def from_argparse(
+            cls,
+            dataset: Dataset,
+            args,
+            from_file: bool = True):
+        return cls(
+            args.model_path,
+            dataset,
+            from_file
+        )
+
     def get_io_specification_from_model(self):
+        mean = self.mean
+        std = self.std
+        if isinstance(mean, np.ndarray):
+            mean = mean.tolist()
+        if isinstance(std, np.ndarray):
+            std = std.tolist()
         return {
-            'input': [{'name': 'input_1', 'shape': (1, 224, 224, 3), 'dtype': 'float32'}],  # noqa: E501
-            'output': [{'name': 'out_layer', 'shape': (1, self.numclasses), 'dtype': 'float32'}]  # noqa: E501
+            'input': [{
+                'name': 'input_1',
+                'shape': (1, 224, 224, 3),
+                'dtype': 'float32',
+                'mean': mean,
+                'std': std
+            }],
+            'output': [{
+                'name': 'out_layer',
+                'shape': (1, self.numclasses),
+                'dtype': 'float32',
+                'class_names': self.class_names
+            }],
         }
 
     def prepare_model(self):

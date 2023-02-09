034f9348e403996fd2ca3d7bec847c00c3ffdb50
Maciej Torhan <mtorhan@antmicro.com>
2023-02-09 14:29:38 +0100

    [#41073] tests: core: test_dataset: Add data length and train test split tests, add docstrings
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/core/test_dataset.py
+++ kenning/tests/core/test_dataset.py
@@ -21,6 +21,9 @@ class TestDataset:
         for dataset_cls in DATASET_SUBCLASSES
     ])
     def test_folder_does_not_exist(self, dataset_cls: Type[Dataset]):
+        """
+        Tests throwing exception when there is no folder with data.
+        """
         dataset_download_dir = get_dataset_download_path(dataset_cls)
         if os.path.isdir(dataset_download_dir):
             shutil.rmtree(dataset_download_dir)
@@ -44,6 +47,9 @@ class TestDataset:
         for dataset_cls in DATASET_SUBCLASSES
     ])
     def test_download(self, dataset_cls: Type[Dataset]):
+        """
+        Tests dataset downloading.
+        """
         dataset_download_dir = get_dataset_download_path(dataset_cls)
         if os.path.isdir(dataset_download_dir):
             shutil.rmtree(dataset_download_dir)
@@ -66,6 +72,9 @@ class TestDataset:
         for dataset_cls in DATASET_SUBCLASSES
     ])
     def test_iterator(self, dataset_cls: Type[Dataset]):
+        """
+        Tests dataset iteration.
+        """
         try:
             dataset = get_dataset(dataset_cls)
             assert len(dataset.dataX) > 0
@@ -81,3 +90,70 @@ class TestDataset:
                 break
 
         assert len(dataset) > 0
+
+    @pytest.mark.parametrize('dataset_cls', [
+        pytest.param(dataset_cls, marks=[
+            pytest.mark.dependency(
+                depends=[f'test_download[{dataset_cls.__name__}]']
+            ),
+            pytest.mark.xdist_group(name=f'TestDataset_{dataset_cls.__name__}')
+        ])
+        for dataset_cls in DATASET_SUBCLASSES
+    ])
+    def test_data_equal_length(self, dataset_cls: Type[Dataset]):
+        """
+        Tests dataset iteration.
+        """
+        try:
+            dataset = get_dataset(dataset_cls)
+            assert len(dataset.dataX) > 0
+        except CannotDownloadDatasetError:
+            pytest.xfail('Cannot download dataset.')
+        except Exception as e:
+            pytest.fail(f'Exception {e}')
+
+        assert len(dataset) == len(dataset.dataX)
+        assert len(dataset.dataX) == len(dataset.dataY)
+
+    @pytest.mark.parametrize('dataset_cls', [
+        pytest.param(dataset_cls, marks=[
+            pytest.mark.dependency(
+                depends=[f'test_download[{dataset_cls.__name__}]']
+            ),
+            pytest.mark.xdist_group(name=f'TestDataset_{dataset_cls.__name__}')
+        ])
+        for dataset_cls in DATASET_SUBCLASSES
+    ])
+    def test_train_test_split(self, dataset_cls: Type[Dataset]):
+        """
+        Tests the `train_test_split_representations` method.
+        """
+        try:
+            dataset = get_dataset(dataset_cls)
+            assert len(dataset.dataX) > 0
+        except CannotDownloadDatasetError:
+            pytest.xfail('Cannot download dataset.')
+        except Exception as e:
+            pytest.fail(f'Exception {e}')
+
+        test_fraction = 0.25
+        dataXtrain, dataXtest, dataYtrain, dataYtest = \
+            dataset.train_test_split_representations(test_fraction)
+
+        assert len(dataXtrain) > 0
+        assert len(dataXtest) > 0
+        assert len(dataYtrain) > 0
+        assert len(dataYtest) > 0
+        assert len(dataXtrain) + len(dataXtest) == len(dataset.dataX)
+        assert len(dataYtrain) + len(dataYtest) == len(dataset.dataY)
+
+        tolerance = 1./len(dataset)
+
+        assert (len(dataXtrain)/len(dataset.dataX)
+                == pytest.approx(1 - test_fraction, abs=tolerance))
+        assert (len(dataYtrain)/len(dataset.dataY)
+                == pytest.approx(1 - test_fraction, abs=tolerance))
+        assert (len(dataXtest)/len(dataset.dataX)
+                == pytest.approx(test_fraction, abs=tolerance))
+        assert (len(dataYtest)/len(dataset.dataY)
+                == pytest.approx(test_fraction, abs=tolerance))

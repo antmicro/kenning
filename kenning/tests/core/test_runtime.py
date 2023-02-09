0aef5452fd5e7bdef1fbfe61a9a68bc0586d29ba
Maciej Torhan <mtorhan@antmicro.com>
2023-02-09 14:31:59 +0100

    [#41073] tests: core: test_runtime: Add prepare local test, add docstrings
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/core/test_runtime.py
+++ kenning/tests/core/test_runtime.py
@@ -27,32 +27,57 @@ class TestRuntime:
         for runtime_cls, inputtype in RUNTIME_INPUTTYPES
     ])
     def test_initializer(self, runtime_cls: Type[Runtime], inputtype: str):
-
+        """
+        Tests runtime initialization.
+        """
         try:
             _, model = get_default_dataset_model(inputtype)
-            _ = runtime_cls(protocol=None, modelpath=model)
         except UnknownFramework:
             pytest.xfail(f'Unknown framework: {inputtype}')
-        except Exception as e:
-            pytest.fail(f'Exception {e}')
+
+        _ = runtime_cls(protocol=None, modelpath=model)
 
     @pytest.mark.parametrize('runtime_cls,inputtype', [
         pytest.param(runtime_cls, inputtype, marks=[
             pytest.mark.dependency(
+                name=f'test_prepare_local[{runtime_cls.__name__}]',
                 depends=[f'test_initializer[{runtime_cls.__name__}]']
             ),
             pytest.mark.xdist_group(name=f'TestRuntime_{runtime_cls.__name__}')
         ])
         for runtime_cls, inputtype in RUNTIME_INPUTTYPES
     ])
-    def test_inference(self, runtime_cls: Type[Runtime], inputtype: str):
+    def test_prepare_local(self, runtime_cls: Type[Runtime], inputtype: str):
+        """
+        Tests the `preprocess_input` method.
+        """
         try:
             dataset, model = get_default_dataset_model(inputtype)
-            runtime = runtime_cls(None, modelpath=model.modelpath)
-
-            runtime.run_locally(dataset, model, str(model.modelpath))
-
         except UnknownFramework:
             pytest.xfail(f'Unknown framework: {inputtype}')
-        except Exception as e:
-            pytest.fail(f'Exception {e}')
+
+        runtime = runtime_cls(None, modelpath=model.modelpath)
+
+        assert runtime.prepare_local()
+
+    @pytest.mark.parametrize('runtime_cls,inputtype', [
+        pytest.param(runtime_cls, inputtype, marks=[
+            pytest.mark.dependency(
+                depends=[f'test_prepare_local[{runtime_cls.__name__}]']
+            ),
+            pytest.mark.xdist_group(name=f'TestRuntime_{runtime_cls.__name__}')
+        ])
+        for runtime_cls, inputtype in RUNTIME_INPUTTYPES
+    ])
+    def test_inference(self, runtime_cls: Type[Runtime], inputtype: str):
+        """
+        Tests the `run_locally` method.
+        """
+        try:
+            dataset, model = get_default_dataset_model(inputtype)
+        except UnknownFramework:
+            pytest.xfail(f'Unknown framework: {inputtype}')
+
+        runtime = runtime_cls(None, modelpath=model.modelpath)
+
+        runtime.run_locally(dataset, model, str(model.modelpath))

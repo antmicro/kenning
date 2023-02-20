0fa5898b9eae9984e76d08aa2116c7466542b4a0
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-02-20 16:58:20 +0100

    [#41811] docs: _ext: select_image_format_html: Fixed indentation - default translator extened
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- docs/source/_ext/select_image_format_html.py
+++ docs/source/_ext/select_image_format_html.py
@@ -8,92 +8,104 @@ from pathlib import Path
 from docutils import nodes
 from sphinx.util.docutils import SphinxTranslator
 from sphinx.application import Sphinx, logger as sphinx_logger
-
-_LOGGER = logging.getLogger(__name__)
-_BACKUP_IMAGE, _BACKUP_RAW = None, None
+from sphinx.writers.html5 import HTML5Translator
 
 
-def visit_figure_image_html(self: SphinxTranslator, figure: nodes.figure):
+def extend_default_translator(app: Sphinx):
     """
-    Function executed when node is visited - right before builder
-    (for HTML files) write this node to file in proper format. It swaps
-    image with raw html node if there is avaiable image in HTML format.
+    Function extends HTML translator.
 
-    Parameters
-    ----------
-    self : SphinxTranslator
-        This function is executed as one of methods from self - instance of
-        SphinxTranslator
-    figure : nodes.figure
-        Processed node
-
-    Note
-    ----
-    To preserve original state of nodes (e.g. for other builds), this function
-    save state of image node to global variable _BACKUP_IMAGE and the orginal
-    state of node is restored during departure - function
-    departure_swap_image_html
+    Function is called when builder is initialized. For HTML format, it extend
+    currently used translator with feature for selecting images in HTML format,
+    if they're avaiable
     """
-    # Searching for image node
-    image: nodes.image = None
-    for child in figure.children:
-        if isinstance(child, nodes.image):
-            image = child
-            break
-    if image is None:
-        _LOGGER.debug("Figure node don't have an image")
+    if app.builder.format != 'html':
         return
-    if 'candidates' not in image or len(image['candidates']) == 1:
-        _LOGGER.info("Figure node have at most one candidate - "
-                     "it's not possible to check other avaiable formats")
-        return
-    if 'image/x-html' not in image['candidates']:
-        _LOGGER.debug("There is no HTML candidate for this image")
-        return
-    # Backup image
-    global _BACKUP_IMAGE, _BACKUP_RAW
-    _BACKUP_IMAGE = copy.deepcopy(image)
-    # Create raw node from image data
-    filepath = image['candidates']['image/x-html']
-    with open(Path(self.settings.env.srcdir, filepath), 'r') as fd:
-        html = fd.read()
-    raw = nodes.raw('', text=html, format='html')
-    _BACKUP_RAW = raw
-    # Replacing image with raw
-    figure.replace(image, [raw])
 
+    translator_class = app.registry.get_translator_class(app.builder)
+    if translator_class is None:
+        translator_class = HTML5Translator
 
-def departure_figure_image_html(self, figure: nodes.figure):
-    """
-    Function executed when visit in node ended - right after builder
-    (for HTML files) wrote this node to file in proper format. It swaps raw
-    html with backuped image node.
+    class SelectHTMLImageTranslator(translator_class):
+        """
+        Custom Translator for HTML, with feature for selecting images in HTML
+        format if avaiable
+        """
+        logger = logging.getLogger(__name__)
+        BACKUP_IMAGE, BACKUP_RAW = None, None
 
-    Parameters
-    ----------
-    self : SphinxTranslator
-        This function is executed as one of methods from self - instance of
-        SphinxTranslator
-    figure : nodes.figure
-        Processed node
-    """
-    # Append missing ending of figcaption
-    self.body.append('</figcaption>')
-    # Checking if there is node to restore
-    global _BACKUP_IMAGE, _BACKUP_RAW
-    if _BACKUP_IMAGE is None or _BACKUP_RAW is None or \
-            all([child != _BACKUP_RAW for child in figure.children]):
-        _LOGGER.debug("There is no image to restore")
-        return
-    # Replacing raw node with image
-    figure.replace(_BACKUP_RAW, _BACKUP_IMAGE)
-    _BACKUP_IMAGE, _BACKUP_RAW = None, None
+        def visit_image(self: SphinxTranslator, image: nodes.image):
+            """
+            Method chooses image in HTML format.
+
+            Method executed when node is visited - right before builder
+            (for HTML files) write this node to file in proper format. It swaps
+            image with raw html node if there is avaiable image in HTML format.
+            It doesn't return anything, but can have influence on output file
+            by editing self.body.
+
+            Parameters
+            ----------
+            image : nodes.image
+                Processed node
+
+            Note
+            ----
+            To preserve original state of nodes (e.g. for other builds), this
+            method save state of image node to global variable BACKUP_IMAGE and
+            the original state of node is restored during departure - method
+            depart_image
+            """
+            if 'candidates' not in image or len(image['candidates']) == 1:
+                self.logger.info(
+                    "Figure node have at most one candidate - it's "
+                    "not possible to check other avaiable formats")
+                return super().visit_image(image)
+            if 'image/x-html' not in image['candidates']:
+                self.logger.debug("There is no HTML candidate for this image")
+                return super().visit_image(image)
+            # Backup image
+            self.BACKUP_IMAGE = copy.deepcopy(image)
+            # Create raw node from image data
+            filepath = image['candidates']['image/x-html']
+            with open(Path(self.settings.env.srcdir, filepath), 'r') as fd:
+                html = fd.read()
+            raw = nodes.raw('', text=html, format='html')
+            self.BACKUP_RAW = raw
+            # Replacing image with raw
+            image.replace_self([raw])
+            super().dispatch_visit(raw)
+
+        def depart_image(self, image: nodes.image):
+            """
+            Method reverts changes made by visit_image.
+
+            Method executed when visit in node ended - right after builder
+            (for HTML files) wrote this node to file in proper format. It swaps
+            raw html with backuped image node. It doesn't return anything, but
+            can have influence on output file by editing self.body.
+
+            Parameters
+            ----------
+            image : nodes.image
+                Processed node
+            """
+            if self.BACKUP_IMAGE is None:
+                return super().depart_image(image)
+            try:
+                super().dispatch_departure(self.BACKUP_RAW)
+            except NotImplementedError:
+                self.logger.info("There is no depart visitor for raw")
+            image.replace_self([self.BACKUP_IMAGE])
+            self.BACKUP_IMAGE, self.BACKUP_RAW = None, None
+
+    SelectHTMLImageTranslator.logger.setLevel(
+        sphinx_logger.getEffectiveLevel())
+    app.set_translator('html', SelectHTMLImageTranslator)
 
 
 def setup(app: Sphinx):
-    _LOGGER.setLevel(sphinx_logger.getEffectiveLevel())
-    app.add_node(nodes.figure, override=False, html=(
-        visit_figure_image_html, departure_figure_image_html))
+    app.connect('builder-inited', extend_default_translator)
 
     return {
         'version': '0.1',

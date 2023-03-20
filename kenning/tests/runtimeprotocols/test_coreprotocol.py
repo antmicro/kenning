64b2c3ee04533a67e924f32a281ca5c630fbac61
Maciej Torhan <mtorhan@antmicro.com>
2023-03-20 16:00:04 +0100

    [#42337] tests: runtimeprotocols: test_coreprotocol: Adapt protocol test to refactored protocol
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/runtimeprotocols/test_coreprotocol.py
+++ kenning/tests/runtimeprotocols/test_coreprotocol.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from kenning.core.measurements import Measurements
 from kenning.core.runtimeprotocol import RuntimeProtocol, MessageType
 from typing import Tuple, List
 import pytest
@@ -64,32 +63,30 @@ class TestCoreRuntimeProtocol:
 
         Returns
         -------
-        Tuple[bytes, List[bytes]] :
-            A tuple containing bytes stream and expected output
+        bytes :
+            Byte array of random data
         """
         data = bytes()
-        answer = list()
         for i in range(random.randint(1, 10)):
             times = random.randint(1, 10)
-            answer.append(bytes())
             tmp_order = bytes()
-            for j in range(times):
-                number = (random.randint(1, 4294967295))
+            for _ in range(times):
+                number = random.randint(0, 0xFFFFFFFF)
                 num_bytes = number.to_bytes(4, byteorder='little',
                                             signed=False)
                 tmp_order += num_bytes
-                answer[i] += num_bytes
             length = len(tmp_order)
             length = length.to_bytes(4, byteorder='little', signed=False)
             data += length + tmp_order
-        return data, answer
+        return data
 
     def test_download_statistics(self):
         """
         Tests the `RuntimeProtocol.download_statistics()` method.
         """
         client = self.initprotocol()
-        assert isinstance(client.download_statistics(), Measurements)
+        with pytest.raises(NotImplementedError):
+            client.download_statistics()
 
     def test_initialize_server(self):
         protocol = self.initprotocol()
@@ -100,41 +97,6 @@ class TestCoreRuntimeProtocol:
         protocol = self.initprotocol()
         with pytest.raises(NotImplementedError):
             protocol.initialize_client()
-
-    def test_wait_for_activity(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.wait_for_activity()
-
-    def test_send_data(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.send_data(b'')
-
-    def test_receive_data(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.receive_data()
-
-    def test_upload_input(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.upload_input(b'')
-
-    def test_upload_model(self, tmpfolder):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.upload_model(tmpfolder)
-
-    def test_upload_io_specification(self, tmpfolder):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.upload_io_specification(tmpfolder)
-
-    def test_download_output(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.download_output()
 
     def test_request_processing(self):
         protocol = self.initprotocol()
@@ -150,11 +112,6 @@ class TestCoreRuntimeProtocol:
         protocol = self.initprotocol()
         with pytest.raises(NotImplementedError):
             protocol.request_failure()
-
-    def test_parse_message(self):
-        protocol = self.initprotocol()
-        with pytest.raises(NotImplementedError):
-            protocol.parse_message(bytes())
 
     def test_disconnect(self):
         protocol = self.initprotocol()

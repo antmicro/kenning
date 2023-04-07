2051bb200710e343fae053eb3e04db4ffd68bac6
Maciej Torhan <mtorhan@antmicro.com>
2023-04-07 13:31:49 +0200

    [#43227] core: dataprovider: Remove methods that create argument parser and schema
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/core/dataprovider.py
+++ kenning/core/dataprovider.py
@@ -7,10 +7,8 @@ Provides an API for gathering and preparing data from external sources
 """
 
 from typing import Any, Dict, Tuple
-import argparse
 
 from kenning.core.runner import Runner
-from kenning.utils.args_manager import add_parameterschema_argument
 
 
 class DataProvider(Runner):
@@ -43,27 +41,6 @@ class DataProvider(Runner):
         )
 
     @classmethod
-    def form_argparse(cls):
-        """
-        Creates argparse parser for the DataProvider object.
-
-        This method is used to create a list of arguments for the object so
-        it is possible to configure the object from the level of command
-        line.
-
-        Returns
-        -------
-        (ArgumentParser, ArgumentGroup) :
-            tuple with the argument parser object that can act as parent for
-            program's argument parser, and the corresponding arguments' group
-            pointer
-        """
-        parser = argparse.ArgumentParser(add_help=False)
-        group = parser.add_argument_group(title='DataProvider arguments')
-
-        return parser, group
-
-    @classmethod
     def from_argparse(cls, args):
         """
         Constructor wrapper that takes the parameters from argparse args.
@@ -81,16 +58,6 @@ class DataProvider(Runner):
         DataProvider : object of class DataProvider
         """
         return cls()
-
-    @classmethod
-    def form_parameterschema(cls):
-        parameterschema = cls._form_parameterschema()
-        if cls.arguments_structure != DataProvider.arguments_structure:
-            add_parameterschema_argument(
-                parameterschema,
-                cls.arguments_structure
-            )
-        return parameterschema
 
     def prepare(self):
         """

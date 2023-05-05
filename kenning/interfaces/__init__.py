c087763becf03423df5b21c7326b9eeb32032758
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-05-05 17:10:19 +0200

    [#44443] interfaces: Add missing init file
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ kenning/interfaces/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides implementation of interface used by other Kenning components to manage
+their input and output types
+"""

27455866d2a49fb953c961a3b9c8399c77517ac1
Maciej Torhan <mtorhan@antmicro.com>
2023-05-29 10:46:58 +0200

    [#44957] datasets: visual_wake_words_dataset: Move preprocessing to model, fix labeling
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/datasets/visual_wake_words_dataset.py
+++ kenning/datasets/visual_wake_words_dataset.py
@@ -66,28 +66,12 @@ class VisualWakeWordsDataset(Dataset):
             'default': .005,
             'type': float
         },
-        'central_fraction': {
-            'argparse_name': '--central-fraction',
-            'description': 'Fraction used to crop images during preprocessing',
-            'default': .875,
-            'type': float
-        },
         'image_memory_layout': {
             'argparse_name': '--image-memory-layout',
             'description': 'Determines if images should be delivered in NHWC '
                            'or NCHW format',
             'default': 'NHWC',
             'enum': ['NHWC', 'NCHW']
-        },
-        'image_width': {
-            'description': 'Width of the input images',
-            'type': int,
-            'default': 96
-        },
-        'image_height': {
-            'description': 'Height of the input images',
-            'type': int,
-            'default': 96
         }
     }
 
@@ -100,18 +84,12 @@ class VisualWakeWordsDataset(Dataset):
             dataset_type: str = 'val2017',
             objects_class: str = 'person',
             area_threshold: float = .005,
-            central_fraction: float = .875,
-            image_memory_layout: str = 'NHWC',
-            image_width: int = 96,
-            image_height: int = 96):
+            image_memory_layout: str = 'NHWC'):
         assert image_memory_layout in ['NHWC', 'NCHW']
         self.dataset_type = dataset_type
         self.objects_class = objects_class
         self.area_threshold = area_threshold
-        self.central_fraction = central_fraction
         self.image_memory_layout = image_memory_layout
-        self.image_width = image_width
-        self.image_height = image_height
         self.numclasses = 2
         self.classnames = ['not-person', 'person']
         self.log = get_logger()
@@ -156,43 +134,42 @@ class VisualWakeWordsDataset(Dataset):
             self.classmap[classid] = self.coco.cats[classid]['name']
 
         cocokeys = list(self.coco.imgs.keys())
-        self.keystoimgs = dict()
+        keystoimgs = dict()
+        filepathtoarea = dict()
 
         for key, imgdata in zip(cocokeys, self.coco.loadImgs(cocokeys)):
             filepath = str(
                 self.root / self.dataset_type / imgdata['file_name']
             )
             self.dataX.append(filepath)
-            self.keystoimgs[key] = filepath
+            keystoimgs[key] = filepath
+            filepathtoarea[filepath] = imgdata['height']*imgdata['width']
 
-        classes = defaultdict(lambda: 0)
+        # compute sum of bounding boxes areas
+        bbox_areas = defaultdict(lambda: 0)
         for _, anndata in self.coco.anns.items():
+            img_file_path = keystoimgs[anndata['image_id']]
             bbox = anndata['bbox']
             bbox_area = bbox[2]*bbox[3]
-            img_area = self.image_width*self.image_height
-            if (self.objects_class == self.classmap[anndata['category_id']] and
-                    bbox_area > self.area_threshold*img_area):
-                classes[self.keystoimgs[anndata['image_id']]] |= 1
+            if self.objects_class == self.classmap[anndata['category_id']]:
+                bbox_areas[img_file_path] += bbox_area
 
-        for inputid in self.dataX:
-            self.dataY.append(classes[inputid])
+        # compare sum of all bounding boxes areas to the threshold
+        for img_file_path in self.dataX:
+            img_area = filepathtoarea[img_file_path]
+            self.dataY.append(int(
+                bbox_areas[img_file_path] > self.area_threshold*img_area
+            ))
 
     def prepare_input_samples(self, samples):
         result = []
         for imgpath in samples:
             img = cv2.imread(str(imgpath))
-            w, h = img.shape[:2]
-            img = img[int((w/2)*(1 - self.central_fraction)):
-                      int((w/2)*(1 + self.central_fraction)),
-                      int((h/2)*(1 - self.central_fraction)):
-                      int((h/2)*(1 + self.central_fraction))]
-            img = cv2.resize(img, (self.image_width, self.image_height))
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY).astype(np.float32)
-            img = img/127.5 - 1
+            img = img.astype(np.float32)/255.
             if self.image_memory_layout == 'NCHW':
                 img = np.transpose(img, (2, 0, 1))
-            img = np.expand_dims(img, -1)
             result.append(img)
+
         return result
 
     def evaluate(self, predictions, truth):

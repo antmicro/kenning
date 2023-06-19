5407b14045426475b1be0c9336ecaed3239c25bf
Maciej Torhan <mtorhan@antmicro.com>
2023-06-19 11:29:20 +0200

    [#45844] datasets: pet_dataset: Add force download param
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/datasets/pet_dataset.py
+++ kenning/datasets/pet_dataset.py
@@ -67,6 +67,7 @@ class PetDataset(Dataset):
             root: Path,
             batch_size: int = 1,
             download_dataset: bool = False,
+            force_download_dataset: bool = False,
             external_calibration_dataset: Optional[Path] = None,
             split_fraction_test: float = 0.2,
             split_fraction_val: Optional[float] = None,
@@ -87,7 +88,10 @@ class PetDataset(Dataset):
         batch_size : int
             The batch size
         download_dataset : bool
-            True if dataset should be downloaded first
+            Downloads the dataset before taking any action. If the dataset
+            files are already downloaded then they are not downloaded again
+        force_download_dataset : bool
+            Forces dataset download
         external_calibration_dataset : Optional[Path]
             Path to the external calibration dataset that can be used for
             quantizing the model. If it is not provided, the calibration
@@ -123,6 +127,7 @@ class PetDataset(Dataset):
             root,
             batch_size,
             download_dataset,
+            force_download_dataset,
             external_calibration_dataset,
             split_fraction_test,
             split_fraction_val,
@@ -135,6 +140,7 @@ class PetDataset(Dataset):
             args.dataset_root,
             args.inference_batch_size,
             download_dataset=args.download_dataset,
+            force_download_dataset=args.force_download_dataset,
             external_calibration_dataset=args.external_calibration_dataset,
             classify_by=args.classify_by,
             image_memory_layout=args.image_memory_layout

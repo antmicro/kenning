75d3a6b148f4a34cfd8e2d252658c54d36db25bc
Pawel Cichowski <pcichowski@internships.antmicro.com>
2023-07-05 15:22:44 +0200

    [#46392] scenarios: class_info: Evaluate argument enums instead of printing raw code
    Signed-off-by: Pawel Cichowski <pcichowski@internships.antmicro.com>

--- kenning/scenarios/class_info.py
+++ kenning/scenarios/class_info.py
@@ -27,7 +27,12 @@ KEYWORDS = ['inputtypes', 'outputtypes', 'arguments_structure']
 
 def print_class_module_docstrings(syntax_node: Union[ast.ClassDef, ast.Module]):
     """
-    Prints formatted docstrings of modules and classes
+    Displays docstrings of provided class or module
+
+    Parameters
+    ----------
+    syntax_node: Union[ast.ClassDef, ast.Module]
+        Syntax node representing a class or module
     """
 
     docstring = ast.get_docstring(syntax_node, clean=True)
@@ -51,6 +56,15 @@ def print_class_module_docstrings(syntax_node: Union[ast.ClassDef, ast.Module]):
 
 def get_dependency(syntax_node: Union[ast.Import, ast.ImportFrom]) \
         -> str:
+    """
+    Extracts a dependency from an import syntax node and checks whether the
+     dependency is satisfied. It also skips internal kenning modules
+
+    Parameters
+    ----------
+    syntax_node: Union[ast.Import, ast.ImportFrom]
+        An assignment like `from iree.compiler import version``
+    """
     for dependency in syntax_node.names:
         module_path = ''
         dependency_path = ''
@@ -77,40 +91,115 @@ def get_dependency(syntax_node: Union[ast.Import, ast.ImportFrom]) \
 
 
 def print_input_specification(syntax_node: ast.Assign):
+    """
+    Displays information about the input specification as bullet points
+
+    Parameters
+    ----------
+    syntax_node: ast.Assign
+        An assignment like `inputtypes = []`
+    """
+
+    if isinstance(syntax_node.value, ast.List) \
+            and len(syntax_node.value.elts) == 0:
+        print('')
+        return
+
     for input_format in syntax_node.value.keys:
         print(f'* {input_format.value}')
     print('')
 
 
 def print_output_specification(syntax_node: ast.Assign):
+    """
+    Displays information about the output specification as bullet points
+
+    Parameters
+    ----------
+    syntax_node: ast.Assign
+        An assignment like `outputtypes = ['iree']`
+    """
     for output_format in syntax_node.value.elts:
         print(f'* {output_format.value}')
     print('')
 
 
-def print_arguments_structure(syntax_node: ast.Assign):
+def print_arguments_structure(syntax_node: ast.Assign, source_path: str):
+    """
+    Displays information about the argument structure specification as
+     bullet points
+
+    Parameters
+    ----------
+    syntax_node: ast.Assign
+        An assignment like `arguments_structure = {'compiler_args': {}}`
+    """
     for argument, argument_specification_dict in zip(syntax_node.value.keys,
                                                      syntax_node.value.values):
         print(f'* {argument.value}')
 
         for key, value in zip(argument_specification_dict.keys,
                               argument_specification_dict.values):
-            key_str = astunparse.unparse(key).strip().\
-                removeprefix("'").removesuffix("'")
-            value_str = astunparse.unparse(value).strip().\
-                removeprefix("'").removesuffix("'")
 
-            print(f'  * {key_str}: {value_str}')
+            if isinstance(value, ast.Call) and value.func.id == 'list':
+                argument_list_variable = astunparse.unparse(value).strip(). \
+                    removeprefix("'").removesuffix("'").replace('list(',
+                                                                '').replace(
+                    '.keys())', '')
+
+                argument_keys = evaluate_argument_list(argument_list_variable,
+                                                       source_path)
+
+                key_str = astunparse.unparse(key).strip(). \
+                    removeprefix("'").removesuffix("'")
+                print(f'  * {key_str}')
+                for argument_key in argument_keys:
+                    print(f'    * {argument_key}')
+            else:
+
+                key_str = astunparse.unparse(key).strip(). \
+                    removeprefix("'").removesuffix("'")
+                value_str = astunparse.unparse(value).strip(). \
+                    removeprefix("'").removesuffix("'")
+
+                print(f'  * {key_str}: {value_str}')
+
+
+def evaluate_argument_list(argument_list_name: str, source_path: str) \
+        -> List[str]:
+    with open(source_path) as file:
+        parsed_file = ast.parse(file.read())
+
+    syntax_nodes = ast.walk(parsed_file)
+
+    argument_list_keys = []
+
+    for node in syntax_nodes:
+        if not isinstance(node, ast.Assign):
+            continue
+
+        if not isinstance(node.targets[0], ast.Name):
+            continue
+
+        if not node.targets[0].id == argument_list_name:
+            continue
+
+        for key in node.value.keys:
+            argument_list_keys.append(key.value)
+        break
+
+    return argument_list_keys
 
 
 def generate_class_info(target: str):
     """
-    Wrapper function that displays information about a given class
+    Wrapper function that handles displaying information about a class
 
     Parameters
     ----------
-
-
+    target: str
+        Target class path or module name e.g. either `kenning.core.flow` or
+         `kenning/core/flow.py`
     """
 
     target_path = target
@@ -181,7 +270,13 @@ def generate_class_info(target: str):
 
     print("Arguments specification:")
     if arguments_structure_node:
-        print_arguments_structure(arguments_structure_node)
+        print_arguments_structure(arguments_structure_node, target_path)
+
+    with open(target_path, 'r') as sourcecodefd:
+        sourcecode = sourcecodefd.read()
+    tree = ast.parse(sourcecode)
+    import astpretty
+    # astpretty.pprint(tree)
 
 
 def main(argv):

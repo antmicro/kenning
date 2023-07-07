9c2a0059038bb3fffa2dd3f8903992fa0168873c
Maciej Torhan <mtorhan@antmicro.com>
2023-07-07 13:22:49 +0200

    [#46504] utils: singleton: Add singleton metaclass implementation
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ kenning/utils/singleton.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides a singleton implementation.
+"""
+
+
+class Singleton(type):
+    """
+    Metaclass that implements singleton pattern.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(
+                *args, **kwargs
+            )
+        return cls._instances[cls]

c76bc84678f5f96e424891239f2a1bc84b77e7b4
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-07-10 13:45:42 +0200

    [#45316] dataproviders: ros2_camera_node_receiver: Removed outdated from_argparse and from_json
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- kenning/dataproviders/ros2_camera_node_receiver.py
+++ kenning/dataproviders/ros2_camera_node_receiver.py
@@ -97,32 +97,6 @@ class ROS2CameraNodeDataProvider(DataProvider):
             outputs=outputs
         )
 
-    @classmethod
-    def from_argparse(cls, args):
-        return cls(
-            args.node_name,
-            args.topic_name,
-            args.output_width,
-            args.output_height
-        )
-
-    @classmethod
-    def from_json(
-            cls,
-            json_dict: Dict,
-            inputs_sources: Dict[str, Tuple[int, str]] = {},
-            inputs_specs: Dict[str, Dict] = {},
-            outputs: Dict[str, str] = {}):
-        parameterschema = cls.form_parameterschema()
-        parsed_json_dict = get_parsed_json_dict(parameterschema, json_dict)
-
-        return cls(
-            **parsed_json_dict,
-            inputs_sources=inputs_sources,
-            inputs_specs=inputs_specs,
-            outputs=outputs
-        )
-
     def prepare(self):
         if not rclpy.ok():
             rclpy.init()

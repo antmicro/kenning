9ee3717dad5c96b585d3075e6702515a551e5460
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-07-12 17:59:43 +0200

    [#42667] __main__: Add execution of Kenning CLI
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ kenning/__main__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Script running Kenning CLI
+"""
+
+from kenning.cli.cli import main
+
+main()

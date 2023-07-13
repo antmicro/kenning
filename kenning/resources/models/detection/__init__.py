e9ea38dd9d6df0f6876f491d18fd416d632e1372
Maciej Torhan <mtorhan@antmicro.com>
2023-07-13 13:48:47 +0200

    [#46504] resources: models: Remove models from resources
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/resources/models/detection/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
-#
-# SPDX-License-Identifier: Apache-2.0
--- kenning/resources/models/detection/get_yolov3_weights.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-wget https://pjreddie.com/media/files/yolov3.weights -O $SCRIPT_DIR/yolov3.weights

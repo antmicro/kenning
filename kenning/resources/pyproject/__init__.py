6e25c431559f8836ed3f74ff8443034ef8660175
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-07-14 18:21:28 +0200

    [#46748] resources: pyproject: Add functions for dependencies
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ kenning/resources/pyproject/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Module containing definition of the project
+"""
+
+PYPROJECT_FILE = "pyproject.toml"
---
+++ kenning/resources/pyproject/dependencies.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Module containing functionalities for Kenning dependencies
+"""
+
+import sys
+from typing import Dict, List
+
+if sys.version_info.minor < 9:
+    from importlib_resources import path
+else:
+    from importlib.resources import path
+if sys.version_info.minor < 11:
+    import tomli as tomllib
+else:
+    import tomllib
+
+from kenning.resources.pyproject import PYPROJECT_FILE
+
+
+DEPENDENCIES: Dict[str, List[str]] = None
+
+
+def get_all_dependencies() -> Dict[str, List[str]]:
+    """
+    Gets Kenning dependencies.
+
+    It buffers them, to avoid reading file multiple times.
+
+    Returns
+    -------
+    Dict[str, List[str]] :
+        Optional and normal dependencies from pyproject.toml
+    """
+    global DEPENDENCIES
+    if DEPENDENCIES is None:
+        DEPENDENCIES = _parse_dependencies()
+    return DEPENDENCIES
+
+
+def _parse_dependencies() -> Dict[str, List[str]]:
+    """
+    Reads pyproject.toml and returns dependencies.
+
+    Returns
+    -------
+    Dict[str, List[str]] :
+        Optional and normal dependencies from pyproject.toml
+    """
+    with path(__package__, PYPROJECT_FILE) as dependencies:
+        with open(dependencies, 'rb') as fd:
+            pyproject = tomllib.load(fd)
+    return pyproject['project']['optional-dependencies'] | {
+        "kenning": pyproject['project']['dependencies']
+    }

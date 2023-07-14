15dfe5ff85c3e989b3a3fc7cd98c5f275ed4cd2b
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-07-14 18:18:02 +0200

    [#46748] __init__: Override sys.excepthook function
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- kenning/__init__.py
+++ kenning/__init__.py
@@ -15,4 +15,11 @@ This module consists of benchmarking tools and scenarios for checking:
 import os
 import sys
 
+from kenning.utils.excepthook import kenning_missing_import_excepthook
+
+
+# Extend execpthook function to handle ModuleNotFoundError
+if os.environ.get("USE_DEFAULT_EXCEPTHOOK", None) is None:
+    sys.excepthook = kenning_missing_import_excepthook
+
 sys.path.insert(0, os.path.abspath(__file__ + '../'))

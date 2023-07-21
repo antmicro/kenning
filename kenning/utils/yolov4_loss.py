3207290ac0270385e52ab088607f9821353b7a4c
Maciej Torhan <mtorhan@antmicro.com>
2023-07-21 13:24:28 +0200

    [#46504] utils: yolov4_loss: Change model path
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/utils/yolov4_loss.py
+++ kenning/utils/yolov4_loss.py
@@ -7,7 +7,8 @@ from kenning.modelwrappers.detectors.yolov4 import ONNXYOLOV4
 class YOLOv4Loss(object):
     def __init__(self):
         self.model_wrapper = ONNXYOLOV4(
-            'kenning/resources/models/detection/yolov4.onnx', None)
+            'kenning:///models/detection/yolov4.onnx', None
+        )
 
     def __call__(self, output: List[torch.Tensor], target: List):
         return self.model_wrapper.loss_torch(output, target)

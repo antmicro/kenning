6a9a11ce72dad2d3f98af96e0d591bbe9e04d843
Maciej Torhan <mtorhan@antmicro.com>
2023-08-03 08:18:59 +0200

    [#47498] tests: utils: test_resource_manager: Add xdist group for tests that use resources
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/utils/test_resource_manager.py
+++ kenning/tests/utils/test_resource_manager.py
@@ -30,6 +30,7 @@ def clear_cache():
 
 
 @pytest.mark.xdist_group(name='cache_test')
+@pytest.mark.xdist_group(name='use_resources')
 class TestResourceManager:
     def test_download_resource(self):
         """

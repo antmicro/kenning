71fb0cbbf4bcbc5d28a98ead44d1afe65f24be3c
Maciej Torhan <mtorhan@antmicro.com>
2023-08-18 13:53:33 +0200

    [#41070] scripts: optimization-tflite-tvm-tensorflow-magic-wand: Add Magic Wand optimization script
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ scripts/optimization-tflite-tvm-tensorflow-magic-wand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+python -m kenning.scenarios.optimization_runner \
+    --json-cfg scripts/optimizationconfigs/tvm-tflite-tensorflow-magic-wand.json \
+    --output build/tvm-tflite-pipeline-optimization-output.json \
+    --verbosity INFO

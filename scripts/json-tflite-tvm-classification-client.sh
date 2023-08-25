b6969281932539ad750c40041120d3a6216cb313
Maciej Torhan <mtorhan@antmicro.com>
2023-08-25 09:11:05 +0200

    [#48319] scripts: json-tflite-tvm-classification-client: Use new CLI and client-sever config
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- scripts/json-tflite-tvm-classification-client.sh
+++ scripts/json-tflite-tvm-classification-client.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-python -m kenning.scenarios.inference_tester \
-    --json-cfg ./scripts/jsonconfigs/tflite-tvm-classification-client.json \
-    --measurements ./build/tflite-tvm-classificationjson.json \
+python -m kenning optimize test  \
+    --json-cfg ./scripts/jsonconfigs/tflite-tvm-classification-client-server.json \
+    --measurements ./build/tflite-tvm-classification.json \
     --verbosity INFO

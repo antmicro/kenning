bb5555aedcce45eec73552bbfcba6b3a1f02db0c
Maciej Torhan <mtorhan@antmicro.com>
2023-09-20 13:21:39 +0200

    [#49375] tests: runtimes: runtimetests: Remove protocol, remove client/server logic tests
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/runtimes/runtimetests.py
+++ kenning/tests/runtimes/runtimetests.py
@@ -10,17 +10,16 @@ from abc import abstractmethod
 from kenning.core.runtime import Runtime
 from kenning.core.runtime import ModelNotPreparedError
 from kenning.core.runtime import InputNotPreparedError
-from kenning.core.runtimeprotocol import RuntimeProtocol
+from kenning.core.protocol import Protocol
 from kenning.core.measurements import MeasurementsCollector
 from pytest_mock import MockerFixture
 from pytest import LogCaptureFixture
 
 
 @pytest.mark.fast
-@pytest.mark.xdist_group(name='use_socket')
 class RuntimeTests:
     runtimecls: Type[Runtime]
-    runtimeprotocolcls: Type[RuntimeProtocol]
+    protocolcls: Type[Protocol]
 
     @abstractmethod
     def initruntime(self, *args, **kwargs):
@@ -49,32 +48,6 @@ class RuntimeTests:
         runtime.inference_session_end()
         assert runtime.statsmeasurements is None
 
-    def test_close_server(self):
-        """
-        Tests the `Runtime.close_server()` method.
-        """
-        runtime = self.initruntime()
-        assert runtime.shouldwork is True
-
-        runtime.close_server()
-        assert runtime.shouldwork is False
-
-    def test_prepare_server(self):
-        """
-        Tests the `Runtime.prepare_server()` method.
-        """
-        runtime = self.initruntime()
-        with pytest.raises(NotImplementedError):
-            runtime.prepare_server()
-
-    def test_prepare_client(self):
-        """
-        Tests the `Runtime.prepare_client()` method.
-        """
-        runtime = self.initruntime()
-        with pytest.raises(NotImplementedError):
-            runtime.prepare_client()
-
     def test_prepare_input(self):
         """
         Tests the `Runtime.prepare_input()` method.
@@ -86,35 +59,6 @@ class RuntimeTests:
         Tests the `Runtime.prepare_model()` method.
         """
         raise NotImplementedError
-
-    def test_process_input(self, mocker: MockerFixture,
-                           caplog: LogCaptureFixture):
-        """
-        Tests the `Runtime.process_input()` method.
-
-        Parameters
-        ----------
-        mocker: MockerFixture
-            Fixture to provide changes to source code
-        caplog: LogCaptureFixture
-            Fixture to read logs
-        """
-        mocker.patch(
-            'kenning.core.runtimeprotocol.RuntimeProtocol.request_success',
-            lambda x: None
-                     )
-        mocker.patch(
-            'kenning.core.runtime.Runtime._run',
-            lambda x: None
-                     )
-
-        import logging
-        log_messages = ('Processing input', 'Input processed')
-        runtime = self.initruntime()
-        caplog.set_level(logging.DEBUG)
-        runtime.process_input(b'')
-        for i in range(len(log_messages)):
-            assert caplog.records[i].msg == log_messages[i]
 
     def test_run(self):
         """
@@ -135,30 +79,6 @@ class RuntimeTests:
         runtime = self.initruntime()
         assert b'{}' == runtime.upload_stats(b'')
 
-    def test_upload_essentials(self, mocker: MockerFixture):
-        """
-        Tests the `Runtime.upload_essentials()` method.
-
-        Parameters
-        ----------
-        mocker: MockerFixture
-            Fixture to provide changes to source code
-        """
-        from pathlib import Path
-        import uuid
-
-        runtime = self.initruntime()
-        path = Path(uuid.uuid4().hex)
-
-        def moc_func(self, model_path: Path):
-            assert model_path.resolve() == path.resolve()
-            return
-
-        mocker.patch(
-            'kenning.core.runtimeprotocol.RuntimeProtocol.upload_model',
-            moc_func)
-        runtime.upload_essentials(path)
-
     def test_prepare_local(self):
         """
         Tests the `Runtime.prepare_local()` method.
@@ -168,12 +88,8 @@ class RuntimeTests:
 
 @pytest.mark.usefixtures('runtimemodel')
 class RuntimeWithModel(RuntimeTests):
-    runtimeprotocolcls = RuntimeProtocol
-
     def initruntime(self, *args, **kwargs):
-        runtime = self.runtimecls(self.runtimeprotocolcls(),
-                                  self.runtimemodel,
-                                  *args, **kwargs)
+        runtime = self.runtimecls(self.runtimemodel, *args, **kwargs)
         return runtime
 
     def test_prepare_model(self):

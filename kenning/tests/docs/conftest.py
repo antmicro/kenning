27db6cb474a5d67b8cee3c83d23cd46d09e0893d
Maciej Torhan <mtorhan@antmicro.com>
2023-10-04 14:53:15 +0200

    [#49921] tests: docs: conftests: Add typing
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/tests/docs/conftest.py
+++ kenning/tests/docs/conftest.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 @pytest.fixture
-def docs_log_dir(request) -> Path:
+def docs_log_dir(request: pytest.FixtureRequest) -> Path:
     log_dir = Path(request.config.getoption('--test-docs-log-dir'))
     log_dir.mkdir(exist_ok=True)
     return log_dir

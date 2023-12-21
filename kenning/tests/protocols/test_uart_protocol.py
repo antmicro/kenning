b3e99dfff84c32ddaa9ad50fcf2b4d0b92422d78
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-12-21 15:47:49 +0100

    [#52228] tests: protocols: test_uart_protocol: Make sure "pretrained_model_uri" is not None
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- kenning/tests/protocols/test_uart_protocol.py
+++ kenning/tests/protocols/test_uart_protocol.py
@@ -46,6 +46,7 @@ MODEL_WRAPPER_SUBCLASSES_WITH_IO_SPEC = [
     modelwrapper_cls
     for modelwrapper_cls in MODEL_WRAPPER_SUBCLASSES
     if hasattr(modelwrapper_cls, "pretrained_model_uri")
+    and modelwrapper_cls.pretrained_model_uri is not None
 ]
 
 

89a690e3f11010d5f578d21846abb4dd5c4caf48
Eryk Szpotanski <eszpotanski@antmicro.com>
2024-02-14 17:13:47 +0100

    [#54203] kenning: Set __version__ for Kenning module
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- kenning/__init__.py
+++ kenning/__init__.py
@@ -14,11 +14,32 @@ This module consists of benchmarking tools and scenarios for checking:
 
 import os
 import sys
+from importlib.metadata import PackageNotFoundError, version
 
 from kenning.utils.excepthook import kenning_missing_import_excepthook
+from kenning.utils.logger import KLogger
 
 # Extend execpthook function to handle ModuleNotFoundError
 if os.environ.get("KENNING_USE_DEFAULT_EXCEPTHOOK", None) is None:
     sys.excepthook = kenning_missing_import_excepthook
 
 sys.path.insert(0, os.path.abspath(__file__ + "../"))
+
+try:
+    __version__ = version("kenning")
+except PackageNotFoundError:
+    # assumes Kenning source code is used
+    import re
+    from pathlib import Path
+
+    try:
+        __version__ = re.search(
+            r'version = "([^"]+)"',
+            (Path(__file__).parent.parent / "pyproject.toml").read_text(),
+        ).group(1)
+    except Exception:
+        KLogger.error(
+            "Version of Kenning cannot be retrieved, "
+            "please install Kenning through `pip`"
+        )
+        __version__ = None

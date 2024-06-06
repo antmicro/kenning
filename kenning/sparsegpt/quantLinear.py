b4a37d0ea53d235fc4506ad30fcd5c32a7c60462
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-06-06 10:29:24 +0200

    [#58953] quantLinear: utils: Add modules for optimized model packing
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ kenning/sparsegpt/quantLinear.py
@@ -0,0 +1,362 @@
+# Copyright (c) 2024 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides functionality for packing the model with
+quantized weights and sparsity metadata.
+"""
+
+import math
+from typing import Optional
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+INT32_BITS = 32
+METADATA_ELEMENTS_PER_INT16 = 8
+
+
+class PackingError(Exception):
+    """
+    Exception raised when the parameters for packing method are invalid.
+    """
+
+    pass
+
+
+class QuantLinear(nn.Module):
+    """
+    Module representing a quantized linear layer that stores its weights
+    along with optimization parameters.
+    """
+
+    def __init__(
+        self,
+        bits: int,
+        group_size: int,
+        infeatures: int,
+        outfeatures: int,
+        prunen: int = 0,
+        prunem: int = 0,
+        sparse: bool = False,
+        bias: bool = False,
+        development_mode: bool = False,
+        weight_dtype: torch.dtype = torch.float16,
+    ):
+        """
+        Initialize the QuantLinear layer and prepare the parameters
+        buffers for weights.
+
+        Parameters
+        ----------
+        bits : int
+            Target precision of the weights.
+        group_size : int
+            Number of columns that are pruned and quantized at once.
+        infeatures : int
+            Number of input features.
+        outfeatures : int
+            Number of output features.
+        prunen : int
+            Number of weights pruned in semi-structured manner
+            per `prunem` weights.
+        prunem : int
+            Block size in semi-structured pruning.
+        sparse : bool
+            Flag indicating if the weights should be compressed.
+        bias : bool
+            Flag indicating if the layer should have a bias.
+        development_mode : bool
+            Flag indicating if the layer should serialize the weights
+            both in compressed and uncompressed format.
+            Additionally, sparsity checks are performed in this mode.
+            This flag may be used for debugging purposes.
+        weight_dtype : torch.dtype
+            Data type of the weights.
+
+        Raises
+        ------
+        NotImplementedError
+            Raised if the parameters passed to the constructor are invalid.
+        """
+        super().__init__()
+        if infeatures < 8 or outfeatures < 8:
+            raise NotImplementedError(
+                "Pack method requires the model to "
+                + "be at least of shape 8x8. "
+                + "Make sure the parameters are set correctly."
+            )
+        if sparse and infeatures < 16:
+            raise NotImplementedError(
+                "Sparse quantization requires the model to "
+                + "be at least of shape 16x8. "
+                + "Make sure the parameters are set correctly."
+            )
+
+        if bits != 4:
+            raise NotImplementedError("Only 4 bits are supported.")
+
+        if prunem * prunen != 0 and prunen != 2 and prunem != 4:
+            raise NotImplementedError("Only 2:4 pruning is supported. ")
+
+        self.infeatures = infeatures
+        self.outfeatures = outfeatures
+        self.bits = bits
+        self.group_size = group_size if group_size != -1 else infeatures
+        self.development_mode = development_mode
+        self.maxq = 2**self.bits - 1
+
+        self.compression_ratio = (
+            (prunem // prunen) if (prunen * prunem != 0) and sparse else 1
+        )
+
+        # Weights are quantized to `self.bits` bits and then packed
+        # into 32 bit integers. Additionally, if weights are compressed,
+        # the compression ratio is taken into account.
+        self.register_buffer(
+            "qweight",
+            torch.zeros(
+                (
+                    infeatures
+                    * self.bits
+                    // INT32_BITS
+                    // self.compression_ratio,
+                    outfeatures,
+                ),
+                dtype=torch.int32,
+            ),
+        )
+
+        self.register_buffer(
+            "qzeros",
+            torch.zeros(
+                (
+                    math.ceil(infeatures / self.group_size),
+                    outfeatures // INT32_BITS * self.bits,
+                ),
+                dtype=torch.int32,
+            ),
+        )
+        self.register_buffer(
+            "scales",
+            torch.zeros(
+                (math.ceil(infeatures / self.group_size), outfeatures),
+                dtype=weight_dtype,
+            ),
+        )
+
+        # If weights are compressed, then groups are `self.compression_ratio`
+        # times smaller.
+        self.register_buffer(
+            "g_idx",
+            torch.tensor(
+                [
+                    i // (self.group_size // self.compression_ratio)
+                    for i in range(infeatures // self.compression_ratio)
+                ],
+                dtype=torch.int32,
+            ),
+        )
+
+        if sparse:
+            self.register_buffer(
+                "qsparsity_metadata",
+                torch.zeros(
+                    (
+                        self.outfeatures,
+                        self.infeatures
+                        // self.compression_ratio
+                        // METADATA_ELEMENTS_PER_INT16,
+                    ),
+                    dtype=torch.int16,
+                ),
+            )
+
+            if development_mode:
+                self.register_buffer(
+                    "qweight_uncompressed",
+                    torch.zeros(
+                        (
+                            self.infeatures * self.bits // INT32_BITS,
+                            self.outfeatures,
+                        ),
+                        dtype=torch.int32,
+                    ),
+                )
+
+        else:
+            self.qsparsity_metadata = None
+
+        if bias:
+            self.register_buffer(
+                "bias", torch.zeros((outfeatures), dtype=weight_dtype)
+            )
+        else:
+            self.bias = None
+
+    def pack_sparsity_metadata(
+        self, sparsity_metadata: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Pack sparsity metadata into a compressed format.
+
+        The format is compatible with CUDA warp-level sparse matrix storage:
+        * https://docs.nvidia.com/cuda/parallel-thread-execution/#warp-level-sparse-matrix-storage
+
+        Parameters
+        ----------
+        sparsity_metadata : torch.Tensor
+            Sparsity metadata to compress.
+
+        Returns
+        -------
+        torch.Tensor
+            Compressed sparsity metadata.
+        """
+        target_shape = (
+            sparsity_metadata.shape[0],
+            sparsity_metadata.shape[1]
+            // self.compression_ratio
+            // METADATA_ELEMENTS_PER_INT16,
+        )
+        # Finding indices of nonzero elements
+        target_metadata = (sparsity_metadata == False).flatten().nonzero()  # noqa: E712
+        target_metadata = target_metadata.reshape(-1, 8) % 4
+
+        # Packing indices of 8 adjacent non zero elements
+        target_metadata = sum(
+            target_metadata[:, i] << (i * 2) for i in range(8)
+        )
+
+        target_metadata = target_metadata.reshape(target_shape)
+        return target_metadata
+
+    def pack(
+        self,
+        linear: nn.Linear,
+        sparsity_metadata: Optional[torch.Tensor],
+        scales: torch.Tensor,
+        zeros: torch.Tensor,
+        g_idx: torch.Tensor,
+    ):
+        """
+        Pack the weights of the linear layer into a compressed format.
+
+        Parameters
+        ----------
+        linear : nn.Linear
+            Linear layer to pack.
+        sparsity_metadata : Optional[torch.Tensor]
+            Sparsity metadata to compress.
+        scales : torch.Tensor
+            Scales used for quantization.
+        zeros : torch.Tensor
+            Zero points used for quantization.
+        g_idx : torch.Tensor
+            Group indices used for quantization.
+
+        Raises
+        ------
+        PackingError
+            Raised if the validation of the quantization process fails.
+        """
+        W = linear.weight.data.clone()
+
+        scales = scales.t().contiguous()
+        zeros = zeros.t().contiguous()
+        scale_zeros = zeros * scales
+
+        # Scales and bias are stored in their native type
+        self.scales[:] = scales.to(dtype=linear.weight.dtype)
+        if linear.bias is not None:
+            self.bias[:] = linear.bias.to(dtype=linear.weight.dtype)
+
+        # Packing g_idx indices only if the weights are not compressed.
+        # Otherwise, the indices are statically generated
+        if self.qsparsity_metadata is None:
+            self.g_idx[:] = g_idx
+
+        # Quantizing weights
+        intweight = []
+        for idx in range(self.infeatures):
+            intweight_row = torch.round(
+                (W[:, idx] + scale_zeros[g_idx[idx]]) / self.scales[g_idx[idx]]
+            ).to(torch.int)[:, None]
+
+            if self.development_mode and sparsity_metadata is not None:
+                dequantized = (
+                    intweight_row[:, 0] - zeros[g_idx[idx]]
+                ) * scales[g_idx[idx]]
+
+                if dequantized[sparsity_metadata[:, idx]].sum() != 0:
+                    raise PackingError(
+                        "Error in quantization. "
+                        + "Dequantized weights are not zero at zeroed indices."
+                    )
+
+            intweight.append(intweight_row)
+        intweight = torch.cat(intweight, dim=1)
+
+        if self.development_mode and self.qsparsity_metadata is not None:
+            # Storing uncompressed weights as well as compressed ones
+            # if the model is in development mode and is sparse
+            intweight_uncompressed = intweight.clone().t().contiguous()
+            intweight_uncompressed = intweight_uncompressed.numpy().astype(
+                np.uint32
+            )
+
+            i = 0
+            row = 0
+            while row < self.qweight_uncompressed.shape[0]:
+                for j in range(i, i + (INT32_BITS // self.bits)):
+                    self.qweight_uncompressed[row] |= intweight_uncompressed[
+                        j
+                    ] << (self.bits * (j - i))
+                i += INT32_BITS // self.bits
+                row += 1
+
+        if self.qsparsity_metadata is not None:
+            # Compressing weight matrix
+            intweight = intweight[sparsity_metadata == False].reshape(  # noqa: E712
+                self.outfeatures, self.infeatures // self.compression_ratio
+            )
+
+        intweight = intweight.t().contiguous()
+        intweight = intweight.numpy().astype(np.uint32)
+
+        i = 0
+        row = 0
+        while row < self.qweight.shape[0]:
+            for j in range(i, i + (INT32_BITS // self.bits)):
+                self.qweight[row] |= intweight[j] << (self.bits * (j - i))
+            i += INT32_BITS // self.bits
+            row += 1
+
+        # Packing zeros
+        i = 0
+        col = 0
+
+        # One has to be subtracted from zeros, as GPTQ kernels make this
+        # assumption:
+        # https://github.com/vllm-project/vllm/blob/88407532e7ec2dd3313f6cb3a31d8dd1fa868178/csrc/quantization/gptq/q_gemm.cu#L1413
+        zeros -= 1
+        zeros = zeros.numpy().astype(np.uint32)
+        while col < self.qzeros.shape[1]:
+            for j in range(i, i + (INT32_BITS // self.bits)):
+                self.qzeros[:, col] |= zeros[:, j] << (self.bits * (j - i))
+            i += INT32_BITS // self.bits
+            col += 1
+
+        # Packing sparsity metadata
+        if self.qsparsity_metadata is not None:
+            sparsity_metadata = self.pack_sparsity_metadata(sparsity_metadata)
+            self.qsparsity_metadata[:] = sparsity_metadata.to(
+                dtype=torch.int16
+            )
+
+    def forward(self, x):
+        raise NotImplementedError(
+            "QuantLinear is not a valid layer for forward pass."
+        )
---
+++ kenning/sparsegpt/utils.py
@@ -0,0 +1,227 @@
+# Copyright (c) 2024 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides functionality for preparing a quantized/pruned model
+to be serialized into a compressed format.
+"""
+
+import logging
+import time
+from typing import Any, Dict, List, Optional
+
+import coloredlogs
+import torch
+import torch.nn as nn
+
+from kenning.sparsegpt.quantLinear import QuantLinear
+
+CPU = torch.device("cpu")
+
+
+def recurse_setattr(module: nn.Module, name: str, value: Any):
+    """
+    A function to recursively set attributes to a module.
+
+    Parameters
+    ----------
+    module : nn.Module
+        Module to set the attribute
+    name : str
+        Name of the attribute
+    value : Any
+        Value to set
+    """
+    if "." not in name:
+        setattr(module, name, value)
+    else:
+        name, rest = name.split(".", 1)
+        recurse_setattr(getattr(module, name), rest, value)
+
+
+def find_layers(
+    module: nn.Module, layers: Optional[List[nn.Module]] = None, name: str = ""
+) -> Dict[str, nn.Module]:
+    """
+    Finds layers in a module and returns them as a dictionary.
+
+    Parameters
+    ----------
+    module : nn.Module
+        Module to search for layers
+    layers : Optional[List[nn.Module]]
+        List of layers to search for
+    name : str
+        Name of the layer
+
+    Returns
+    -------
+    Dict[str, nn.Module]
+        Dictionary with keys as layer names and values as layers
+    """
+    if not layers:
+        layers = [nn.Linear]
+    for layer in layers:
+        if isinstance(module, layer):
+            return {name: module}
+    res = {}
+    for name1, child in module.named_children():
+        res.update(
+            find_layers(
+                child,
+                layers=layers,
+                name=name + "." + name1 if name != "" else name1,
+            )
+        )
+    return res
+
+
+def make_quant(
+    module: nn.Module,
+    names: Dict,
+    bits: int,
+    group_size: int,
+    prunen: int,
+    prunem: int,
+    development_mode: bool,
+) -> None:
+    """
+    Replaces the layers in the model with quantized layers.
+
+    Parameters
+    ----------
+    module : nn.Module
+        Module to pack
+    names : Dict
+        Dictionary with keys as layer names and values
+        as quantization parameters
+    bits : int
+        Target precisioin of the quantized weights
+    group_size : int
+        Group size for the quantization
+    prunen : int
+        Number of pruned elements per `prunem` elements
+    prunem : int
+        Size of sparsity block
+    development_mode : bool
+        Development mode flag, determines whether the model should serialize
+        both compressed and uncompressed weights in case of a sparse model.
+        Additional checks to validate the model are also enabled.
+
+    Returns
+    -------
+    None
+    """
+    if isinstance(module, QuantLinear):
+        return
+
+    for name, submodule in module.named_modules():
+        if name in names:
+            ori_layer_device = next(submodule.parameters()).device
+
+            in_features = submodule.in_features
+            out_features = submodule.out_features
+            has_bias = submodule.bias is not None
+            weight_dtype = submodule.weight.dtype
+
+            quantization_parameters = names[name]
+
+            # Sparsity metadata is stored as a second element
+            is_sparse = quantization_parameters[1] is not None
+
+            new_layer = QuantLinear(
+                bits,
+                group_size,
+                in_features,
+                out_features,
+                prunen,
+                prunem,
+                sparse=is_sparse,
+                bias=has_bias,
+                development_mode=development_mode,
+                weight_dtype=weight_dtype,
+            )
+
+            new_layer.device = ori_layer_device
+            recurse_setattr(module, name, new_layer.to(ori_layer_device))
+
+
+def pack_model(
+    model: nn.Module,
+    quantizers: Dict,
+    bits: int,
+    group_size: int,
+    prunen: int,
+    prunem: int,
+    verbosity: str,
+    development_mode: bool,
+):
+    """
+    Pack the model with quantized weights and quantization parameters.
+
+    Parameters
+    ----------
+    model : nn.Module
+        Model to pack
+    quantizers : Dict
+        Dictionary of quantization parameters
+    bits : int
+        Target precision of the quantized weights
+    group_size : int
+        Group size for the quantization
+    prunen : int
+        Number of pruned elements per `prunem` elements
+    prunem : int
+        Size of sparsity block
+    verbosity : str
+        Logging verbosity level
+    development_mode : bool
+        Development mode flag, determines whether the model should serialize
+        both compressed and uncompressed weights in case of a sparse model.
+        Additional checks to validate the model are also enabled.
+    """
+    logger = logging.getLogger(__name__)
+    coloredlogs.install(
+        level=verbosity,
+        fmt="%(asctime)s %(name)s %(levelname)s %(message)s",
+    )
+
+    logger.info("Packing model...")
+    layers = find_layers(model)
+    layers = {n: layers[n] for n in quantizers}
+
+    if development_mode:
+        logger.debug(
+            "Development mode enabled. Additional information "
+            + "may be serialized."
+        )
+
+    make_quant(
+        model, quantizers, bits, group_size, prunen, prunem, development_mode
+    )
+    qlayers = find_layers(model, [QuantLinear])
+
+    for name in qlayers:
+        quantizers[name], sparsity_metadata, scale, zero, g_idx = quantizers[
+            name
+        ]
+        layer_device = qlayers[name].device
+        qlayers[name].to(CPU)
+
+        layers[name], sparsity_metadata, scale, zero, g_idx = (
+            layers[name].to(CPU),
+            sparsity_metadata.to(CPU)
+            if sparsity_metadata is not None
+            else None,
+            scale.to(CPU),
+            zero.to(CPU),
+            g_idx.to(CPU),
+        )
+
+        logger.debug(f"Packing layer {name}")
+        tick = time.time()
+        qlayers[name].pack(layers[name], sparsity_metadata, scale, zero, g_idx)
+        qlayers[name].to(layer_device)
+        logger.debug("Layer packing took: %.2f s" % (time.time() - tick))
+    logger.info("Model packed.")

54ccaee9551925e0b9fefe6fc195ae642890f4f7
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-09-27 23:46:42 +0200

    [#64282] pyproject.toml: Configured installation flow to include sparsity_aware_kernel

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-# Copyright (c) 2020-2023 Antmicro <www.antmicro.com>
+# Copyright (c) 2020-2024 Antmicro <www.antmicro.com>
 #
 # SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
 
 import numpy
 import setuptools
@@ -9,10 +11,19 @@ from Cython.Build import cythonize
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+project_path = (
+    Path(__file__).parent / "kenning/sparsity_aware_kernel"
+).absolute()
+
 setuptools.setup(
     packages=setuptools.find_packages(),
     long_description=long_description,
     include_package_data=True,
+    extras_require={
+        "sparsity-aware-kernel": [
+            f"kenning-sparsity-aware-kernel @ file://{project_path}"
+        ]
+    },
     ext_modules=cythonize(
         setuptools.Extension(
             "kenning.modelwrappers.instance_segmentation.cython_nms",

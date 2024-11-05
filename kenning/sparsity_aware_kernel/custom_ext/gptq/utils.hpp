43983e23b253f59db2fbc0bf9cd62e15e378f55d
Maciej Torhan <mtorhan@antmicro.com>
2024-11-05 21:54:38 +0100

    [#67802] .pre-commit-config: Add clang-format
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

--- kenning/sparsity_aware_kernel/custom_ext/gptq/utils.hpp
+++ kenning/sparsity_aware_kernel/custom_ext/gptq/utils.hpp
@@ -1,24 +1,24 @@
 #include <cuda_runtime.h>
 #include <cutlass/cutlass.h>
 
-#define CHECK_CUDA(func)                                                       \
-  {                                                                            \
-    cudaError_t status = (func);                                               \
-    if (status != cudaSuccess) {                                               \
-      std::cerr << "CUDA API failed at line: " << __LINE__                     \
-                << " with error: " << cudaGetErrorString(status) << "("        \
-                << status << ")" << std::endl;                                 \
-      exit(EXIT_FAILURE);                                                      \
-    }                                                                          \
-  }
+#define CHECK_CUDA(func)                                                                                          \
+    {                                                                                                             \
+        cudaError_t status = (func);                                                                              \
+        if (status != cudaSuccess)                                                                                \
+        {                                                                                                         \
+            std::cerr << "CUDA API failed at line: " << __LINE__ << " with error: " << cudaGetErrorString(status) \
+                      << "(" << status << ")" << std::endl;                                                       \
+            exit(EXIT_FAILURE);                                                                                   \
+        }                                                                                                         \
+    }
 
-#define CHECK_CUTLASS(func)                                                    \
-  {                                                                            \
-    cutlass::Status error = (func);                                            \
-    if (error != cutlass::Status::kSuccess) {                                  \
-      std::cerr << "CUTLASS API failed at line: " << __LINE__                  \
-                << " with error: " << cutlassGetStatusString(error)            \
-                << std::endl;                                                  \
-      exit(EXIT_FAILURE);                                                      \
-    }                                                                          \
-  }
+#define CHECK_CUTLASS(func)                                                             \
+    {                                                                                   \
+        cutlass::Status error = (func);                                                 \
+        if (error != cutlass::Status::kSuccess)                                         \
+        {                                                                               \
+            std::cerr << "CUTLASS API failed at line: " << __LINE__                     \
+                      << " with error: " << cutlassGetStatusString(error) << std::endl; \
+            exit(EXIT_FAILURE);                                                         \
+        }                                                                               \
+    }
--- kenning/sparsity_aware_kernel/custom_ext/pybind.cpp
+++ kenning/sparsity_aware_kernel/custom_ext/pybind.cpp
@@ -2,11 +2,10 @@
 
 #include "ops.h"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("uncompress_weights", &uncompress_weights, "Test for metadata");
-  m.def("unquantize_weights", &unquantize_weights, "Test for metadata");
-  m.def("reorder_metadata", &reorder_metadata,
-        "Preprocessed metadata to be used by sparse CUTLASS kernel");
-  m.def("compressed_gptq_gemm", &compressed_gptq_gemm,
-        "Quantized Compressed GEMM for GPTQ");
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("uncompress_weights", &uncompress_weights, "Test for metadata");
+    m.def("unquantize_weights", &unquantize_weights, "Test for metadata");
+    m.def("reorder_metadata", &reorder_metadata, "Preprocessed metadata to be used by sparse CUTLASS kernel");
+    m.def("compressed_gptq_gemm", &compressed_gptq_gemm, "Quantized Compressed GEMM for GPTQ");
 }

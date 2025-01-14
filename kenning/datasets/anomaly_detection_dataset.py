1592d5e269185a03579750d8fd0f53135ef16d79
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-01-14 18:17:41 +0100

    [#70406] datasets: anomaly_detection_dataset: Implement generic anomaly detection dataset
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ kenning/datasets/anomaly_detection_dataset.py
@@ -0,0 +1,187 @@
+# Copyright (c) 2020-2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Dataset wrapper for anomaly detection in time series.
+"""
+
+import shutil
+from pathlib import Path
+from typing import Any, List, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+
+from kenning.core.dataset import Dataset
+from kenning.core.measurements import Measurements
+from kenning.utils.resource_manager import ResourceURI
+
+
+class AnomalyDetectionDataset(Dataset):
+    """
+    Generic dataset for anomaly detection in time series problem.
+
+    It reads data from provided CSV file and prepares sequences of data.
+
+    CSV file is assumed to contain header, first column with timestamps
+    (will be discarded) and last column with anomaly classification.
+    """
+
+    arguments_structure = {
+        "csv_file": {
+            "argparse_name": "--csv-file",
+            "description": "Path or URL to the CSV file with dataset",
+            "type": ResourceURI,
+            "required": True,
+        },
+        "window_size": {
+            "argparse_name": "--window-size",
+            "description": "The number of consecutive timestamps included in one entry",  # noqa: E501
+            "type": int,
+            "default": 5,
+        },
+        "gather_predictions": {
+            "argparse_name": "--gather-predictions",
+            "description": "Determines whether returned measurements should include target and predicted anomalies",  # noqa: E501
+            "type": bool,
+            "default": True,
+        },
+    }
+
+    def __init__(
+        self,
+        root: Path,
+        csv_file: Union[str, ResourceURI],
+        batch_size: int = 1,
+        download_dataset: bool = True,
+        force_download_dataset: bool = False,
+        external_calibration_dataset: Optional[Path] = None,
+        split_fraction_test: float = 0.2,
+        split_fraction_val: Optional[float] = None,
+        split_seed: int = 1234,
+        window_size: int = 5,
+        gather_predictions: bool = True,
+    ):
+        """
+        Representation of dataset for anomaly detection.
+
+        Parameters
+        ----------
+        root : Path
+            The path to the dataset data.
+        csv_file : Union[str, ResourceURI]
+            Path or URL to the CSV file with dataset.
+        batch_size : int
+            The batch size.
+        download_dataset : bool
+            Downloads the dataset before taking any action. If the dataset
+            files are already downloaded then they are not downloaded again.
+        force_download_dataset : bool
+            Forces dataset download.
+        external_calibration_dataset : Optional[Path]
+            Path to the external calibration dataset that can be used for
+            quantizing the model. If it is not provided, the calibration
+            dataset is generated from the actual dataset.
+        split_fraction_test : float
+            Default fraction of data to leave for model testing.
+        split_fraction_val : Optional[float]
+            Default fraction of data to leave for model validation.
+        split_seed : int
+            Default seed used for dataset split.
+        window_size : int
+            The number of consecutive timestamps included in one entry.
+        gather_predictions : bool
+            Whether returned measurements should include target
+            and predicted sentences.
+        """
+        self.csv_file = csv_file
+        if not isinstance(self.csv_file, ResourceURI):
+            self.csv_file = ResourceURI(self.csv_file)
+        self.num_features = None
+        super().__init__(
+            root,
+            batch_size,
+            download_dataset,
+            force_download_dataset,
+            external_calibration_dataset,
+            split_fraction_test,
+            split_fraction_val,
+            split_seed,
+        )
+        self.window_size = window_size
+        self.gather_predictions = gather_predictions
+        self.classnames = self.get_class_names()
+
+    def download_dataset_fun(self):
+        self.root.mkdir(parents=True, exist_ok=True)
+        data_path = self.root / self.csv_file.stem
+        if not data_path.exists():
+            shutil.copy(self.csv_file, data_path)
+
+    def prepare(self):
+        data_path = self.root / self.csv_file.stem
+        data = pd.read_csv(data_path)
+        # Remove timestamps (first column)
+        data = data.drop(columns=[data.columns[0]])
+        data = data.to_numpy(np.float32)
+        self.dataX = data[:, :-1]
+        self.dataY = data[:, -1].astype(np.int_)
+
+        # Simplify anomaly categories into one class
+        self.dataY[self.dataY > 0] = 1
+        self.num_features = self.dataX.shape[1]
+
+    def evaluate(self, predictions, truth) -> Measurements:
+        confusion_matrix = metrics.confusion_matrix(
+            truth[0], predictions[0][: len(truth[0])], labels=[0, 1]
+        )
+        measurements = Measurements()
+        measurements.accumulate(
+            "eval_confusion_matrix",
+            confusion_matrix,
+            lambda: np.zeros((2, 2)),
+        )
+        measurements.accumulate("total", len(predictions), lambda: 0)
+        if self.gather_predictions:
+            measurements.add_measurement(
+                "predictions",
+                [
+                    {
+                        "target": t,
+                        "prediction": p,
+                    }
+                    for t, p in zip(truth[0], predictions[0])
+                ],
+            )
+        return measurements
+
+    def get_input_mean_std(self) -> Tuple[Any, Any]:
+        return np.mean(self.dataX, axis=0), np.std(self.dataX, axis=0)
+
+    def get_class_names(self) -> List[str]:
+        return ["normal", "anomaly"]
+
+    def __next__(self) -> Tuple[List, List]:
+        # Handle the case when the next method is called without the iterator
+        if self._dataindex == 0 and not self._dataindices:
+            self._dataindices = list(range(len(self.dataX)))
+
+        if self._dataindex < len(self._dataindices):
+            samples = self._dataindices[
+                self._dataindex : self._dataindex + self.batch_size
+            ]
+            self._dataindex += len(samples)
+            dataX = [
+                self.dataX[i - self.window_size + 1 : i + 1]
+                if i >= self.window_size - 1
+                else self.dataX[0 : self.window_size]
+                for i in samples
+            ]
+            dataY = [self.dataY[i] for i in samples]
+            return (
+                self.prepare_input_samples(dataX),
+                self.prepare_output_samples(dataY),
+            )
+        raise StopIteration

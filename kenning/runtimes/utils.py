0255c43503eec2bf69f29cd681827b1e4d897657
Maciej Torhan <mtorhan@antmicro.com>
2025-02-12 14:45:04 +0100

    [#71899] runtimes: utils: Add function to retrieve default runtime based on model type
    Signed-off-by: Maciej Torhan <mtorhan@antmicro.com>

---
+++ kenning/runtimes/utils.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2020-2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides helper functions for runtimes.
+"""
+
+from kenning.core.runtime import Runtime
+from kenning.utils.resource_manager import PathOrURI
+
+
+def get_default_runtime(
+    model_framework: str, model_path: PathOrURI
+) -> Runtime:
+    """
+    Returns default Runtime for given model framework.
+
+    Parameters
+    ----------
+    model_framework : str
+        Framework of the model.
+    model_path : PathOrURI
+        Path to the model.
+
+    Returns
+    -------
+    Runtime
+        Default Runtime for given model.
+    """
+    if model_framework == "tvm":
+        from kenning.runtimes.tvm import TVMRuntime
+
+        return TVMRuntime(model_path)
+
+    if model_framework == "tflite":
+        from kenning.runtimes.tflite import TFLiteRuntime
+
+        return TFLiteRuntime(model_path)
+
+    if model_framework == "torch":
+        from kenning.runtimes.pytorch import PyTorchRuntime
+
+        return PyTorchRuntime(model_path)
+
+    if model_framework == "iree":
+        from kenning.runtimes.iree import IREERuntime
+
+        return IREERuntime(model_path)

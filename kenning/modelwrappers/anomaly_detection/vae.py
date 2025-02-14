332c3358b07a2cf6ffbe53e01532605a6258dca2
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-02-14 16:48:30 +0100

    [#72231] modelwrappers: anomaly_detection: vae: Add methods and configs requaried for AutoML
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- kenning/modelwrappers/anomaly_detection/vae.py
+++ kenning/modelwrappers/anomaly_detection/vae.py
@@ -8,14 +8,16 @@ Contains Variational Autoencoder (VAE) model wrapper.
 Compatible with AnomalyDetectionDataset.
 """
 
+from enum import Enum
 from pathlib import Path
 from random import shuffle
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple, Type
 
 import numpy as np
 from sklearn.metrics import f1_score, roc_auc_score
 from tqdm import tqdm
 
+from kenning.automl.auto_pytorch import AutoPyTorchModel
 from kenning.datasets.anomaly_detection_dataset import AnomalyDetectionDataset
 from kenning.modelwrappers.frameworks.pytorch import PyTorchWrapper
 from kenning.utils.logger import KLogger, LoggerProgressBar
@@ -32,7 +34,17 @@ AVAILABLE_ACTIVATION_NAMES = [
 ]
 
 
-class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
+class TrainerParams(str, Enum):
+    """
+    Parameters for VAETrainer.
+    """
+
+    BETA = "loss_beta"
+    CAPACITY = "loss_capacity"
+    CLIP_GRAD = "clip_grad_max_norm"
+
+
+class PyTorchAnomalyDetectionVAE(PyTorchWrapper, AutoPyTorchModel):
     """
     Model wrapper for anomaly detection with VAE.
 
@@ -48,63 +60,81 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
 
     default_dataset = AnomalyDetectionDataset
     arguments_structure = {
-        "encoder_layers": {
+        "encoder_neuron_list": {
             "description": "List of dense layer dimensions of encoder",
             "type": list,
             "items": int,
             "default": [16, 8],
+            "AutoML": True,
+            "list_range": (2, 16),
+            "item_range": (4, 256),
         },
-        "decoder_layers": {
+        "decoder_neuron_list": {
             "description": "List of dense layer dimensions of decoder",
             "type": list,
             "items": int,
             "default": [16, 32],
+            "AutoML": True,
+            "list_range": (2, 16),
+            "item_range": (4, 256),
         },
         "latent_dim": {
             "argparse_name": "--latent-dim",
             "description": "Dimensions of latent layer",
             "type": int,
             "default": 2,
+            "AutoML": True,
+            "item_range": (2, 256),
         },
-        "hidden_activation_name": {
+        "hidden_activation": {
             "argparse_name": "--hidden-activation",
-            "description": "Activation of hidden layers",
+            "description": "Name of the activation of hidden layers",
             "type": str,
             "enum": AVAILABLE_ACTIVATION_NAMES,
             "default": "relu",
+            "AutoML": True,
         },
-        "output_activation_name": {
+        "output_activation": {
             "argparse_name": "--output-activation",
-            "description": "Activation of output layers",
+            "description": "Name of the activation of output layers",
             "type": str,
-            "enum": AVAILABLE_ACTIVATION_NAMES,
+            "enum": ["sigmoid", "tanh"],
             "default": "sigmoid",
+            "AutoML": True,
         },
         "batch_norm": {
             "argparse_name": "--batch-norm",
             "description": "Whether batch norm should be enabled",
             "type": bool,
             "default": False,
+            "AutoML": True,
         },
         "dropout_rate": {
             "argparse_name": "--dropout-rate",
             "description": "Dropout rate - disabled if set to zero",
             "type": float,
             "default": 0.0,
+            "AutoML": True,
+            "item_range": (0.0, 0.7),
         },
-        "beta": {
-            "argparse_name": "--loss-beta",
+        TrainerParams.BETA.value: {
             "description": "Parameter of the VAE loss function - scales KL divegence",  # noqa: E501
             "type": float,
             "default": 1.0,
         },
-        "capacity": {
-            "argparse_name": "--loss-capacity",
+        TrainerParams.CAPACITY.value: {
             "description": "Parameter of the VAE loss function - defines upper limit of KL divegence",  # noqa: E501
             "type": float,
             "default": 0.0,
         },
+        TrainerParams.CLIP_GRAD.value: {
+            "description": "Max norm for clipping gradients",
+            "type": float,
+            "default": 2.0,
+        },
     }
+
+    model_class = "AnomalyDetectionVAE"
 
     def __init__(
         self,
@@ -112,15 +142,16 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
         dataset: AnomalyDetectionDataset,
         from_file: bool = True,
         model_name: Optional[str] = None,
-        encoder_layers: List[int] = [16, 8],
-        decoder_layers: List[int] = [16, 32],
+        encoder_neuron_list: List[int] = [16, 8],
+        decoder_neuron_list: List[int] = [16, 32],
         latent_dim: int = 2,
-        hidden_activation_name: str = "relu",
-        output_activation_name: str = "sigmoid",
+        hidden_activation: str = "relu",
+        output_activation: str = "sigmoid",
         batch_norm: bool = False,
         dropout_rate: float = 0.0,
-        beta: float = 1.0,
-        capacity: float = 0.0,
+        loss_beta: float = 1.0,
+        loss_capacity: float = 0.0,
+        clip_grad_max_norm: float = 2.0,
     ):
         """
         Creates the model wrapper with VAE.
@@ -135,56 +166,83 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
             True if the model should be loaded from file.
         model_name : Optional[str]
             Name of the model used for the report.
-        encoder_layers : List[int]
+        encoder_neuron_list : List[int]
             List of dense layer dimensions of encoder.
-        decoder_layers : List[int]
+        decoder_neuron_list : List[int]
             List of dense layer dimensions of decoder.
         latent_dim : int
             Dimensions of latent layer.
-        hidden_activation_name : str
+        hidden_activation : str
             Activation of hidden layers.
-        output_activation_name : str
+        output_activation : str
             Activation of output layers.
         batch_norm : bool
             Whether batch norm should be enabled.
         dropout_rate : float
             Dropout rate - disabled if set to zero.
-        beta : float
+        loss_beta : float
             Parameter of the VAE loss function - scales KL divegence.
-        capacity : float
+        loss_capacity : float
             Parameter of the VAE loss function - defines upper limit
             of KL divegence.
+        clip_grad_max_norm: float
+            Max norm for clipping gradients
         """
         super().__init__(model_path, dataset, from_file, model_name)
 
-        self.encoder_layers = encoder_layers
-        self.decoder_layers = decoder_layers
+        self.encoder_neuron_list = encoder_neuron_list
+        self.decoder_neuron_list = decoder_neuron_list
         self.latent_dim = latent_dim
-        self.hidden_activation_name = hidden_activation_name
-        self.output_activation_name = output_activation_name
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
         self.batch_norm = batch_norm
         self.dropout_rate = dropout_rate
-        self.beta = beta
-        self.capacity = capacity
+        self.loss_beta = loss_beta
+        self.loss_capacity = loss_capacity
+        self.clip_grad_max_norm = clip_grad_max_norm
 
         if dataset:
             self.mean, self.std = self.dataset.get_input_mean_std()
 
-    def create_model_structure(self):
+    @staticmethod
+    def _create_model_structure(
+        input_shape: Tuple[int, ...],
+        encoder_neuron_list: List[int],
+        decoder_neuron_list: List[int],
+        latent_dim: int,
+        hidden_activation: str,
+        output_activation: str,
+        batch_norm: bool,
+        dropout_rate: float,
+        dataset: AnomalyDetectionDataset,
+    ):
         from kenning.modelwrappers.anomaly_detection.models.vae import (
             AnomalyDetectionVAE,
         )
 
-        self.model = AnomalyDetectionVAE(
-            feature_size=self.dataset.num_features,
-            window_size=self.dataset.window_size,
-            encoder_neuron_list=self.encoder_layers,
-            decoder_neuron_list=self.decoder_layers,
+        return AnomalyDetectionVAE(
+            window_size=dataset.window_size,
+            feature_size=dataset.num_features,
+            encoder_neuron_list=encoder_neuron_list,
+            decoder_neuron_list=decoder_neuron_list,
+            latent_dim=latent_dim,
+            hidden_activation_name=hidden_activation,
+            output_activation_name=output_activation,
+            batch_norm=batch_norm,
+            dropout_rate=dropout_rate,
+        )
+
+    def create_model_structure(self):
+        self.model = self._create_model_structure(
+            input_shape=None,
+            encoder_neuron_list=self.encoder_neuron_list,
+            decoder_neuron_list=self.decoder_neuron_list,
             latent_dim=self.latent_dim,
-            hidden_activation_name=self.hidden_activation_name,
-            output_activation_name=self.output_activation_name,
+            hidden_activation=self.hidden_activation,
+            output_activation=self.output_activation,
             batch_norm=self.batch_norm,
             dropout_rate=self.dropout_rate,
+            dataset=self.dataset,
         )
 
     def prepare_model(self):
@@ -239,7 +297,12 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
         eval: bool = True,
     ):
         import torch
-        from pyod.models.vae import VAEModel, vae_loss
+        from pyod.models.vae import VAEModel
+
+        from kenning.modelwrappers.anomaly_detection.models.vae import (
+            calibrate_threshold,
+            train_step,
+        )
 
         default_reparameterize = self.model.reparameterize
         default_batch_size = self.dataset.batch_size
@@ -284,19 +347,14 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
                 ):
                     x = X_train[batch_start : batch_start + batch_size]
                     x = torch.stack(x)
-                    optimizer.zero_grad()
-                    x_recon, z_mu, z_logvar = self.model.forward_minimal(x)
-                    loss = vae_loss(
-                        x[:, -self.dataset.num_features :],
-                        x_recon,
-                        z_mu,
-                        z_logvar,
-                        beta=self.beta,
-                        capacity=self.capacity,
+                    loss, _ = train_step(
+                        self.model,
+                        optimizer,
+                        x,
+                        self.clip_grad_max_norm,
+                        self.loss_beta,
+                        self.loss_capacity,
                     )
-                    loss.backward()
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), 5)
-                    optimizer.step()
                     total_loss += loss
                     if torch.isnan(loss):
                         KLogger.error(
@@ -319,15 +377,15 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
                 for batch_start in range(0, len(X_train), batch_size):
                     x = X_train[batch_start : batch_start + batch_size]
                     with torch.no_grad():
-                        distance = self.model.forward_distances(torch.stack(x))
+                        distance = self.model._forward_distances(
+                            torch.stack(x)
+                        )
                     distances.append(distance.cpu().numpy())
 
-                distances = np.concatenate(distances)
-                threshold = np.percentile(distances, 100 * (1 - contamination))
-                KLogger.debug(f"Calibrated threshold: {threshold}")
-                self.model.threshold = torch.nn.parameter.Parameter(
-                    torch.tensor(threshold),
-                    requires_grad=False,
+                calibrate_threshold(
+                    self.model,
+                    contamination,
+                    distances,
                 )
 
                 # Evaluate model on test set
@@ -414,3 +472,90 @@ class PyTorchAnomalyDetectionVAE(PyTorchWrapper):
             self.dataset.window_size,
             self.dataset.batch_size,
         )
+
+    @classmethod
+    def register_components(
+        cls, dataset: Optional[AnomalyDetectionDataset] = None
+    ) -> List[Type]:
+        from kenning.automl.auto_pytorch_components.network_head_passthrough import (  # noqa: E501
+            register_passthrough,
+        )
+        from kenning.automl.auto_pytorch_components.vae_trainer import (
+            register_vae_trainer,
+        )
+
+        components = super().register_components(dataset)
+        register_passthrough()
+        register_vae_trainer()
+        return components
+
+    @staticmethod
+    def define_forbidden_clauses(cs, **kwargs):
+        import ConfigSpace as CS
+
+        from kenning.automl.auto_pytorch_components.utils import (
+            _create_forbidden_choices,
+        )
+
+        vae_component = PyTorchAnomalyDetectionVAE.get_component_name()
+        network_back = cs.get_hyperparameter("network_backbone:__choice__")
+        vae_only = (
+            len(network_back.choices) == 1
+            and vae_component in network_back.choices
+        )
+        network_back_vae = CS.ForbiddenEqualsClause(
+            network_back,
+            vae_component,
+        )
+
+        clauses = [
+            _create_forbidden_choices(cs, name, (choice,), vae_only)
+            for name, choice in (
+                ("imputer:numerical_strategy", "constant_zero"),
+                ("network_head:__choice__", "PassthroughHead"),
+                ("network_embedding:__choice__", "NoEmbedding"),
+                ("feature_preprocessor:__choice__", "NoFeaturePreprocessor"),
+                ("encoder:__choice__", "NoEncoder"),
+                ("coalescer:__choice__", "NoCoalescer"),
+                ("scaler:__choice__", "StandardScaler"),
+            )
+        ]
+
+        cs.add_forbidden_clauses(
+            [
+                CS.ForbiddenAndConjunction(
+                    network_back_vae,
+                    clause,
+                )
+                for clause in clauses
+            ]
+        )
+        return cs
+
+    @classmethod
+    def prepare_config(cls, configuration: Dict):
+        kenning_conf = super().prepare_config(configuration)
+        vae_trainer_conf = {
+            k.rsplit(":", 1)[1]: v
+            for k, v in configuration.items()
+            if k.startswith("trainer")
+        }
+        for arg in [a.value for a in list(TrainerParams)]:
+            if arg not in vae_trainer_conf:
+                KLogger.warn(f"Missing {arg} for VAE trainer")
+                continue
+            kenning_conf["model_wrapper"]["parameters"][
+                arg
+            ] = vae_trainer_conf[arg]
+        return kenning_conf
+
+    @classmethod
+    def fit(cls, self, X: Dict[str, Any], y: Any = None):
+        """
+        AutoPyTorch fit method.
+
+        Overrides additional loss calculated by AutoPyTorch.
+        """
+        # Use binary cross entropy
+        X["additional_losses"] = "BCEWithLogitsLoss"
+        return super(type(self), self).fit(X, y)

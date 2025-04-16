bc9f5991f57c9592662b20da29e905d2dc2df6ef
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-04-16 18:55:52 +0200

    [#75546] modelwrappers: pytorch: Implement general training loop
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- kenning/modelwrappers/frameworks/pytorch.py
+++ kenning/modelwrappers/frameworks/pytorch.py
@@ -9,19 +9,30 @@ Provides base methods for using PyTorch models in Kenning.
 import copy
 from abc import ABC
 from collections import OrderedDict
-from typing import Any, List, Optional
+from typing import Any, Callable, List, Optional, Tuple, TypeVar
 
 import numpy as np
+from sklearn import metrics
+from tqdm import tqdm
 
 from kenning.core.dataset import Dataset
 from kenning.core.model import ModelWrapper
+from kenning.utils.logger import LoggerProgressBar
 from kenning.utils.resource_manager import PathOrURI
+
+DataLoader = TypeVar("torch.utils.data.DataLoader")
+Optimizer = TypeVar("torch.optim.Optimizer")
+Tensor = TypeVar("torch.Tensor")
 
 
 class PyTorchWrapper(ModelWrapper, ABC):
     """
     Base model wrapper for PyTorch models.
     """
+
+    # Whether model should be saved as exported dict with parameters
+    # or as pickled object
+    DEFAULT_SAVE_MODEL_EXPORT_DICT = True
 
     def __init__(
         self,
@@ -110,10 +121,14 @@ class PyTorchWrapper(ModelWrapper, ABC):
             ],
         )
 
-    def save_model(self, model_path: PathOrURI, export_dict: bool = True):
+    def save_model(
+        self, model_path: PathOrURI, export_dict: Optional[bool] = None
+    ):
         import torch
 
         self.prepare_model()
+        if export_dict is None:
+            export_dict = self.DEFAULT_SAVE_MODEL_EXPORT_DICT
         if export_dict:
             torch.save(self.model.state_dict(), model_path)
         else:
@@ -176,18 +191,109 @@ class PyTorchWrapper(ModelWrapper, ABC):
 
         return result
 
-    def get_model_size(self) -> float:
+    def _train_model(
+        self,
+        train_loader: DataLoader,
+        test_loader: DataLoader,
+        opt: Optimizer,
+        criterion: Callable[[Tensor], Tensor],
+        postprocess: Callable[[Tensor, Tensor], Tuple[Tensor, Tensor]],
+        metric_func: Callable[
+            [np.ndarray, np.ndarray], float
+        ] = metrics.accuracy_score,
+        epoch_start_hook: Callable[[int], None] = None,
+    ):
         """
-        Calculates model size, combining parameters and buffers.
+        General training loop for PyTorch models.
 
-        Returns
-        -------
-        float
-            The model size in KB.
+        Parameters
+        ----------
+        train_loader : DataLoader
+            The train set loader.
+        test_loader : DataLoader
+            The test set loader.
+        opt : Optimizer
+            The model optimizer.
+        criterion : Callable[[Tensor], Tensor]
+            The function calculation loss.
+        postprocess : Callable[[Tensor, Tensor], Tuple[Tensor, Tensor]]
+            The function post-processing the model output and label.
+        metric_func : Callable[[np.ndarray, np.ndarray], float]
+            The function calculation metric.
+        epoch_start_hook : Callable[[int], None]
+            The hook called at the beginning of each epoch.
         """
-        model_size = 0
-        for param in self.model.parameters():
-            model_size += param.nelement() * param.element_size()
-        for buffer in self.model.buffers():
-            model_size += buffer.nelement() * buffer.element_size()
-        return model_size / 1024
+        import torch
+        from torch.utils.tensorboard import SummaryWriter
+
+        best_metric = 0
+
+        writer = SummaryWriter(log_dir=self.logdir)
+
+        for epoch in range(self.num_epochs):
+            if epoch_start_hook:
+                epoch_start_hook(epoch)
+
+            self.model.train()
+            loss_sum = torch.zeros(1).to(self.device)
+            loss_count = 0
+            with LoggerProgressBar() as logger_progress_bar:
+                bar = tqdm(train_loader, file=logger_progress_bar)
+                for input, labels in bar:
+                    input = input.to(self.device)
+                    labels = labels.to(self.device)
+                    opt.zero_grad()
+
+                    outputs = self.model(input)
+                    loss = criterion(outputs, labels)
+
+                    loss.backward()
+                    opt.step()
+
+                    loss_sum += loss
+                    loss_count += 1
+                    bar.set_description(
+                        f"train epoch: {epoch:3d} loss: "
+                        f"{loss_sum.data.cpu().numpy().sum() / loss_count:.3f}"
+                    )
+
+            writer.add_scalar(
+                "Loss/train", loss_sum.data.cpu().numpy() / loss_count, epoch
+            )
+
+            self.model.eval()
+            predicted = np.array([])
+            true = np.array([])
+            with torch.no_grad(), LoggerProgressBar() as logger_progress_bar:
+                bar = tqdm(test_loader, file=logger_progress_bar)
+                for input, labels in bar:
+                    input = input.to(self.device)
+                    labels = labels.to(self.device)
+                    outputs = self.model(input)
+                    outputs, labels = postprocess(outputs, labels)
+                    predicted = np.concatenate(
+                        [predicted, outputs.cpu().numpy()]
+                    )
+                    true = np.concatenate((true, labels.cpu().numpy()))
+                    metric = metric_func(true, predicted)
+                    bar.set_description(
+                        f"valid epoch: {epoch:3d} {metric_func.__name__}: "
+                        f"{metric:.3f}"
+                    )
+
+                writer.add_scalar(
+                    f"{metric_func.__name__}/valid", metric, epoch
+                )
+
+                if metric > best_metric:
+                    self.save_model(self.model_path)
+                    best_metric = metric
+
+        self.save_model(
+            self.model_path.with_stem(f"{self.model_path.stem}_final"),
+        )
+
+        self.dataset.standardize = True
+
+        writer.close()
+        self.model.eval()

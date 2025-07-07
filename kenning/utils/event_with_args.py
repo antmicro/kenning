7344f0c63674abcf234db2d90a22cb662b6bf51d
Mateusz Nurczynski <mnurczynski@internships.antmicro.com>
2025-07-07 17:11:09 +0200

    [#78945] utils: event_with_args: Added a wrapper for 'threading.Event', that allows for passing arguments through the thread
    Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

---
+++ kenning/utils/event_with_args.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2020-2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Mechanism for sending signals between threads, based on threading.Event,
+that allows values to be passed with the event.
+
+Usage:
+
+```
+event = EventWithArgs()
+```
+
+In the waiting thread:
+
+```
+(arg1, arg2) = event.wait()
+```
+
+In the notifying thread:
+
+```
+event.set((arg1, arg2))
+```
+"""
+
+from threading import Event
+from typing import Tuple
+
+
+class EventWithArgs(Event):
+    """
+    A wrapper for threading.Event, allowing to pass a value with the event.
+    """
+
+    def set(self, args: Tuple = ()):
+        """
+        Sets the internal flag to 1, notifying all waiting threads (those,
+        that called the 'wait' method).
+
+        Parameters
+        ----------
+        args: Tuple
+            Values, that will be passed to the waiting threads.
+        """
+        self.args = args
+        super().set()
+
+    def wait(self) -> Tuple:
+        """
+        Stops the thread until the internal flag is set to 1.
+
+        Returns
+        -------
+        Tuple
+            Values passed by the notifying thread.
+        """
+        super().wait()
+        return self.args

c12ad6b19ee95100cfad7903f4848285e171f61f
Mateusz Nurczynski <mnurczynski@internships.antmicro.com>
2025-07-25 16:36:27 +0200

    [#78945] core: protocol: Updated server-side API
    Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

--- kenning/core/protocol.py
+++ kenning/core/protocol.py
@@ -10,11 +10,14 @@ the client.
 import time
 from abc import ABC, abstractmethod
 from argparse import Namespace
+from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 from kenning.core.measurements import Measurements
 from kenning.utils.args_manager import ArgumentsHandler
+from kenning.utils.logger import KLogger
 
 
 class RequestFailure(Exception):
@@ -57,6 +60,83 @@ def check_request(
         raise RequestFailure(f"Failed to handle request: {msg}")
 
     return request
+
+
+class ServerAction(Enum):
+    """
+    Enum with all types of actions/operations, that may be executed
+    by a Kenning inference server using the protocol.
+    """
+
+    WAITING_FOR_CLIENT = 0
+    CLIENT_CONNECTED = 1
+    UPLOADING_IOSPEC = 2
+    UPLOADING_MODEL = 3
+    UPLOADING_UNOPTIMIZED_MODEL = 4
+    UPLOADING_INPUT = 5
+    PROCESSING_INPUT = 6
+    EXTRACTING_OUTPUT = 7
+    COMPUTING_STATISTICS = 8
+    OPTIMIZING_MODEL = 9
+    UPLOADING_OPTIMIZERS = 10
+    UPLOADING_RUNTIME = 11
+    IDLE = 12
+
+    def to_bytes(self) -> bytes:
+        return self.value.to_bytes(1, "little")
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> "ServerAction":
+        return cls(int.from_bytes(data, "little"))
+
+
+@dataclass
+class ServerStatus:
+    """
+    Class denoting status of an inference server (last executed action
+    and information whether is succeeded or not).
+    """
+
+    last_action: ServerAction = ServerAction.WAITING_FOR_CLIENT
+    success: bool = True
+
+    def start_action(self, action: ServerAction):
+        """
+        Updates the status with information about new task started by the
+        server. Sets the 'success' value to None (which means, that the
+        action is in progress).
+
+        Parameters
+        ----------
+        action: ServerAction
+            Action being started.
+        """
+        KLogger.debug(f"Server action started: {action}")
+        self.last_action = action
+        self.success = None
+
+    def finish_action(self, success: bool):
+        """
+        Updates the status with information about current action being
+        finished and whether is succeeded or not.
+
+        Parameters
+        ----------
+        success: bool
+            True if action succeeded, False if not.
+        """
+        if success:
+            KLogger.debug(f"Server action completed: {self.last_action}")
+        else:
+            KLogger.error(f"Server action failed: {self.last_action}")
+        self.success = success
+
+
+# Type definitions for 2 kinds of server callback functions (functions,
+# that a server needs to implement to use the protocol) - look at the
+# 'Protocol.serve' method.
+ServerUploadCallback = Callable[bytes, ServerStatus]
+ServerDownloadCallback = Callable[None, Tuple[ServerStatus, Optional[bytes]]]
 
 
 class Protocol(ArgumentsHandler, ABC):
@@ -134,13 +214,90 @@ class Protocol(ArgumentsHandler, ABC):
         return super().from_json(json_dict)
 
     @abstractmethod
-    def initialize_server(self) -> bool:
+    def serve(
+        self,
+        upload_input_callback: Optional[ServerUploadCallback] = None,
+        upload_model_callback: Optional[ServerUploadCallback] = None,
+        process_input_callback: Optional[ServerUploadCallback] = None,
+        download_output_callback: Optional[ServerDownloadCallback] = None,
+        download_stats_callback: Optional[ServerDownloadCallback] = None,
+        upload_iospec_callback: Optional[ServerUploadCallback] = None,
+        upload_optimizers_callback: Optional[ServerUploadCallback] = None,
+        upload_unoptimized_model_callback: Optional[
+            ServerUploadCallback
+        ] = None,
+        download_optimized_model_callback: Optional[
+            ServerDownloadCallback
+        ] = None,
+        upload_runtime_callback: Optional[ServerUploadCallback] = None,
+    ):
+        """
+        Waits for requests from the other device (the client) and calls
+        appropriate callbacks. Some callbacks take bytes as arguments,
+        all return ServerStatus and some also return bytes. These
+        responses are then sent back to the client.
+
+        Callbacks are guaranteed to be executed in the order requests
+        were sent in.
+
+        This method is non-blocking.
+
+        Parameters
+        ----------
+        upload_input_callback: Optional[ServerUploadCallback]
+            Called, when the client uploads input ('upload_input'
+            method below). Should upload model input into the runtime.
+        upload_model_callback: Optional[ServerUploadCallback]
+            Called. when the client uploads optimized model
+            ('upload_model' method below). It should load the model
+            and start inference session.
+        process_input_callback: Optional[ServerUploadCallback]
+            Called, when the client requests inference. Should return
+            after inference is completed.
+        download_output_callback: Optional[ServerDownloadCallback]
+            Called, when the client requests inference output, should
+            return status of the server and the output.
+        download_stats_callback: Optional[ServerDownloadCallback]
+            Called, when the client requests inference stats, should
+            end the inference session and return status of the server
+            and stats.
+        upload_iospec_callback: Optional[ServerUploadCallback]
+            Called to upload model input/output specifications (iospec).
+        upload_optimizers_callback: Optional[ServerUploadCallback]
+            Called to upload optimizer config (serialized JSON)
+            - 'upload_optimizers' method call by the client.
+        upload_unoptimized_model_callback: Optional[ServerUploadCallback]
+            Called to upload an unoptimized ML model, should save it.
+        download_optimized_model_callback: Optional[ServerDownloadCallback]
+            Called, when client requests optimization of the model
+            (uploaded with 'upload_unoptimized_model_callback').
+            Should optimize the model and return it.
+        upload_runtime_callback: Optional[ServerUploadCallback]
+            Called, when the client uploads runtime.
+        """
+        ...
+
+    @abstractmethod
+    def initialize_server(
+        self,
+        client_connected_callback: Optional[Callable[Any, None]] = None,
+        client_disconnected_callback: Optional[Callable[None, None]] = None,
+    ) -> bool:
         """
         Initializes server side of the protocol.
 
         The server side is supposed to run on target hardware.
 
         The parameters for the server should be provided in the constructor.
+
+        Parameters
+        ----------
+        client_connected_callback: Optional[Callable[Any, None]]
+            Called when a client connects to the server. Either IP address
+            or another distinguishing characteristic of the client will be
+            passed to the callback (depending on the underlying protocol).
+        client_disconnected_callback: Optional[Callable[None, None]]
+            Called, when the current client disconnects from the server.
 
         Returns
         -------
@@ -354,35 +511,6 @@ class Protocol(ArgumentsHandler, ABC):
         Tuple[bool, Optional[bytes]]
             First element is equal to True if optimization finished
             successfully and the second element contains compiled model.
-        """
-        ...
-
-    @abstractmethod
-    def request_success(self, data: Optional[bytes] = bytes()) -> bool:
-        """
-        Sends OK message back to the client once the request is finished.
-
-        Parameters
-        ----------
-        data : Optional[bytes]
-            Optional data upon success, if any.
-
-        Returns
-        -------
-        bool
-            True if sent successfully.
-        """
-        ...
-
-    @abstractmethod
-    def request_failure(self) -> bool:
-        """
-        Sends ERROR message back to the client if it failed to handle request.
-
-        Returns
-        -------
-        bool
-            True if sent successfully.
         """
         ...
 

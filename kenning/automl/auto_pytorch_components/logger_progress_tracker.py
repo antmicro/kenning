d6d860901d7e2e4bbb69ec9df3e29ea8fcad6e1d
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2025-08-05 15:55:47 +0200

    [#80850] automl: apytorch: logger: tidied up unnecessary imports and added docstrings

--- kenning/automl/auto_pytorch_components/logger_progress_tracker.py
+++ kenning/automl/auto_pytorch_components/logger_progress_tracker.py
@@ -1,17 +1,32 @@
-import kenning.utils.logger
-from autoPyTorch.utils.progress_tracker import (
-    TrainingProgressTracker, EpochTracker
-)
-from smac.runhistory.runhistory import RunInfo, RunValue, RunHistory
-from smac.utils.constants import MAXINT
-from logging import Logger
-from kenning.utils.logger import KLogger, LoggerProgressBar
-from tqdm import tqdm
-from typing import Optional, Dict
+# Copyright (c) 2020-2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Module implements loggers for autopytorch progress tracking.
+"""
+
 import time
+from logging import Logger
+from typing import Dict, Optional
+
 import torch
+from autoPyTorch.utils.progress_tracker import (
+    EpochTracker,
+    TrainingProgressTracker,
+)
+from smac.utils.constants import MAXINT
+from tqdm import tqdm
+
+from kenning.utils.logger import KLogger, LoggerProgressBar
+
 
 class TrainingProgressLogger(TrainingProgressTracker):
+    """
+    Progress tracker for the whole autoPyTorch
+    training and evaluation process.
+    """
+
     def __init__(
         self,
         total_time_expected_seconds: float,
@@ -26,44 +41,36 @@ class TrainingProgressLogger(TrainingProgressTracker):
 
         TrainingProgressTracker.__init__(self, total_time_expected_seconds)
 
-
     def __enter__(self):
         with LoggerProgressBar() as logger_progress_bar:
             self.pbar = tqdm(
                 total=self.total_time,
                 desc="Time limit",
                 position=0,
-                **logger_progress_bar.kwargs
+                **logger_progress_bar.kwargs,
             )
         self.total_time_passed = 0
         self.lowest_cost = MAXINT
         self.best_metrics = {}
         return super().__enter__()
 
-
-    def __exit__(self, exc_type, exc_val, exc_tb ):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         cur_perc = self.pbar.n
         total_perc = self.total_time
         self.pbar.update(total_perc - cur_perc)
         self.pbar.refresh()
         self.pbar.close()
-        super().__exit__(exc_type, exc_val, exc_tb )
+        super().__exit__(exc_type, exc_val, exc_tb)
 
-
-    def report_time( self, time_passed: Optional[float] = None):
+    def report_time(self, time_passed: Optional[float] = None):
         cur_time = time.time()
         if not time_passed:
             time_passed = cur_time - self.last_time_updated
         self.last_time_updated = cur_time
         self.pbar.update(time_passed)
 
-
     def report_progress(
-        self,
-        time_passed: float,
-        metrics: Dict,
-        model: str,
-        cost
+        self, time_passed: float, metrics: Dict, model: str, cost
     ) -> None:
         self.total_time_passed = self.total_time_passed + time_passed
         self.report_time(time_passed)
@@ -73,12 +80,16 @@ class TrainingProgressLogger(TrainingProgressTracker):
 
         self.logger.info(f"Model backbone: {model}")
         if self.best_metrics != {}:
-            self.logger.info(f"so-far best configuration metrics:")
+            self.logger.info("so-far best configuration metrics:")
             for name, metric in self.best_metrics.items():
                 self.logger.info(f"- {name}: {metric}")
 
 
 class EpochTrainingStepLogger(EpochTracker):
+    """
+    Tracks and logs progress of one epoch training in autoPyTorch.
+    """
+
     def __init__(
         self,
         progress_tracker: Optional[TrainingProgressLogger] = None,
@@ -89,7 +100,6 @@ class EpochTrainingStepLogger(EpochTracker):
         self.pbar = None
 
         EpochTracker.__init__(self)
-
 
     def __enter__(self):
         with LoggerProgressBar() as logger_progress_bar:
@@ -97,7 +107,7 @@ class EpochTrainingStepLogger(EpochTracker):
                 desc="Epoch training",
                 total=self.total_steps,
                 position=1,
-                **logger_progress_bar.kwargs
+                **logger_progress_bar.kwargs,
             )
         return super().__enter__()
 
@@ -117,13 +127,18 @@ class EpochTrainingStepLogger(EpochTracker):
         batch_size: int,
         outputs: torch.Tensor,
         targets: torch.Tensor,
-        additional_info: Optional[Dict] = None
+        additional_info: Optional[Dict] = None,
     ) -> None:
         if self.progress_tracker:
             self.progress_tracker.report_time()
         self.pbar.update(1)
 
+
 class EpochEvaluationStepLogger(EpochTracker):
+    """
+    Tracks and logs progress of one epoch evaluation in autoPyTorch.
+    """
+
     def __init__(
         self,
         progress_tracker: Optional[TrainingProgressLogger] = None,
@@ -135,14 +150,13 @@ class EpochEvaluationStepLogger(EpochTracker):
 
         EpochTracker.__init__(self)
 
-
     def __enter__(self):
         with LoggerProgressBar() as logger_progress_bar:
             self.pbar = tqdm(
                 desc="Epoch evaluation",
                 total=self.total_steps,
                 position=1,
-                **logger_progress_bar.kwargs
+                **logger_progress_bar.kwargs,
             )
         return super().__enter__()
 
@@ -162,7 +176,7 @@ class EpochEvaluationStepLogger(EpochTracker):
         batch_size: int,
         outputs: torch.Tensor,
         targets: torch.Tensor,
-        additional_info: Optional[Dict] = None
+        additional_info: Optional[Dict] = None,
     ) -> None:
         if self.progress_tracker:
             self.progress_tracker.report_time()

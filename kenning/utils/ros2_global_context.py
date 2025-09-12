8c11ab3ad7b46a3210a160567c4d7994b8e34ff0
Patryk Peroński <pperonski@internships.antmicro.com>
2025-09-12 08:09:10 +0100

    [#82922] cli: ros2: utils: Created ROS2GlobalContext singleton object and moved ROS 2 initialization logic there

--- kenning/utils/ros2_global_context.py
+++ kenning/utils/ros2_global_context.py
@@ -15,25 +15,24 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter, parameter_value_to_python
 
 from kenning.utils.logger import KLogger
+from kenning.utils.singleton import Singleton
 
 KENNING_ROS_NODE_NAME = "kenning"
 
 KENNING_ROS_LOGGER_BACKEND_NAME = "ros2_backend"
 
 
-class ROS2GlobalContext:
+class _ROS2GlobalContext(metaclass=Singleton):
     """
     A class that holds static ROS 2 Node used by other ROS 2 related modules.
     """
 
-    node = None
+    def __init__(self):
+        self.node = None
+        self.node_thread = None
+        self.logger = None
 
-    node_thread = None
-
-    logger = None
-
-    @classmethod
-    def init_node(cls):
+    def init_node(self) -> List[str]:
         """
         Function that initialize global ROS 2 Node.
         """
@@ -44,35 +43,44 @@ class ROS2GlobalContext:
 
         KLogger.debug("Creating global ROS 2 Node...")
 
-        cls.node = Node(
-            KENNING_ROS_NODE_NAME, allow_undeclared_parameters=True
+        self.node = Node(
+            KENNING_ROS_NODE_NAME,
+            allow_undeclared_parameters=True,
+            automatically_declare_parameters_from_overrides=True,
         )
 
-        cls.logger = cls.node.get_logger()
+        self.logger = self.node.get_logger()
 
-        KLogger.debug("Created ROS 2 Node!")
-
-        KLogger.debug("Initializing ROS 2 Node thread.")
-        # create node loop that will spin kenning node in the background
-        cls.node_thread = Thread(target=cls.node_loop)
-        cls.node_thread.start()
-        KLogger.info("Started ROS2 Node!")
+        KLogger.debug("Created ROS 2 Node")
 
         # add ROS2 logger backend
 
         KLogger.add_custom_backend(
-            KENNING_ROS_LOGGER_BACKEND_NAME, cls.logger_backend
+            KENNING_ROS_LOGGER_BACKEND_NAME, self.logger_backend
         )
 
-    @classmethod
-    def logger_backend(cls, msg: str):
-        if cls.logger is None:
+    def start_node(self):
+        if self.node_thread is not None:
+            KLogger.debug("ROS 2 Node is aleardy started")
             return
 
-        cls.logger.info(msg)
+        KLogger.debug("Initializing ROS 2 Node thread.")
+        # create node loop that will spin kenning node in the background
+        self.node_thread = Thread(target=self.node_loop)
+        self.node_thread.daemon = True
+        self.node_thread.start()
+        KLogger.info("Started ROS 2 Node")
 
-    @classmethod
-    def get_param(cls, name: str) -> Any:
+    def clean_args_list(self, args: Optional[List[str]] = None):
+        return rclpy.utilities.remove_ros_args(args)
+
+    def logger_backend(self, msg: str):
+        if self.logger is None:
+            return
+
+        self.logger.info(msg)
+
+    def get_param(self, name: str) -> Any:
         """
         Try to get parameter from ROS 2.
 
@@ -87,7 +95,7 @@ class ROS2GlobalContext:
             An parameter value.
         """
         param = parameter_value_to_python(
-            cls.node.get_parameter_or(name, None).get_parameter_value()
+            self.node.get_parameter_or(name, None).get_parameter_value()
         )
 
         KLogger.debug(f"Parameter {param} of: {name}")
@@ -97,8 +105,7 @@ class ROS2GlobalContext:
 
         return param
 
-    @classmethod
-    def get_config_file_path(cls) -> str:
+    def get_config_file_path(self) -> str:
         """
         Get path to Kenning pipeline config file.
 
@@ -107,14 +114,13 @@ class ROS2GlobalContext:
         str
             A string with path to config file.
         """
-        cfg = cls.node.get_parameter_or(
+        cfg = self.node.get_parameter_or(
             "config_file", Parameter("config_file", value="")
         ).get_parameter_value()
 
         return cfg.string_value
 
-    @classmethod
-    def get_logging_level(cls) -> LoggingSeverity:
+    def get_logging_level(self) -> LoggingSeverity:
         """
         Function to get logging level from ROS 2 Node.
 
@@ -123,71 +129,40 @@ class ROS2GlobalContext:
         LoggingSeverity
             A current logging level.
         """
-        return cls.node.get_logger().get_effective_level()
+        return self.node.get_logger().get_effective_level()
 
-    @classmethod
-    def get_pipeline(cls) -> List[str]:
-        """
-        Function that returns command sequence that will
-        be used by Kenning.
-
-        Returns
-        -------
-        List[str]
-            A sequence of commands that will be executed by kenning.
-
-        Raises
-        ------
-        Exception
-            Raised when unknown command is passed to pipeline.
-        """
-        pipeline = cls.node.get_parameter_or(
-            "pipeline", Parameter("pipeline", value=[])
-        ).get_parameter_value()
-
-        pipeline = pipeline.string_array_value
-
-        for cmd in pipeline:
-            if cmd not in AVAILABLE_COMMANDS:
-                raise Exception(f"Unknown pipeline command: {cmd}")
-
-        return pipeline
-
-    @classmethod
-    def init_parameters(cls):
+    def init_parameters(self):
         """
         Function that initialize ROS 2 parameters from.
         """
-        KLogger.debug(f"Parameters: {cls.node.get_parameters()}")
+        KLogger.debug(f"Parameters: {self.node.get_parameters()}")
 
-    @classmethod
-    def node_loop(cls):
+    def node_loop(self):
         """
         Function that spins node.
         """
         while rclpy.ok():
-            rclpy.spin_once(cls.node, timeout_sec=0.001)
+            rclpy.spin_once(self.node, timeout_sec=0.001)
 
-    @classmethod
-    def stop_node(cls):
+    def stop_node(self):
         """
         Function that shutdown global context and stop ROS 2 node
         execution.
         """
-        KLogger.info("Shutting down ROS 2 Node")
+        if rclpy.ok():
+            KLogger.info("Shutting down ROS 2 Node")
 
-        rclpy.shutdown()
-        cls.node.destroy_node()
+            rclpy.shutdown()
+            self.node.destroy_node()
 
-        cls.node_thread.join()
+            self.node_thread.join()
 
-        cls.node = None
-        cls.node_thread = None
+            self.node = None
+            self.node_thread = None
 
-        KLogger.remove_custom_backend(KENNING_ROS_LOGGER_BACKEND_NAME)
+            KLogger.remove_custom_backend(KENNING_ROS_LOGGER_BACKEND_NAME)
 
-    @classmethod
-    def node_name(cls) -> str:
+    def node_name(self) -> str:
         """
         Function that gets used Node name.
 
@@ -196,4 +171,10 @@ class ROS2GlobalContext:
         str
             A name of the created Node.
         """
-        return cls.node.get_name()
+        return self.node.get_name()
+
+    def __del__(self):
+        self.stop_node()
+
+
+ROS2GlobalContext = _ROS2GlobalContext()

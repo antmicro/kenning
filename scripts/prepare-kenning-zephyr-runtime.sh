19fd95039d90e2dbfe10e812d7de5f80b22e4010
Mateusz Nurczynski <mnurczynski@internships.antmicro.com>
2025-10-15 17:47:55 +0200

    [#84762] scripts: prepare-kenning-zephyr-runtime.sh: Installing Zephyr SDK with a west command, instead of a script from Kenning Zephyr Runtime
    Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

--- scripts/prepare-kenning-zephyr-runtime.sh
+++ scripts/prepare-kenning-zephyr-runtime.sh
@@ -10,9 +10,16 @@ mkdir -p $ZEPHYR_WORKSPACE && pushd $ZEPHYR_WORKSPACE
 git clone https://github.com/antmicro/kenning-zephyr-runtime.git
 cd kenning-zephyr-runtime
 
-./scripts/prepare_zephyr_env.sh
-./scripts/prepare_modules.sh
+python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
+pip install pip setuptools --upgrade
+pip install -r requirements.txt
+west init -l .
+west update
+west zephyr-export
+west packages pip --install
+west sdk install --toolchains x86_64-zephyr-elf arm-zephyr-eabi riscv64-zephyr-elf
+./scripts/prepare_modules.sh
 source ./scripts/prepare_renode.sh
 
 popd

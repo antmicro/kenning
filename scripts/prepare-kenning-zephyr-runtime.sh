6a45f6711395bdda3c731c6fe47271a54c1dcce9
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2025-11-05 10:45:04 +0100

    [#86044] scripts: prep-kenning-zephyr-runtime: updated prepare-zephyr-runtime script

--- scripts/prepare-kenning-zephyr-runtime.sh
+++ scripts/prepare-kenning-zephyr-runtime.sh
@@ -12,12 +12,11 @@ cd kenning-zephyr-runtime
 
 python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
-pip install pip setuptools --upgrade
-pip install -r requirements.txt
+pip install pip setuptools west --upgrade
 west init -l .
 west update
 west zephyr-export
-west packages pip --install
+pip install -r requirements.txt -r ../zephyr/scripts/requirements-base.txt
 west sdk install --toolchains x86_64-zephyr-elf arm-zephyr-eabi riscv64-zephyr-elf
 ./scripts/prepare_modules.sh
 source ./scripts/prepare_renode.sh

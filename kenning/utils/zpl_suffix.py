4feb7f3d96407bfcbdaa209ef755afbb9a7afe89
Mateusz Nurczynski <mnurczynski@internships.antmicro.com>
2026-01-23 17:42:07 +0100

    [#86600] utils: Added a utility for computing default paths to Zephelin trace files, based on given measurements path
    Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

---
+++ kenning/utils/zpl_suffix.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2026 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides enumeration of Zephelin related file extensions.
+"""
+
+from enum import Enum
+from pathlib import Path
+
+
+class ZplSuffix(Enum):
+    """
+    List of common report file suffixes.
+    """
+
+    CTF = ".ctf"
+    TVM_GRAPH_JSON = ".graph.json"
+    TFLITE = ".tflite"
+    TRACE_JSON = ".trace.json"
+
+    def _get_path_with_suffix(self, path: Path) -> Path:
+        return path.with_suffix(self.value)

[#93189] datasets: tabular_dataset: Fixed metric calculation for datasets with non-expanded classes
Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

kenning/datasets/tabular_dataset.py
@@ -147,8 +147,12 @@ class TabularDataset(Dataset):
 
     def evaluate(self, predictions, truth):
         measurements = Measurements()
-        ts = [np.argmax(truth[0], axis=-1)]
-        ps = [np.argmax(predictions[0], axis=-1)]
+        if self.expand_classes:
+            ts = [np.argmax(truth[0], axis=-1)]
+            ps = [np.argmax(predictions[0], axis=-1)]
+        else:
+            ts = truth
+            ps = np.round(np.asarray(predictions[0]).flatten()).astype("int32")
         confusion_matrix = metrics.confusion_matrix(
             ts, ps, labels=range(len(self.classnames))
         )
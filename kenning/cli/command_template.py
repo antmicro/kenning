325d6f337acc0546e51f3102f2669941ca18ede3
Mateusz Nurczynski <mnurczynski@internships.antmicro.com>
2026-07-28 17:21:30 +0200

    [#98429] cli: command_template: Add functions for parsing and merging configuration from different sources
    Signed-off-by: Mateusz Nurczynski <mnurczynski@internships.antmicro.com>

--- kenning/cli/command_template.py
+++ kenning/cli/command_template.py
@@ -11,8 +11,24 @@ import sys
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional, Tuple, Union
 
+import yaml
+
 from kenning.cli.parser import HELP_FLAGS, Parser, ParserHelpException
-from kenning.utils.class_loader import ConfigKey
+from kenning.dispatcher.block_config import (
+    CONFIG_KEY_TO_CLS_FLAG,
+    KenningBlockConfigDict,
+    apply_default_blocks_by_block_type,
+    argparse_to_config_dict,
+    filter_block_types_from_config_dict,
+    merge_config_dicts,
+    yaml_or_json_to_config_dict,
+)
+from kenning.utils.class_loader import (
+    ConfigKey,
+    classes_from_full_dict_config,
+    objs_from_full_dict_config,
+    parse_classes,
+)
 from kenning.utils.logger import KLogger
 
 # Subcommands:
@@ -68,6 +84,11 @@ class CommandTemplate(ABC):
     ID = generate_command_type()
 
     current_command: str = ""
+
+    default_block_classes: Dict[ConfigKey, str] = {
+        ConfigKey.report: "MarkdownReport",
+        ConfigKey.platform: "LocalPlatform",
+    }
 
     @staticmethod
     def configure_parser(
@@ -178,6 +199,75 @@ class CommandTemplate(ABC):
         return groups
 
     @staticmethod
+    def add_block_flags_to_argparse(
+        flag_group: argparse._ArgumentGroup, block_types: List[ConfigKey]
+    ):
+        """
+        Adds to the chosen argparse group CLI flags for setting blocks of
+        selected block types (such as --modelwrapper-cls).
+
+        Parameters
+        ----------
+        flag_group: argparse._ArgumentGroup
+            Group that the flags will be added to.
+        block_types: List[ConfigKey]
+            Block types, flags for which are supposed to be parsed.
+        """
+        from kenning.cli.completers import (
+            AUTOML,
+            DATASETS,
+            MODEL_WRAPPERS,
+            OPTIMIZERS,
+            PLATFORMS,
+            RUNTIME_PROTOCOLS,
+            RUNTIMES,
+            ClassPathCompleter,
+        )
+
+        help_and_completer_map = {
+            ConfigKey.optimizers: (
+                OPTIMIZERS,
+                "Optimizer-based class with compiling routines to import",
+            ),
+            ConfigKey.dataset: (
+                DATASETS,
+                "Dataset-based class with dataset to import",
+            ),
+            ConfigKey.model_wrapper: (
+                MODEL_WRAPPERS,
+                "ModelWrapper-based class with inference implementation to import",  # noqa: E501
+            ),
+            ConfigKey.platform: (
+                PLATFORMS,
+                "Platform-based class that wraps platform being tested",
+            ),
+            ConfigKey.report: (
+                REPORT,
+                "Report-based class with report generation code",
+            ),
+            ConfigKey.runtime: (
+                RUNTIMES,
+                "Runtime-based class with the implementation of model runtime",
+            ),
+            ConfigKey.protocol: (
+                RUNTIME_PROTOCOLS,
+                "Protocol-based class with the implementation of communication between inference tester and inference runner",  # noqa: E501
+            ),
+            ConfigKey.automl: (
+                AUTOML,
+                "AutoML-based class with AutoML flow implementation",
+            ),
+        }
+        for block_type in block_types:
+            flag_group.add_argument(
+                CONFIG_KEY_TO_CLS_FLAG[block_type],
+                help=help_and_completer_map[block_type][1],
+                default=None,
+            ).completer = ClassPathCompleter(
+                help_and_completer_map[block_type][0]
+            )
+
+    @staticmethod
     @abstractmethod
     def run(
         args: argparse.Namespace, not_parsed: List[str] = [], **kwargs: Any
@@ -243,3 +333,110 @@ class CommandTemplate(ABC):
     @staticmethod
     def get_overridable(subcommands: List[str]) -> List[ConfigKey]:
         return []
+
+    @classmethod
+    def parse_configuration(
+        cls,
+        args: argparse.Namespace,
+        not_parsed: List[str],
+        keys: List[ConfigKey],
+    ) -> KenningBlockConfigDict:
+        """
+        Converts parsed command line flags (including an optional path to
+        YAML/JSON config file given under --cfg flag) to a standard Kenning
+        configuration dict.
+
+        Parameters
+        ----------
+        args: argparse.Namespace
+            Parsed CLI flags.
+        not_parsed: List[str]
+            Flags that were not matched and parsed by argparse. Since we do
+            not know what blocks will be chosen before parsing some of the
+            arguments, we cannot parse flag parameters specific to blocks
+            right away - therefore argparse parsing has to be done in 2 stages.
+        keys: List[ConfigKey]
+            List of block types required by the scenario.
+
+        Returns
+        -------
+        KenningBlockConfigDict
+            Standard-format Kenning configuration dict with parsed blocks and
+            parameters. See 'block_config' module for details on the format.
+        """
+        KLogger.info(f"Parsing configuration for scenario {cls.__name__}...")
+        args = cls.prepare_args(args)
+        config = argparse_to_config_dict(args)
+        if args.json_cfg is not None:
+            KLogger.debug(f"Config file detected at {args.json_cfg}.")
+            with open(args.json_cfg, "r") as f:
+                cfg = yaml.safe_load(f)
+                config = merge_config_dicts(
+                    yaml_or_json_to_config_dict(cfg), config
+                )
+        config = apply_default_blocks_by_block_type(
+            cls.default_block_classes, config
+        )
+
+        def flatten_list(irregular_list):
+            return (
+                [
+                    element
+                    for item in irregular_list
+                    for element in flatten_list(item)
+                ]
+                if type(irregular_list) is list
+                else [irregular_list]
+            )
+
+        classes = flatten_list(
+            list(classes_from_full_dict_config(config).values())
+        )
+        args = parse_classes(
+            classes,
+            args,
+            not_parsed,
+        )
+        config = merge_config_dicts(config, argparse_to_config_dict(args))
+        config = filter_block_types_from_config_dict(keys, config)
+        KLogger.info(
+            f"Collected blocks: {[block.__name__ for block in classes]}"
+        )
+        return config
+
+    @classmethod
+    def initialize_blocks(
+        cls,
+        args: argparse.Namespace,
+        not_parsed: List[str],
+        keys: List[ConfigKey],
+    ) -> Dict[ConfigKey, Any]:
+        """
+        Converts parsed command line flags (including an optional path to
+        YAML/JSON config file given under --cfg flag) to a standard Kenning
+        configuration dict. Then uses this dict to create block objects with
+        the required parameters.
+
+        Parameters
+        ----------
+        args: argparse.Namespace
+            Parsed CLI flags.
+        not_parsed: List[str]
+            Flags that were not matched and parsed by argparse. Since we do
+            not know what blocks will be chosen before parsing some of the
+            arguments, we cannot parse flag parameters specific to blocks
+            right away - therefore argparse parsing has to be done in 2 stages.
+        keys: List[ConfigKey]
+            List of block types required by the scenario.
+
+        Returns
+        -------
+        Dict[ConfigKey, Any]
+            Block objects sorted by block type. For ConfigKey.optimizers block
+            type, the value is a list of objects (because there can be many
+            optimizers). For all other block types, the value is an object.
+        """
+        KLogger.info(f"Building Kenning blocks for scenario {cls.__name__}...")
+        return objs_from_full_dict_config(
+            cls.parse_configuration(args, not_parsed, keys)
+        )
